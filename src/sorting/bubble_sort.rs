b1edeeb3ba51ccea7759138a733b6d4dc134124c
Yashwanth Reddy Gondi <yashwanthreddy.g@live.com>
2018-10-02 12:33:08 +0530

    added quicksort, bubblesort and related tests (#1)

---
+++ src/sorting/bubble_sort.rs
@@ -0,0 +1,10 @@
+#[allow(dead_code)]
+pub fn bubble_sort<T: Ord>(arr: &mut [T]) {
+    for i in 0..arr.len() {
+        for j in 0..arr.len() - 1 - i {
+            if arr[j] > arr[j + 1] {
+                arr.swap(j, j + 1);
+            }
+        }
+    }
+}
