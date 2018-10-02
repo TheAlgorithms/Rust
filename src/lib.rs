c361f56208a370c0e9bc8261dc1edbd0fd396d79
yashwanthreddyg <yashwanthreddy.g@live.com>
2018-10-02 09:06:11 +0530

    ran rustfmt on files

--- src/lib.rs
+++ src/lib.rs
@@ -30,41 +30,35 @@ mod tests {
     }
     use sorting;
     #[test]
-    fn quick_sort()
-    {
+    fn quick_sort() {
         //descending
-        let mut ve1 = vec![6,5,4,3,2,1];
+        let mut ve1 = vec![6, 5, 4, 3, 2, 1];
         sorting::quick_sort(&mut ve1);
-        for i in 0..ve1.len() - 1 
-        {
-            assert!(ve1[i] <= ve1[i+1]);
+        for i in 0..ve1.len() - 1 {
+            assert!(ve1[i] <= ve1[i + 1]);
         }
 
         //pre-sorted
-        let mut ve2 = vec![1,2,3,4,5,6];
+        let mut ve2 = vec![1, 2, 3, 4, 5, 6];
         sorting::quick_sort(&mut ve2);
-        for i in 0..ve2.len() - 1 
-        {
-            assert!(ve2[i] <= ve2[i+1]);
+        for i in 0..ve2.len() - 1 {
+            assert!(ve2[i] <= ve2[i + 1]);
         }
     }
     #[test]
-    fn bubble_sort()
-    {
+    fn bubble_sort() {
         //descending
-        let mut ve1 = vec![6,5,4,3,2,1];
+        let mut ve1 = vec![6, 5, 4, 3, 2, 1];
         sorting::bubble_sort(&mut ve1);
-        for i in 0..ve1.len() - 1 
-        {
-            assert!(ve1[i] <= ve1[i+1]);
+        for i in 0..ve1.len() - 1 {
+            assert!(ve1[i] <= ve1[i + 1]);
         }
 
         //pre-sorted
-        let mut ve2 = vec![1,2,3,4,5,6];
+        let mut ve2 = vec![1, 2, 3, 4, 5, 6];
         sorting::bubble_sort(&mut ve2);
-        for i in 0..ve2.len() - 1 
-        {
-            assert!(ve2[i] <= ve2[i+1]);
+        for i in 0..ve2.len() - 1 {
+            assert!(ve2[i] <= ve2[i + 1]);
         }
     }
 }
--- src/sorting/quick_sort.rs
+++ src/sorting/quick_sort.rs
@@ -1,42 +1,30 @@
-
 extern crate std;
 
-fn _display<T : std::fmt::Display>(arr: &[T])
-{
+fn _display<T: std::fmt::Display>(arr: &[T]) {
     print!("\n");
-    for item in arr
-    {
+    for item in arr {
         print!("{} ", item);
     }
 }
 
-fn _partition<T : Ord>(arr: &mut [T], lo: isize, hi: isize) -> isize
-{
+fn _partition<T: Ord>(arr: &mut [T], lo: isize, hi: isize) -> isize {
     let pivot: *mut T = &mut arr[hi as usize];
-    let mut  i = lo - 1;
+    let mut i = lo - 1;
     let mut j = hi;
-    
-    
-    unsafe
-    {
-        loop
-        {
+
+    unsafe {
+        loop {
             i += 1;
-            while arr[i as usize] < *pivot
-            {
-                i+=1;
+            while arr[i as usize] < *pivot {
+                i += 1;
             }
             j -= 1;
-            while j >= 0 && arr[j as usize] > *pivot
-            {
-                j-=1;
+            while j >= 0 && arr[j as usize] > *pivot {
+                j -= 1;
             }
-            if i >= j
-            {
+            if i >= j {
                 break;
-            }
-            else
-            {
+            } else {
                 arr.swap(i as usize, j as usize);
             }
         }
@@ -44,18 +32,15 @@ fn _partition<T : Ord>(arr: &mut [T], lo: isize, hi: isize) -> isize
     arr.swap(i as usize, hi as usize);
     return i;
 }
-fn _quick_sort<T: Ord>(arr: &mut [T], lo: isize, hi: isize)
-{
-    if lo < hi
-    {
+fn _quick_sort<T: Ord>(arr: &mut [T], lo: isize, hi: isize) {
+    if lo < hi {
         let p = _partition(arr, lo, hi);
-        _quick_sort(arr, lo, p-1);
-        _quick_sort(arr, p+1, hi);
+        _quick_sort(arr, lo, p - 1);
+        _quick_sort(arr, p + 1, hi);
     }
 }
 #[allow(dead_code)]
-pub fn quick_sort<T: Ord>(arr: &mut [T])
-{
+pub fn quick_sort<T: Ord>(arr: &mut [T]) {
     let len = arr.len();
     _quick_sort(arr, 0, (len - 1) as isize);
 }
