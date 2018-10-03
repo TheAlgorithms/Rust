68aac0fd02da70e5b2c34fe5e964614ef5922f04
Emilien Fugier <mail@emilienfugier.xyz>
2018-10-03 11:53:06 +0200

    Refactor Searching (#30)

---
+++ src/searching/binary_search.rs
@@ -0,0 +1,25 @@
+use std::cmp::{PartialEq, PartialOrd};
+
+pub fn binary_search<T: PartialEq + PartialOrd>(item: &T, arr: &[T]) -> Option<usize> {
+    let mut left = 0;
+    let mut right = arr.len() - 1;
+
+    while left < right {
+        let mid = left + (right - left) / 2;
+
+        if &arr[mid] > item {
+            right = mid - 1;
+        } else if &arr[mid] < item {
+            left = mid + 1;
+        } else {
+            left = mid;
+            break;
+        }
+    }
+
+    if &arr[left] != item {
+        return None;
+    }
+
+    return Some(left);
+}
---
+++ src/searching/linear_search.rs
@@ -0,0 +1,11 @@
+use std::cmp::PartialEq;
+
+pub fn linear_search<T: PartialEq>(item: &T, arr: &[T]) -> Option<usize> {
+    for (i, data) in arr.iter().enumerate() {
+        if item == data {
+            return Some(i);
+        }
+    }
+
+    return None;
+}
