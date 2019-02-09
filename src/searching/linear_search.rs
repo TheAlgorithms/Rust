20d2fbd81da6faa1e6ce23434c02619e86117b34
Arnav Borborah <arnavborborah11@gmail.com>
2019-02-09 22:52:50 +0000

    Separate tests more and add another test

--- src/searching/linear_search.rs
+++ src/searching/linear_search.rs
@@ -12,24 +12,38 @@ pub fn linear_search<T: PartialEq>(item: &T, arr: &[T]) -> Option<usize> {
 
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn linear() {
-        let index = super::linear_search(&"a", &vec!["a", "b", "c", "d", "google", "zoo"]);
-        assert_eq!(index, Some(0));
+    use super::*;
 
-        let index = super::linear_search(&4, &vec![1, 2, 3, 4]);
+    #[test]
+    fn search_strings() {
+        let index = linear_search(&"a", &vec!["a", "b", "c", "d", "google", "zoo"]);
+        assert_eq!(index, Some(0));
+    }
+
+    #[test]
+    fn search_ints() {
+        let index = linear_search(&4, &vec![1, 2, 3, 4]);
         assert_eq!(index, Some(3));
 
-        let index = super::linear_search(&3, &vec![1, 2, 3, 4]);
+        let index = linear_search(&3, &vec![1, 2, 3, 4]);
         assert_eq!(index, Some(2));
 
-        let index = super::linear_search(&2, &vec![1, 2, 3, 4]);
+        let index = linear_search(&2, &vec![1, 2, 3, 4]);
         assert_eq!(index, Some(1));
 
-        let index = super::linear_search(&1, &vec![1, 2, 3, 4]);
+        let index = linear_search(&1, &vec![1, 2, 3, 4]);
         assert_eq!(index, Some(0));
+    }
 
-        let index = super::linear_search(&5, &vec![1, 2, 3, 4]);
+    #[test]
+    fn not_found() {
+        let index = linear_search(&5, &vec![1, 2, 3, 4]);
+        assert_eq!(index, None);
+    }
+
+    #[test]
+    fn empty() {
+        let index = linear_search(&1, &vec![]);
         assert_eq!(index, None);
     }
 }
