14cf788702abeafe9ae6df93e77d7f4c83932908
teanaa <2017.tina.chandwani@ves.ac.in>
2019-10-29 00:21:23 +0530

    Added comments in Jump search

--- src/searching/jump_search.rs
+++ src/searching/jump_search.rs
@@ -3,7 +3,7 @@ fn jump_search(arr: Vec<i32>,n :i32) -> i32 {
     let mut step = (arr.len() as f64).sqrt() as usize;
     let len = arr.len();
     let mut prev: usize = 0;
-    //Jumps 
+    // Jumps 
     while arr[min(step, len)-1] < n {
         prev = step;
         step += step;
@@ -18,14 +18,15 @@ fn jump_search(arr: Vec<i32>,n :i32) -> i32 {
             return -1
         }
     }
-    // Element found
+    // If Element Found
     if arr[prev] == n {
         return prev as i32
     }
     -1
 }
+// Main Function
 fn main() {
-    let arr = vec![0, 1, 1, 2, 3, 5, 8, 13, 21,34, 55, 89, 144, 233, 377, 610];
-    println!("Found {} at index {}",55,jump_search(arr,55));
+    let arr = vec![0, 1, 1, 2, 3, 5, 8, 13, 21,34, 55, 89, 144, 233, 377, 610]; // Array elements
+    println!("Found {} at index {}",233,jump_search(arr,55)); // Write here the element to be found
 }
   
