7756342c930d60666bce1f73f756042386c9de07
Pierre Gimalac <pierre.gimalac@gmail.com>
2019-12-19 16:57:49 +0100

    convex hull algorithm: graham scan

--- src/general/mod.rs
+++ src/general/mod.rs
@@ -1,6 +1,8 @@
+mod convex_hull;
 mod hanoi;
 mod kmeans;
 
+pub use self::convex_hull::convex_hull_graham;
 pub use self::hanoi::hanoi;
 pub use self::kmeans::f32::kmeans as kmeans_f32;
 pub use self::kmeans::f64::kmeans as kmeans_f64;
