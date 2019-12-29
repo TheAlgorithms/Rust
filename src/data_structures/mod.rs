25b309ede1893acec08ce3588c4758ff36e2c39b
gustavohd18@gmail.com <gustavo.hernandez@acad.pucrs.br>
2019-12-29 20:45:31 -0300

    change mod file add queue

--- src/data_structures/mod.rs
+++ src/data_structures/mod.rs
@@ -1,7 +1,9 @@
 mod b_tree;
 mod binary_search_tree;
 mod heap;
+mod queue;
 
 pub use self::b_tree::BTree;
 pub use self::binary_search_tree::BinarySearchTree;
 pub use self::heap::{Heap, MaxHeap, MinHeap};
+pub use self::queue::Queue;
