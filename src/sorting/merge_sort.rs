fce63aabb650e5f924fb4a056eaaedf158e68f2d
pokotsun <gorigolila@gmail.com>
2020-01-10 13:29:51 +0900

    refactor merge sort

--- src/sorting/merge_sort.rs
+++ src/sorting/merge_sort.rs
@@ -1,10 +1,10 @@
-use std::fmt::Display;
-pub fn merge_sort<T: Ord + Copy + Display>(arr: &mut [T], tmp: &mut [T]) {
+pub fn merge_sort<T: Ord + Copy + Default>(arr: &mut Vec<T>) {
     let len = arr.len();
-    m_sort(arr, tmp, 0, len);
+    let mut tmp = vec![T::default(); len];
+    m_sort(arr, &mut tmp, 0, len);
 }
 
-fn m_sort<T: Ord + Copy + Display>(arr: &mut [T], tmp: &mut [T], left: usize, right: usize) {
+fn m_sort<T: Ord + Copy>(arr: &mut Vec<T>, tmp: &mut Vec<T>, left: usize, right: usize) {
     let mid = (right + left) / 2;
     if mid > left {
         m_sort(arr, tmp, left, mid);
@@ -12,46 +12,39 @@ fn m_sort<T: Ord + Copy + Display>(arr: &mut [T], tmp: &mut [T], left: usize, ri
 
         merge(arr, tmp, left, right);
     }
-    for i in 0 .. arr.len() {
-        print!("{}, ", arr[i]);
-    }
-    println!("\n");
 }
 
-fn merge<T: Ord + Copy>(arr: &mut [T], tmp: &mut [T], l: usize, r: usize) {
+fn merge<T: Ord + Copy>(arr: &mut Vec<T>, tmp: &mut Vec<T>, l: usize, r: usize) {
     let mut cpos = l;
     let mut left = l;
     let mut mid = (l + r) / 2;
 
-    let left_end = mid;
-    println!("(left, mid, right): ({}, {}, {})", l, mid, r);
+    let lend = mid;
 
-    while left < left_end && mid < r {
+    while left < lend && mid < r {
         if arr[left] <= arr[mid] {
             tmp[cpos] = arr[left];
-            cpos+=1;
-            left+=1;
-
+            left += 1;
         } else {
             tmp[cpos] = arr[mid];
-            cpos+=1;
-            mid+=1;
+            mid += 1;
         }
+        cpos += 1;
     }
 
-    while left < left_end {
+    while left < lend {
         tmp[cpos] = arr[left];
-        left+=1;
-        cpos+=1;
+        left += 1;
+        cpos += 1;
     }
 
     while mid < r {
         tmp[cpos] = arr[mid];
-        mid+=1;
-        cpos+=1;
+        mid += 1;
+        cpos += 1;
     }
 
-    for i in l .. r {
+    for i in l..r {
         arr[i] = tmp[i];
     }
 }
@@ -62,39 +55,35 @@ mod test {
 
     #[test]
     fn basic() {
-        let mut arr:[i32; 8] = [6, 5, 3, 1, 8, 7, 2, 4];
-        let mut tmp: [i32; 8] = [0; 8];
-        merge_sort(&mut arr, &mut tmp);
+        let mut arr = vec![6, 5, 3, 1, 8, 7, 2, 4];
+        merge_sort(&mut arr);
         for i in 0..arr.len() - 1 {
-            assert!(arr[i] <= arr[i+1]);
+            assert!(arr[i] <= arr[i + 1]);
         }
     }
 
     #[test]
     fn empty() {
-        let mut arr:[i32; 0] = [];
-        let mut tmp: [i32; 0] = [];
-        merge_sort(&mut arr, &mut tmp);
+        let mut arr: Vec<i32> = vec![];
+        merge_sort(&mut arr);
         assert_eq!(arr, []);
     }
 
     #[test]
     fn reverse() {
-        let mut arr:[i32; 8] = [8, 7, 6, 5, 4, 3, 2, 1];
-        let mut tmp: [i32; 8] = [0; 8];
-        merge_sort(&mut arr, &mut tmp);
+        let mut arr = vec![8, 7, 6, 5, 4, 3, 2, 1];
+        merge_sort(&mut arr);
         for i in 0..arr.len() - 1 {
-            assert!(arr[i] <= arr[i+1]);
+            assert!(arr[i] <= arr[i + 1]);
         }
     }
 
     #[test]
     fn already_sorted() {
-        let mut arr:[i32; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
-        let mut tmp: [i32; 8] = [0; 8];
-        merge_sort(&mut arr, &mut tmp);
+        let mut arr = vec![1, 2, 3, 4, 5, 6, 7, 8];
+        merge_sort(&mut arr);
         for i in 0..arr.len() - 1 {
-            assert!(arr[i] <= arr[i+1]);
+            assert!(arr[i] <= arr[i + 1]);
         }
     }
 }
