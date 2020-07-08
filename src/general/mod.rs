0677ffc7be825ecf223c3ad0c0161a012d02b1af
D4rkia <d4rkia@gmail.com>
2020-07-08 13:00:20 +0200

    Add xorshift algorithm

--- src/general/mod.rs
+++ src/general/mod.rs
@@ -1,8 +1,10 @@
 mod convex_hull;
 mod hanoi;
 mod kmeans;
+mod xorshift;
 
 pub use self::convex_hull::convex_hull_graham;
 pub use self::hanoi::hanoi;
 pub use self::kmeans::f32::kmeans as kmeans_f32;
 pub use self::kmeans::f64::kmeans as kmeans_f64;
+pub use self::xorshift::Rand;
