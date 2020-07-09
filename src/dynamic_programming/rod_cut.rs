9c813937790882042d7b63001eff93fef7d0a693
rocas777 <lmiguelapinto@gmail.com>
2020-07-09 12:57:10 +0100

    code style fixed

--- src/dynamic_programming/rod_cut.rs
+++ src/dynamic_programming/rod_cut.rs
@@ -7,25 +7,24 @@
 //lenghts  -> vector with the possible lenghts a rope can be cut
 //prices   -> prices of the respective leghts
 
-pub fn rod_cut(rope_len: usize, lengths_vector: Vec<usize>, prices_vector: Vec<usize>) -> usize
-{
+pub fn rod_cut(rope_len: usize, lengths_vector: Vec<usize>, prices_vector: Vec<usize>) -> usize {
     //both vectors have to be the same size
-    if prices_vector.len()!=lengths_vector.len() {
+    if prices_vector.len() != lengths_vector.len() {
         return 0;
     }
-    if prices_vector.len()==0 {
+    if prices_vector.len() == 0 {
         return 0;
     }
 
-    let mut auxiliary_vec: Vec<usize> = vec![0; rope_len+1]; // vector to store information, "memoization"
-    let mut current_vec: Vec<usize> = vec![0; rope_len+1];
+    let mut auxiliary_vec: Vec<usize> = vec![0; rope_len + 1]; // vector to store information, "memoization"
+    let mut current_vec: Vec<usize> = vec![0; rope_len + 1];
     let mut minimum = 0;
     //for each possible lenght in the vector
-    for (i,rope_available_len) in lengths_vector.iter().enumerate() {
+    for (i, rope_available_len) in lengths_vector.iter().enumerate() {
         //try to cut rope with size [1,rope_len[
         for size in minimum..rope_len {
             //if the rope size trying to cut is bigger than the rope actual size, the segment cant be cut with that size
-            if size+1>= *rope_available_len {
+            if size + 1 >= *rope_available_len {
                 let last_v = current_vec[(size + 1) - *rope_available_len] + prices_vector[i];
                 let current_v = auxiliary_vec[size + 1];
 
@@ -37,14 +36,13 @@ pub fn rod_cut(rope_len: usize, lengths_vector: Vec<usize>, prices_vector: Vec<u
             }
         }
         //fill the auxiliary vector with the values from the current vector
-        for (i,v) in current_vec.iter().enumerate(){
+        for (i, v) in current_vec.iter().enumerate() {
             auxiliary_vec[i] = *v;
         }
-        minimum+=1;
+        minimum += 1;
     }
     return current_vec[rope_len];
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -52,11 +50,35 @@ mod tests {
 
     #[test]
     fn test_rod_cut() {
-        assert_eq!(rod_cut(11,vec![1,2,3,4,5,6,7,8],vec![1,5,8,9,10,17,17,20]),30);
-        assert_eq!(rod_cut(8,vec![1,2,3,4,5,6,7,8],vec![1,5,8,9,10,17,17,20]),22);
-        assert_eq!(rod_cut(4,vec![1,2,3,4,5,6,7,8],vec![1,5,8,9,10,17,17,20]),10);
-        assert_eq!(rod_cut(105,vec![1,2,3,5,6,7],vec![1,5,9,10,17,17]),315);
-        assert_eq!(rod_cut(8,vec![7],vec![17]),17);
-        assert_eq!(rod_cut(1,vec![1,2,3,4],vec![1,2,3,4]),1);
+        assert_eq!(
+            rod_cut(
+                11,
+                vec![1, 2, 3, 4, 5, 6, 7, 8],
+                vec![1, 5, 8, 9, 10, 17, 17, 20]
+            ),
+            30
+        );
+        assert_eq!(
+            rod_cut(
+                8,
+                vec![1, 2, 3, 4, 5, 6, 7, 8],
+                vec![1, 5, 8, 9, 10, 17, 17, 20]
+            ),
+            22
+        );
+        assert_eq!(
+            rod_cut(
+                4,
+                vec![1, 2, 3, 4, 5, 6, 7, 8],
+                vec![1, 5, 8, 9, 10, 17, 17, 20]
+            ),
+            10
+        );
+        assert_eq!(
+            rod_cut(105, vec![1, 2, 3, 5, 6, 7], vec![1, 5, 9, 10, 17, 17]),
+            315
+        );
+        assert_eq!(rod_cut(8, vec![7], vec![17]), 17);
+        assert_eq!(rod_cut(1, vec![1, 2, 3, 4], vec![1, 2, 3, 4]), 1);
     }
 }
