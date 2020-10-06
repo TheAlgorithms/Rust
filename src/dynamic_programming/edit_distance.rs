6118f5b3473aae963862738d3aa3960cd3a6bd17
Tianyi Shi <ShiTianyi2001@outlook.com>
2020-10-06 13:49:46 +0100

    space efficient edit distance algorithm

--- src/dynamic_programming/edit_distance.rs
+++ src/dynamic_programming/edit_distance.rs
@@ -8,6 +8,13 @@ use std::cmp::min;
 ///
 /// This function iterates over the bytes in the string, so it may not behave
 /// entirely as expected for non-ASCII strings.
+///
+/// # Complexity
+///
+/// - time complexity: O(nm),
+/// - space complexity: O(nm),
+///
+/// where n and m are lengths of `str_a` and `str_b`
 pub fn edit_distance(str_a: &str, str_b: &str) -> u32 {
     // distances[i][j] = distance between a[..i] and b[..j]
     let mut distances = vec![vec![0; str_b.len() + 1]; str_a.len() + 1];
@@ -31,14 +38,53 @@ pub fn edit_distance(str_a: &str, str_b: &str) -> u32 {
     distances[str_a.len()][str_b.len()]
 }
 
+/// The space efficient version of the above algorithm.
+///
+/// # Complexity
+///
+/// - time complexity: O(nm),
+/// - space complexity: O(n),
+///
+/// where n and m are lengths of `str_a` and `str_b`
+pub fn edit_distance_se(str_a: &str, str_b: &str) -> u32 {
+    let (str_a, str_b) = (str_a.as_bytes(), str_b.as_bytes());
+    let (m, n) = (str_a.len() + 1, str_b.len() + 1);
+    let mut dp_matrix: Vec<u32> = vec![0; n]; // the dynamic programming matrix (only 1 row stored)
+    let mut s: u32; // dp_matrix[i - 1][j - 1] or dp_matrix[i - 1][j]
+    let mut c: u32; // dp_matrix[i][j - 1] or dp_matrix[i][j]
+    let mut a: u8; // str_a[i - 1]
+    let mut b: u8; // str_b[j - 1]
+
+    // 0th row
+    for j in 1..n {
+        dp_matrix[j] = j as u32;
+    }
+    // rows 1 to m
+    for i in 1..m {
+        s = (i - 1) as u32;
+        c = i as u32;
+        a = str_a[i - 1];
+        for j in 1..n {
+            b = str_b[j - 1];
+            c = min(s + if a == b { 0 } else { 1 }, min(c + 1, dp_matrix[j] + 1)); // c becomes dp_matrix[i][j]
+            s = dp_matrix[j]; // s becomes dp_matrix[i - 1][j]
+            dp_matrix[j] = c;
+        }
+    }
+
+    dp_matrix[n - 1]
+}
+
 #[cfg(test)]
 mod tests {
-    use super::edit_distance;
+    use super::*;
 
     #[test]
     fn equal_strings() {
         assert_eq!(0, edit_distance("Hello, world!", "Hello, world!"));
+        assert_eq!(0, edit_distance_se("Hello, world!", "Hello, world!"));
         assert_eq!(0, edit_distance("Test_Case_#1", "Test_Case_#1"));
+        assert_eq!(0, edit_distance_se("Test_Case_#1", "Test_Case_#1"));
     }
 
     #[test]
@@ -46,6 +92,9 @@ mod tests {
         assert_eq!(1, edit_distance("Hello, world!", "Hell, world!"));
         assert_eq!(1, edit_distance("Test_Case_#1", "Test_Case_#2"));
         assert_eq!(1, edit_distance("Test_Case_#1", "Test_Case_#10"));
+        assert_eq!(1, edit_distance_se("Hello, world!", "Hell, world!"));
+        assert_eq!(1, edit_distance_se("Test_Case_#1", "Test_Case_#2"));
+        assert_eq!(1, edit_distance_se("Test_Case_#1", "Test_Case_#10"));
     }
 
     #[test]
@@ -53,5 +102,8 @@ mod tests {
         assert_eq!(2, edit_distance("My Cat", "My Case"));
         assert_eq!(7, edit_distance("Hello, world!", "Goodbye, world!"));
         assert_eq!(6, edit_distance("Test_Case_#3", "Case #3"));
+        assert_eq!(2, edit_distance_se("My Cat", "My Case"));
+        assert_eq!(7, edit_distance_se("Hello, world!", "Goodbye, world!"));
+        assert_eq!(6, edit_distance_se("Test_Case_#3", "Case #3"));
     }
 }
