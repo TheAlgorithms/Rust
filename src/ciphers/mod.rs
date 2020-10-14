5a1525c23d89238f85241ad6404f5c3a257a1d5f
Sutthinart Khunvadhana <iakgoog@gmail.com>
2020-10-15 05:28:55 +0700

    Add another implementation of rot13

--- src/ciphers/mod.rs
+++ src/ciphers/mod.rs
@@ -1,7 +1,9 @@
+mod another_rot13;
 mod caesar;
 mod rot13;
 mod vigenere;
 
+pub use self::another_rot13::another_rot13;
 pub use self::caesar::caesar;
 pub use self::rot13::rot13;
 pub use self::vigenere::vigenere;
