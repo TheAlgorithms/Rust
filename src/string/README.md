9c26ab52f96b035a6e02ecd09924b1c5bf6a865b
Tanishq Jain <tanishqjain1002@gmail.com>
2020-10-23 20:39:29 +0530

    Added Rabin Karp

--- src/string/README.md
+++ src/string/README.md
@@ -8,4 +8,10 @@ __Properties__
 * Case performance  O(s + w)
 * Case space complexity  O(w)
 
-[kmp-wiki]: https://en.wikipedia.org/wiki/Knuth–Morris–Pratt_algorithm
+### [Rabin Karp](./rabin_karp.rs)
+From [Wikipedia][]In computer science, the Rabin–Karp algorithm or Karp–Rabin algorithm is a string-searching algorithm created by Richard M. Karp and Michael O. Rabin (1987) that uses hashing to find an exact match of a pattern string in a text. It uses a rolling hash to quickly filter out positions of the text that cannot match the pattern, and then checks for a match at the remaining positions. Generalizations of the same idea can be used to find more than one match of a single pattern, or to find matches for more than one pattern.
+
+__Properties__
+* Average time complexit  O(m + n)
+
+[kmp-wiki]: https://en.wikipedia.org/wiki/Rabin%E2%80%93Karp_algorithm
---
+++ src/string/rabin_karp.rs
@@ -0,0 +1,116 @@
+pub fn rabin_karp(txt: String, pat: String, q: i64) -> Vec<usize> {
+    let mut res = Vec::<usize>::new();
+
+    // Rust strings are utf8 compatible and hence they can store all the 1,112,064 characters and not just the 256 ASCII characters
+    // Note: even if it is set to 256, result will have no effect
+    let d = 1_112_064 as i64;
+
+    let mut p = 0 as i64;
+    let mut t = 0 as i64;
+    let mut h = 1;
+
+    let m = pat.len();
+    let n = txt.len();
+
+    if m != 0 && n != 0 {
+        for _i in 0..m - 1 {
+            h = (h * d) % q;
+        }
+
+        for i in 0..m {
+            p = ((d * p + pat.chars().nth(i).unwrap() as i64) % q).into();
+            t = ((d * t + txt.chars().nth(i).unwrap() as i64) % q).into();
+        }
+
+        for i in 0..=(n - m) {
+            if p == t {
+                for j in 0..m {
+                    if txt.chars().nth(i + j).unwrap() != pat.chars().nth(j).unwrap() {
+                        break;
+                    }
+
+                    if j == m - 1 {
+                        res.push(i);
+                    }
+                }
+            }
+
+            if i < n - m {
+                t = (d * (t - txt.chars().nth(i).unwrap() as i64 * h)
+                    + (txt.chars().nth(i + m).unwrap() as i64))
+                    % q;
+
+                if t < 0 {
+                    t = t + q;
+                }
+            }
+        }
+    } else if n != 0 && m == 0 {
+        res.push(0);
+    }
+
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rabin_karp_each_letter_matches() {
+        let index = rabin_karp("aaa".to_string(), "a".to_string(), 101);
+        assert_eq!(index, vec![0, 1, 2]);
+    }
+
+    #[test]
+    fn rabin_karp_a_few_separate_matches() {
+        let index = rabin_karp("abababa".to_string(), "ab".to_string(), 101);
+        assert_eq!(index, vec![0, 2, 4]);
+    }
+
+    #[test]
+    fn rabin_karp_one_match() {
+        let index = rabin_karp(
+            "ABC ABCDAB ABCDABCDABDE".to_string(),
+            "ABCDABD".to_string(),
+            101,
+        );
+        assert_eq!(index, vec![15]);
+    }
+
+    #[test]
+    fn rabin_karp_lots_of_matches() {
+        let index = rabin_karp("aaabaabaaaaa".to_string(), "aa".to_string(), 101);
+        assert_eq!(index, vec![0, 1, 4, 7, 8, 9, 10]);
+    }
+
+    #[test]
+    fn rabin_karp_lots_of_intricate_matches() {
+        let index = rabin_karp("ababababa".to_string(), "aba".to_string(), 101);
+        assert_eq!(index, vec![0, 2, 4, 6]);
+    }
+
+    #[test]
+    fn rabin_karp_not_found0() {
+        let index = rabin_karp("abcde".to_string(), "f".to_string(), 101);
+        assert_eq!(index, vec![]);
+    }
+
+    #[test]
+    fn rabin_karp_not_found1() {
+        let index = rabin_karp("abcde".to_string(), "ac".to_string(), 101);
+        assert_eq!(index, vec![]);
+    }
+
+    #[test]
+    fn rabin_karp_not_found2() {
+        let index = rabin_karp("ababab".to_string(), "bababa".to_string(), 101);
+        assert_eq!(index, vec![]);
+    }
+
+    #[test]
+    fn rabin_karp_empty_string() {
+        let index = rabin_karp("".to_string(), "abcdef".to_string(), 101);
+        assert_eq!(index, vec![]);
+    }
+}
