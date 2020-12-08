37635d0322342ee2fc9b7e002da78f2fb60f455f
Huaping Yu <yearnfar@gmail.com>
2020-12-08 10:15:42 +0800

    Update bubble_sort.rs

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -1,5 +1,5 @@
 pub fn bubble_sort<T: Ord>(arr: &mut [T]) {
-    for i in 0..arr.len() {
+    for i in 0..arr.len() - 1 {
         for j in 0..arr.len() - 1 - i {
             if arr[j] > arr[j + 1] {
                 arr.swap(j, j + 1);
