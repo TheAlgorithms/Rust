5b7856a48e93f9b4e20be02ab0e0a20882e8d83b
Egor Bronnikov <52889537+endygamedev@users.noreply.github.com>
2021-01-25 14:03:10 +0300

    Added 0-1 Knapsack (#176)
    * Added 0-1 Knapsack
    
    * Edit 0-1 Knapsack problem
    
    * Edited 0-1 Knapsack problem
    
    * Corrected all notes 0-1 Knapsack problem
    
    * Updated README (0-1 Knapsack)

--- src/dynamic_programming/mod.rs
+++ src/dynamic_programming/mod.rs
@@ -1,10 +1,12 @@
 mod edit_distance;
 mod egg_dropping;
 mod fibonacci;
+mod knapsack;
 mod longest_common_subsequence;
 
 pub use self::edit_distance::{edit_distance, edit_distance_se};
 pub use self::egg_dropping::egg_drop;
 pub use self::fibonacci::fibonacci;
 pub use self::fibonacci::recursive_fibonacci;
+pub use self::knapsack::knapsack;
 pub use self::longest_common_subsequence::longest_common_subsequence;
