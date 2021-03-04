11fb7bd7d251399bc6ad567ab7dfe4d3ee68d19a
Darshan Kathiriya <darshankathiriya121@github.com>
2021-03-03 23:44:36 -0400

    fix clippy warning for linked list. convert list to singly liked list.

--- src/data_structures/linked_list.rs
+++ src/data_structures/linked_list.rs
@@ -4,23 +4,17 @@ use std::ptr::NonNull;
 struct Node<T> {
     val: T,
     next: Option<NonNull<Node<T>>>,
-    prev: Option<NonNull<Node<T>>>,
 }
 
 impl<T> Node<T> {
     fn new(t: T) -> Node<T> {
-        Node {
-            val: t,
-            prev: None,
-            next: None,
-        }
+        Node { val: t, next: None }
     }
 }
 
 pub struct LinkedList<T> {
-    length: u32,
+    length: usize,
     start: Option<NonNull<Node<T>>>,
-    end: Option<NonNull<Node<T>>>,
 }
 
 impl<T> Default for LinkedList<T> {
@@ -34,25 +28,86 @@ impl<T> LinkedList<T> {
         Self {
             length: 0,
             start: None,
-            end: None,
         }
     }
 
-    pub fn add(&mut self, obj: T) {
-        let mut node = Box::new(Node::new(obj));
-        // Since we are adding node at the end, next will always be None
-        node.next = None;
-        node.prev = self.end;
-        // Get a pointer to node
-        let node_ptr = Some(unsafe { NonNull::new_unchecked(Box::into_raw(node)) });
-        match self.end {
-            // This is the case of empty list
-            None => self.start = node_ptr,
-            Some(end_ptr) => unsafe { (*end_ptr.as_ptr()).next = node_ptr },
+    pub fn insert_head(&mut self, data: T) {
+        self.insert_at_index(0, data)
+    }
+
+    pub fn insert(&mut self, data: T) {
+        self.insert_at_index(self.length, data)
+    }
+
+    pub fn insert_at_index(&mut self, index: usize, data: T) {
+        if index > self.length {
+            panic!("list index out of range")
         }
-        self.end = node_ptr;
+        let mut node = Box::new(Node::new(data));
+        if self.start.is_none() {
+            self.start = Some(unsafe { NonNull::new_unchecked(Box::into_raw(node)) });
+        } else if index == 0 {
+            node.next = self.start;
+            self.start = Some(unsafe { NonNull::new_unchecked(Box::into_raw(node)) });
+        } else {
+            let mut tmp = self.start;
+            for _ in 0..index - 1 {
+                tmp = unsafe { &*tmp.as_ref().unwrap().as_ptr() }.next;
+            }
+            node.next = unsafe { &*tmp.as_ref().unwrap().as_ptr() }.next;
+            unsafe { &mut *tmp.as_ref().unwrap().as_ptr() }.next =
+                Some(unsafe { NonNull::new_unchecked(Box::into_raw(node)) });
+        }
         self.length += 1;
     }
+
+    pub fn delete_head(&mut self) -> Option<T> {
+        self.delete_at_index(0)
+    }
+    pub fn delete_tail(&mut self) -> Option<T> {
+        if self.length > 0 {
+            self.delete_at_index(self.length - 1)
+        } else {
+            None
+        }
+    }
+
+    pub fn delete_at_index(&mut self, index: usize) -> Option<T> {
+        assert!(index <= self.length, "list index out of range");
+        self.start?;
+        self.length -= 1;
+        if index == 0 {
+            let node = unsafe { std::ptr::read(self.start.unwrap().as_ptr()) };
+            self.start = node.next;
+            Some(node.val)
+        } else {
+            let mut tmp = self.start;
+            for _ in 0..index - 1 {
+                tmp = unsafe { tmp.unwrap().as_ref() }.next;
+            }
+            let delete_node =
+                unsafe { std::ptr::read((tmp.unwrap().as_ref()).next.unwrap().as_ptr()) };
+            unsafe {
+                tmp.unwrap().as_mut().next = tmp.unwrap().as_ref().next.unwrap().as_ref().next
+            };
+            Some(delete_node.val)
+        }
+    }
+    //pub fn add(&mut self, obj: T) {
+    //    let mut node = Box::new(Node::new(obj));
+    //    // Since we are adding node at the end, next will always be None
+    //    node.next = None;
+    //    node.prev = self.end;
+    //    // Get a pointer to node
+    //    let node_ptr = Some(unsafe { NonNull::new_unchecked(Box::into_raw(node)) });
+    //    match self.end {
+    //        // This is the case of empty list
+    //        None => self.start = node_ptr,
+    //        Some(end_ptr) => unsafe { (*end_ptr.as_ptr()).next = node_ptr },
+    //    }
+    //    self.end = node_ptr;
+    //    self.length += 1;
+    //}
 
     pub fn get(&mut self, index: i32) -> Option<&T> {
         self.get_ith_node(self.start, index)
@@ -66,6 +121,14 @@ impl<T> LinkedList<T> {
                 _ => self.get_ith_node(unsafe { (*next_ptr.as_ptr()).next }, index - 1),
             },
         }
+    }
+
+    pub fn len(&self) -> usize {
+        self.length
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.length == 0
     }
 }
 
@@ -100,9 +163,9 @@ mod tests {
     #[test]
     fn create_numeric_list() {
         let mut list = LinkedList::<i32>::new();
-        list.add(1);
-        list.add(2);
-        list.add(3);
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
         println!("Linked List is {}", list);
         assert_eq!(3, list.length);
     }
@@ -110,9 +173,9 @@ mod tests {
     #[test]
     fn create_string_list() {
         let mut list_str = LinkedList::<String>::new();
-        list_str.add("A".to_string());
-        list_str.add("B".to_string());
-        list_str.add("C".to_string());
+        list_str.insert("A".to_string());
+        list_str.insert("B".to_string());
+        list_str.insert("C".to_string());
         println!("Linked List is {}", list_str);
         assert_eq!(3, list_str.length);
     }
@@ -120,8 +183,8 @@ mod tests {
     #[test]
     fn get_by_index_in_numeric_list() {
         let mut list = LinkedList::<i32>::new();
-        list.add(1);
-        list.add(2);
+        list.insert(1);
+        list.insert(2);
         println!("Linked List is {}", list);
         let retrived_item = list.get(1);
         assert!(retrived_item.is_some());
@@ -131,11 +194,29 @@ mod tests {
     #[test]
     fn get_by_index_in_string_list() {
         let mut list_str = LinkedList::<String>::new();
-        list_str.add("A".to_string());
-        list_str.add("B".to_string());
+        list_str.insert("A".to_string());
+        list_str.insert("B".to_string());
         println!("Linked List is {}", list_str);
         let retrived_item = list_str.get(1);
         assert!(retrived_item.is_some());
         assert_eq!("B", *retrived_item.unwrap());
+    }
+
+    #[test]
+    fn delete_various() {
+        let mut list = LinkedList::<usize>::new();
+        assert_eq!(list.delete_head(), None);
+        assert_eq!(list.delete_tail(), None);
+        for i in 0..5 {
+            list.insert(i);
+        }
+        assert_eq!(list.delete_tail(), Some(4));
+        assert_eq!(list.delete_head(), Some(0));
+        assert_eq!(list.delete_at_index(1), Some(2));
+        assert_eq!(list.len(), 2);
+        assert_eq!(list.delete_head(), Some(1));
+        assert_eq!(list.delete_head(), Some(3));
+        assert_eq!(list.delete_head(), None);
+        assert_eq!(list.delete_tail(), None);
     }
 }
