daceb672ae29d1d3218c19edf00cda3f93a468a6
zixuanzhang <zixuanzhang1998@gmail.com>
2021-05-13 15:23:47 -0600

    Add Queue

--- src/data_structures/mod.rs
+++ src/data_structures/mod.rs
@@ -2,6 +2,7 @@ mod b_tree;
 mod binary_search_tree;
 mod heap;
 mod linked_list;
+mod queue;
 
 pub use self::b_tree::BTree;
 pub use self::binary_search_tree::BinarySearchTree;
---
+++ src/data_structures/queue.rs
@@ -0,0 +1,55 @@
+use std::default::Default;
+
+struct Queue<T>
+where
+    T: Default,
+{
+    items: Vec<T>
+}
+
+impl<T> Queue<T>
+where
+    T: Default,
+{
+    pub fn new() -> Self{
+        Self{
+            items: Vec::new()
+        }
+    }
+
+    pub fn is_empty(&self) -> bool{
+        self.items.is_empty()
+    }
+
+    pub fn len(&self) -> usize{
+        self.items.len()
+    }
+
+    pub fn enqueue(&mut self, item: T){
+        self.items.push(item)
+    }
+
+    pub fn dequeue(&mut self) -> T{
+        self.items.remove(0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_queue() {
+        let mut q = Queue::new();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(6);
+        q.enqueue(9);
+        assert_eq!(q.len(), 4);
+        assert_eq!(q.dequeue(), 1);
+        assert_eq!(q.dequeue(), 2);
+        assert_eq!(q.dequeue(), 6);
+        assert_eq!(q.dequeue(), 9);
+        assert_eq!(q.len(), 0);
+    }
+}
