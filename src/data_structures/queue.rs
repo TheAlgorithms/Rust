1da6b15543afa47f50f8035198d5e62a73f75de3
zixuanzhang <zixuanzhang1998@gmail.com>
2021-05-13 18:46:21 -0600

    Fix format.

--- src/data_structures/queue.rs
+++ src/data_structures/queue.rs
@@ -4,32 +4,30 @@ struct Queue<T>
 where
     T: Default,
 {
-    items: Vec<T>
+    items: Vec<T>,
 }
 
 impl<T> Queue<T>
 where
     T: Default,
 {
-    pub fn new() -> Self{
-        Self{
-            items: Vec::new()
-        }
+    pub fn new() -> Self {
+        Self { items: Vec::new() }
     }
 
-    pub fn is_empty(&self) -> bool{
+    pub fn is_empty(&self) -> bool {
         self.items.is_empty()
     }
 
-    pub fn len(&self) -> usize{
+    pub fn len(&self) -> usize {
         self.items.len()
     }
 
-    pub fn enqueue(&mut self, item: T){
+    pub fn enqueue(&mut self, item: T) {
         self.items.push(item)
     }
 
-    pub fn dequeue(&mut self) -> T{
+    pub fn dequeue(&mut self) -> T {
         self.items.remove(0)
     }
 }
@@ -37,7 +35,6 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn test_queue() {
         let mut q = Queue::new();
@@ -47,8 +44,11 @@ mod tests {
         q.enqueue(9);
         assert_eq!(q.len(), 4);
         assert_eq!(q.dequeue(), 1);
+        assert_eq!(q.len(), 3);
         assert_eq!(q.dequeue(), 2);
+        assert_eq!(q.len(), 2);
         assert_eq!(q.dequeue(), 6);
+        assert_eq!(q.len(), 1);
         assert_eq!(q.dequeue(), 9);
         assert_eq!(q.len(), 0);
     }
