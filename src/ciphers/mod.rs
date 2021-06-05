3318096fe2e9662622690974384dd70fe5f3e6dc
Inver <jankowskipawelxd@gmail.com>
2021-06-05 23:11:31 +0200

    Add Morse code cipher

--- src/ciphers/mod.rs
+++ src/ciphers/mod.rs
@@ -1,7 +1,9 @@
 mod caesar;
 mod rot13;
 mod vigenere;
+mod morse_code;
 
 pub use self::caesar::caesar;
 pub use self::rot13::rot13;
 pub use self::vigenere::vigenere;
+pub use self::morse_code::morse_code;
