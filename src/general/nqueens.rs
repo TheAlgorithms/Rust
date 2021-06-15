d8069b723f2ffa34c0d65f755ccf15d52e862532
S <32769890+shreeshjha@users.noreply.github.com>
2021-06-16 00:12:30 +0530

    Added Comments on Nqueens Problem

--- src/general/nqueens.rs
+++ src/general/nqueens.rs
@@ -1,6 +1,10 @@
+/* The eight queens' problem is the task of positioning eight chess queens on an 8/8 chessboard so that no two queens threaten each other.
+Consequently, no two queens must share the same row, column, or diagonal in order for a solution to be found.
+The eight queens problem is a variation on the two queens problem, which entails arranging two non-attacking queens on a board. */
+
 const N : usize = 8;
 
-
+// Solutions exist for all natural numbers N with the exception of N = 2 and N = 3.
 fn t(mut board: &mut [[bool; N]; N], r: usize, mut cnt: &mut i64){
 	if r == N {
 		*cnt += 1;
@@ -40,3 +44,63 @@ fn main() {
 	let mut cnt: i64 = 0;
 	t(&mut board, 0, &mut cnt);
 }
+
+
+/* Sample Output of the following code :- 
+
+Answer 1
+
+Q . . . . . . .
+. . . . Q . . .
+. . . . . . . Q
+. . . . . Q . .
+. . Q . . . . .
+. . . . . . Q .
+. Q . . . . . .
+. . . Q . . . .
+ 
+Answer 2
+
+Q . . . . . . .
+. . . . . Q . .
+. . . . . . . Q
+. . Q . . . . .
+. . . . . . Q .
+. . . Q . . . .
+. Q . . . . . .
+. . . . Q . . .
+ 
+Answer 3
+
+Q . . . . . . .
+. . . . . . Q .
+. . . Q . . . .
+. . . . . Q . .
+. . . . . . . Q
+. Q . . . . . .
+. . . . Q . . .
+. . Q . . . . .
+ 
+Answer 4
+
+Q . . . . . . .
+. . . . . . Q .
+. . . . Q . . .
+. . . . . . . Q
+. Q . . . . . .
+. . . Q . . . .
+. . . . . Q . .
+. . Q . . . . .
+ 
+Answer 5
+
+. Q . . . . . .
+. . . Q . . . .
+. . . . . Q . .
+. . . . . . . Q
+. . Q . . . . .
+Q . . . . . . .
+. . . . . . Q .
+. . . . Q . . .
+ 
+*/
