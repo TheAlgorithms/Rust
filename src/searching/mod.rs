e0e5381dfa5c92c10dbb4c6c9f1aa47dd43b6c85
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-09-10 05:29:48 -0300

    Add recursive Binary Search (#217)
    Co-authored-by: Andrii Siriak <siryaka@gmail.com>

--- src/searching/mod.rs
+++ src/searching/mod.rs
@@ -1,5 +1,7 @@
 mod binary_search;
+mod binary_search_recursive;
 mod linear_search;
 
 pub use self::binary_search::binary_search;
+pub use self::binary_search_recursive::binary_search_rec;
 pub use self::linear_search::linear_search;
