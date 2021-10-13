a7be1d4ea0a6c6f1cc9d5f86657ccfe05454d688
duy quang <hyouka266@gmail.com>
2021-10-13 22:11:00 +0700

    Update is_subsequence.rs

--- src/dynamic_programming/is_subsequence.rs
+++ src/dynamic_programming/is_subsequence.rs
@@ -1,3 +1,8 @@
+// Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+// A subsequence of a string is a new string that is formed from the original string
+// by deleting some (can be none) of the characters without disturbing the relative
+// positions of the remaining characters.
+// (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 pub fn is_subsequence(s: String, t: String) -> bool {
     let m = s.len();
     let n = t.len();
