d1e4c0f4407e7730b3a9bbef7c7ea290a8eb102c
DONSIMON92 <semenische@yandex.com>
2021-10-30 23:41:42 +0300

    add prime factorization

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -2,8 +2,10 @@ mod extended_euclidean_algorithm;
 mod pascal_triangle;
 mod prime_check;
 mod prime_numbers;
+mod trial_division;
 
 pub use self::extended_euclidean_algorithm::extended_euclidean_algorithm;
 pub use self::pascal_triangle::pascal_triangle;
 pub use self::prime_check::prime_check;
 pub use self::prime_numbers::prime_numbers;
+pub use self::trial_division::trial_division;
