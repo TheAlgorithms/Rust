77dd90366cffc4ba81b177c31ddfee1c35bee568
DONSIMON92 <semenische@yandex.com>
2021-10-30 23:38:51 +0300

    create prime number generator and primality test

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,5 +1,9 @@
 mod extended_euclidean_algorithm;
 mod pascal_triangle;
+mod prime_check;
+mod prime_numbers;
 
 pub use self::extended_euclidean_algorithm::extended_euclidean_algorithm;
 pub use self::pascal_triangle::pascal_triangle;
+pub use self::prime_check::prime_check;
+pub use self::prime_numbers::prime_numbers;
