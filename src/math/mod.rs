7aadffa3e50958c1e0f78514278ca1fe4f2c717e
DONSIMON92 <semenische@yandex.com>
2021-10-31 12:01:57 +0300

    add generator and program to check perfect number

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,5 +1,7 @@
 mod extended_euclidean_algorithm;
 mod pascal_triangle;
+mod perfect_numbers;
 
 pub use self::extended_euclidean_algorithm::extended_euclidean_algorithm;
 pub use self::pascal_triangle::pascal_triangle;
+pub use self::perfect_numbers::perfect_numbers;
