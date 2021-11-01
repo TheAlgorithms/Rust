bb9927ebd28eb8ee7af23e888b920e9a20096632
David Makray <dmakray@gmail.com>
2021-10-31 20:19:30 -0400

    Better Depth First Search Demo

--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,6 +1,7 @@
 mod bellman_ford;
 mod breadth_first_search;
 mod depth_first_search;
+mod depth_first_search_tic_tac_toe;
 mod dijkstra;
 mod minimum_spanning_tree;
 mod prim;
@@ -8,6 +9,7 @@ mod prim;
 pub use self::bellman_ford::bellman_ford;
 pub use self::breadth_first_search::breadth_first_search;
 pub use self::depth_first_search::depth_first_search;
+pub use self::depth_first_search_tic_tac_toe::minimax;
 pub use self::dijkstra::dijkstra;
 pub use self::minimum_spanning_tree::kruskal;
 pub use self::prim::{prim, prim_with_start};
