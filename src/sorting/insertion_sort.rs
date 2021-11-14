3cad41e826591a7f16cd5837306d63af0f54fab2
Arthur Kurbidaev <artkurbidaev@gmail.com>
2021-11-14 10:56:29 +0300

    Add bucket sort, improve insertion sort (#257)
    * Bucket sort implementation + in-place insertion sort
    
    * Bucket sort README
    
    Co-authored-by: Andrii Siriak <siryaka@gmail.com>

--- src/sorting/insertion_sort.rs
+++ src/sorting/insertion_sort.rs
@@ -1,73 +1,71 @@
 use std::cmp;
 
-#[allow(dead_code)]
-pub fn insertion_sort<T>(arr: &[T]) -> Vec<T>
+/// Sorts a mutable slice using in-place insertion sort algorithm.
+///
+/// Time complexity is `O(n^2)`, where `n` is the number of elements.
+/// Space complexity is `O(1)` as it sorts elements in-place.
+pub fn insertion_sort<T>(arr: &mut [T])
 where
-    T: cmp::PartialEq + cmp::PartialOrd + Clone,
+    T: cmp::PartialOrd + Copy,
 {
-    // The resulting vector should contain the same amount of elements as
-    // the slice that is being sorted, so enough room is preallocated
-    let mut result: Vec<T> = Vec::with_capacity(arr.len());
+    for i in 1..arr.len() {
+        let cur = arr[i];
+        let mut j = i - 1;
 
-    // Iterate over the elements to sort and
-    // put a clone of the element to insert in elem.
-    for elem in arr.iter().cloned() {
-        // How many elements have already been inserted?
-        let n_inserted = result.len();
-
-        // Loop over the inserted elements and one more index.
-        for i in 0..=n_inserted {
-            // If at the end or result[i] is larger than the current element,
-            // we have found the right spot:
-            if i == n_inserted || result[i] > elem {
-                // Insert the element at i,
-                // move the rest to higher indexes:
-                result.insert(i, elem);
+        while arr[j] > cur {
+            arr.swap(j + 1, j);
+            if j == 0 {
                 break;
             }
+            j -= 1;
         }
     }
-
-    result
 }
 
 #[cfg(test)]
 mod tests {
+    use super::super::is_sorted;
     use super::*;
 
     #[test]
     fn empty() {
-        let res = insertion_sort(&Vec::<u8>::new());
-        assert_eq!(res, vec![]);
+        let mut arr: [u8; 0] = [];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
     }
 
     #[test]
     fn one_element() {
-        let res = insertion_sort(&vec!["a"]);
-        assert_eq!(res, vec!["a"]);
+        let mut arr: [char; 1] = ['a'];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
     }
 
     #[test]
     fn already_sorted() {
-        let res = insertion_sort(&vec!["a", "b", "c"]);
-        assert_eq!(res, vec!["a", "b", "c"]);
+        let mut arr: [&str; 3] = ["a", "b", "c"];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
     }
 
     #[test]
     fn basic() {
-        let res = insertion_sort(&vec!["d", "a", "c", "b"]);
-        assert_eq!(res, vec!["a", "b", "c", "d"]);
+        let mut arr: [&str; 4] = ["d", "a", "c", "b"];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
     }
 
     #[test]
     fn odd_number_of_elements() {
-        let res = insertion_sort(&vec!["d", "a", "c", "e", "b"]);
-        assert_eq!(res, vec!["a", "b", "c", "d", "e"]);
+        let mut arr: Vec<&str> = vec!["d", "a", "c", "e", "b"];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
     }
 
     #[test]
     fn repeated_elements() {
-        let res = insertion_sort(&vec![542, 542, 542, 542]);
-        assert_eq!(res, vec![542, 542, 542, 542]);
+        let mut arr: Vec<usize> = vec![542, 542, 542, 542];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
     }
 }
