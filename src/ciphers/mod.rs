e5e0aefd9395cdfbeef33eae57525c3d2aadb93c
NashJames <37304960+NashJames@users.noreply.github.com>
2021-12-19 20:56:08 +0000

    add transposition cipher

--- src/ciphers/mod.rs
+++ src/ciphers/mod.rs
@@ -4,6 +4,7 @@ mod morse_code;
 mod polybius;
 mod rot13;
 mod sha256;
+mod transposition;
 mod vigenere;
 mod xor;
 
@@ -13,5 +14,6 @@ pub use self::morse_code::{decode, encode};
 pub use self::polybius::{decode_ascii, encode_ascii};
 pub use self::rot13::rot13;
 pub use self::sha256::sha256;
+pub use self::transposition::transposition;
 pub use self::vigenere::vigenere;
 pub use self::xor::xor;
