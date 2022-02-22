19a02b8634f80bcb37866c6fc5c0c60db4d2bf8e
fffzlfk <1319933925qq@gmail.com>
2022-02-22 23:00:34 +0800

    performance_improvement of math::prime_check

--- src/math/prime_check.rs
+++ src/math/prime_check.rs
@@ -1,13 +1,19 @@
 pub fn prime_check(num: usize) -> bool {
-    if (num > 1) & (num < 4) {
+    if num <= 1 {
+        return false;
+    }
+    if num <= 3 {
         return true;
-    } else if (num < 2) || (num % 2 == 0) {
+    }
+
+    // This is checked so that we can skip middle five numbers in blow loop
+    if num % 2 == 0 || num % 3 == 0 {
         return false;
     }
 
     let stop: usize = (num as f64).sqrt() as usize + 1;
-    for i in (3..stop).step_by(2) {
-        if num % i == 0 {
+    for i in (5..stop).step_by(6) {
+        if num % i == 0 || num % (i + 2) == 0 {
             return false;
         }
     }
