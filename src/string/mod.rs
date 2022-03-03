93594b013d2c0a0d63ce1c44385b4143e6d2a8a4
Erfan Khadem <erfankhademerkh@gmail.com>
2022-03-04 00:41:49 +0330

    Added Z Algorithm

--- src/string/mod.rs
+++ src/string/mod.rs
@@ -4,6 +4,7 @@ mod knuth_morris_pratt;
 mod manacher;
 mod rabin_karp;
 mod reverse;
+mod z_algorithm;
 
 pub use self::aho_corasick::AhoCorasick;
 pub use self::burrows_wheeler_transform::{
@@ -13,3 +14,5 @@ pub use self::knuth_morris_pratt::knuth_morris_pratt;
 pub use self::manacher::manacher;
 pub use self::rabin_karp::rabin_karp;
 pub use self::reverse::reverse;
+pub use self::z_algorithm::match_pattern;
+pub use self::z_algorithm::z_array;
