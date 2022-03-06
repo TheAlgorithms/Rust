244d3e4806947d78cf9f8b660fc96c72b179f1d6
fffzlfk <44939690+fffzlfk@users.noreply.github.com>
2022-03-06 15:04:48 +0800

    Add square root by Newtons method (#281)

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -5,6 +5,7 @@ mod pascal_triangle;
 mod perfect_numbers;
 mod prime_check;
 mod prime_numbers;
+mod square_root;
 mod trial_division;
 
 pub use self::extended_euclidean_algorithm::extended_euclidean_algorithm;
@@ -16,4 +17,5 @@ pub use self::pascal_triangle::pascal_triangle;
 pub use self::perfect_numbers::perfect_numbers;
 pub use self::prime_check::prime_check;
 pub use self::prime_numbers::prime_numbers;
+pub use self::square_root::square_root;
 pub use self::trial_division::trial_division;
