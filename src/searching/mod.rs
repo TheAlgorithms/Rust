749e8e1484bfff989a60117628ec9721853bf94a
DONSIMON92 <semenische@yandex.ocm>
2022-03-10 00:38:17 +0300

    add algorithm jump search

--- src/searching/mod.rs
+++ src/searching/mod.rs
@@ -1,9 +1,11 @@
 mod binary_search;
 mod binary_search_recursive;
+mod jump_search;
 mod kth_smallest;
 mod linear_search;
 
 pub use self::binary_search::binary_search;
 pub use self::binary_search_recursive::binary_search_rec;
+pub use self::jump_search::jump_search;
 pub use self::kth_smallest::kth_smallest;
 pub use self::linear_search::linear_search;
