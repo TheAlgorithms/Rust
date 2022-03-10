801053d41ddc7c1abb45803aea349c630cb9d28b
DONSIMON92 <semenische@yandex.ocm>
2022-03-10 19:43:53 +0300

    add algorithm jump exponential search

--- src/searching/mod.rs
+++ src/searching/mod.rs
@@ -1,11 +1,13 @@
 mod binary_search;
 mod binary_search_recursive;
+mod exponential_search;
 mod jump_search;
 mod kth_smallest;
 mod linear_search;
 
 pub use self::binary_search::binary_search;
 pub use self::binary_search_recursive::binary_search_rec;
+pub use self::exponential_search::exponential_search;
 pub use self::jump_search::jump_search;
 pub use self::kth_smallest::kth_smallest;
 pub use self::linear_search::linear_search;
