139659bdf07bb9115a6d16ff5dd0658023c0f25b
DONSIMON92 <semenische@yandex.ocm>
2022-03-11 13:53:39 +0300

    add exponential search

--- src/searching/mod.rs
+++ src/searching/mod.rs
@@ -1,5 +1,6 @@
 mod binary_search;
 mod binary_search_recursive;
+mod exponential_search;
 mod fibonacci_search;
 mod jump_search;
 mod kth_smallest;
@@ -7,6 +8,7 @@ mod linear_search;
 
 pub use self::binary_search::binary_search;
 pub use self::binary_search_recursive::binary_search_rec;
+pub use self::exponential_search::exponential_search;
 pub use self::fibonacci_search::fibonacci_search;
 pub use self::jump_search::jump_search;
 pub use self::kth_smallest::kth_smallest;
