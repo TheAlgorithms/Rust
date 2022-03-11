6770179f2c8b85434156da050f9dffebf1300c1b
DONSIMON92 <semenische@yandex.ocm>
2022-03-11 13:52:49 +0300

    add fibonacci search

--- src/searching/mod.rs
+++ src/searching/mod.rs
@@ -1,11 +1,13 @@
 mod binary_search;
 mod binary_search_recursive;
+mod fibonacci_search;
 mod jump_search;
 mod kth_smallest;
 mod linear_search;
 
 pub use self::binary_search::binary_search;
 pub use self::binary_search_recursive::binary_search_rec;
+pub use self::fibonacci_search::fibonacci_search;
 pub use self::jump_search::jump_search;
 pub use self::kth_smallest::kth_smallest;
 pub use self::linear_search::linear_search;
