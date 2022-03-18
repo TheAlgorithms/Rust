c3c35107d8935be4a5f552505adfdf8831273770
DONSIMON92 <semenische@protonmail.com>
2022-03-18 11:26:57 +0300

    add Sieve of Eratosthenes

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,12 +1,12 @@
 mod extended_euclidean_algorithm;
 mod fast_power;
 mod greatest_common_divisor;
-mod linear_sieve;
 mod miller_rabin;
 mod pascal_triangle;
 mod perfect_numbers;
 mod prime_check;
 mod prime_numbers;
+mod sieve_of_eratosthenes;
 mod square_root;
 mod trial_division;
 
@@ -15,11 +15,11 @@ pub use self::fast_power::fast_power;
 pub use self::greatest_common_divisor::{
     greatest_common_divisor_iterative, greatest_common_divisor_recursive,
 };
-pub use self::linear_sieve::LinearSieve;
 pub use self::miller_rabin::miller_rabin;
 pub use self::pascal_triangle::pascal_triangle;
 pub use self::perfect_numbers::perfect_numbers;
 pub use self::prime_check::prime_check;
 pub use self::prime_numbers::prime_numbers;
+pub use self::sieve_of_eratosthenes::sieve_of_eratosthenes;
 pub use self::square_root::square_root;
 pub use self::trial_division::trial_division;
