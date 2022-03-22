d8cf9042985a259486390e0e59b12d4f707b2a7d
saybarsnazlica <saybarsnazlica@gmail.com>
2022-03-22 13:52:42 +0900

    Add use declaration for hamming_distance

--- src/string/mod.rs
+++ src/string/mod.rs
@@ -1,11 +1,11 @@
 mod aho_corasick;
 mod burrows_wheeler_transform;
-mod hamming_distance;
 mod knuth_morris_pratt;
 mod manacher;
 mod rabin_karp;
 mod reverse;
 mod z_algorithm;
+mod hamming_distance;
 
 pub use self::aho_corasick::AhoCorasick;
 pub use self::burrows_wheeler_transform::{
@@ -17,3 +17,4 @@ pub use self::rabin_karp::rabin_karp;
 pub use self::reverse::reverse;
 pub use self::z_algorithm::match_pattern;
 pub use self::z_algorithm::z_array;
+pub use self::hamming_distance::hamming_distance;
