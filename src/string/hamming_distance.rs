b46052f72bbfc7a6393efb8954d4dc1669d3ad84
saybarsnazlica <saybarsnazlica@gmail.com>
2022-03-23 15:21:45 +0900

    Modify match arms

--- src/string/hamming_distance.rs
+++ src/string/hamming_distance.rs
@@ -6,9 +6,10 @@ pub fn hamming_distance(string1: &str, string2: &str) -> usize {
     loop {
         match (string1.next(), string2.next()) {
             (Some(char1), Some(char2)) if char1 != char2 => distance += 1,
+            (Some(char1), Some(char2)) if char1 == char2 => continue,
             (None, Some(_)) | (Some(_), None) => panic!("Strings must have the same length"),
             (None, None) => break,
-            _ => continue,
+            _ => unreachable!(),
         }
     }
     distance
@@ -19,7 +20,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn empty() {
+    fn empty_strings() {
         let result = hamming_distance("", "");
         assert_eq!(result, 0);
     }
