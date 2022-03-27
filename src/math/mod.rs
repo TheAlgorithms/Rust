2d3cbf6b57c6c8cacb83bb19d0ef6fbd8cf5b627
Saksham Madan <43085346+Hawk453@users.noreply.github.com>
2022-03-27 13:38:03 +0530

    update

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,8 +1,8 @@
 mod extended_euclidean_algorithm;
 mod fast_power;
 mod greatest_common_divisor;
-mod nthprime;
 mod miller_rabin;
+mod nthprime;
 mod pascal_triangle;
 mod perfect_numbers;
 mod prime_check;
@@ -15,8 +15,8 @@ pub use self::fast_power::fast_power;
 pub use self::greatest_common_divisor::{
     greatest_common_divisor_iterative, greatest_common_divisor_recursive,
 };
-pub use self::nthprime::nthprime;
 pub use self::miller_rabin::miller_rabin;
+pub use self::nthprime::nthprime;
 pub use self::pascal_triangle::pascal_triangle;
 pub use self::perfect_numbers::perfect_numbers;
 pub use self::prime_check::prime_check;
