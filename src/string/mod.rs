709b93707078ad87e0774b9f7f94399b7448575b
saybarsnazlica <saybarsnazlica@gmail.com>
2022-03-30 00:04:28 +0900

    Fix cargo fmt check

--- src/string/mod.rs
+++ src/string/mod.rs
@@ -1,20 +1,20 @@
 mod aho_corasick;
 mod burrows_wheeler_transform;
+mod hamming_distance;
 mod knuth_morris_pratt;
 mod manacher;
 mod rabin_karp;
 mod reverse;
 mod z_algorithm;
-mod hamming_distance;
 
 pub use self::aho_corasick::AhoCorasick;
 pub use self::burrows_wheeler_transform::{
     burrows_wheeler_transform, inv_burrows_wheeler_transform,
 };
+pub use self::hamming_distance::hamming_distance;
 pub use self::knuth_morris_pratt::knuth_morris_pratt;
 pub use self::manacher::manacher;
 pub use self::rabin_karp::rabin_karp;
 pub use self::reverse::reverse;
 pub use self::z_algorithm::match_pattern;
 pub use self::z_algorithm::z_array;
-pub use self::hamming_distance::hamming_distance;
