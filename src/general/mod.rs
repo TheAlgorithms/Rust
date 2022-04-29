10f1fe8334193ecc27675994ecd6b1ff33d6aa4a
Erfan Khadem <erfankhademerkh@gmail.com>
2022-04-29 23:20:29 +0430

    Add Huffman Encoding

--- src/general/mod.rs
+++ src/general/mod.rs
@@ -3,6 +3,7 @@ mod hanoi;
 mod kmeans;
 mod nqueens;
 mod two_sum;
+mod huffman_encoding;
 
 pub use self::convex_hull::convex_hull_graham;
 pub use self::hanoi::hanoi;
@@ -10,3 +11,4 @@ pub use self::kmeans::f32::kmeans as kmeans_f32;
 pub use self::kmeans::f64::kmeans as kmeans_f64;
 pub use self::nqueens::nqueens;
 pub use self::two_sum::two_sum;
+pub use self::huffman_encoding::{HuffmanEncoding, HuffmanDictionary};
