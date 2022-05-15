7ab20fd6b9c424be0f19c02dfa3551318b78bb0a
Leon Amtmann <leon@amtmann.de>
2022-05-15 23:09:00 +0200

    Added gaussian elimination

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -3,6 +3,7 @@ mod baby_step_giant_step;
 mod extended_euclidean_algorithm;
 mod fast_fourier_transform;
 mod fast_power;
+mod gaussian_elimination;
 mod gcd_of_n_numbers;
 mod greatest_common_divisor;
 mod karatsuba_multiplication;
@@ -23,6 +24,7 @@ mod square_root;
 mod trial_division;
 mod zellers_congruence_algorithm;
 
+
 pub use self::armstrong_number::is_armstrong_number;
 pub use self::baby_step_giant_step::baby_step_giant_step;
 pub use self::extended_euclidean_algorithm::extended_euclidean_algorithm;
@@ -31,6 +33,7 @@ pub use self::fast_fourier_transform::{
     inverse_fast_fourier_transform,
 };
 pub use self::fast_power::fast_power;
+pub use self::gaussian_elimination::gaussian_elimination;
 pub use self::gcd_of_n_numbers::gcd;
 pub use self::greatest_common_divisor::{
     greatest_common_divisor_iterative, greatest_common_divisor_recursive,
