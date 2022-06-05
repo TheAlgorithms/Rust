f391b87398273d866f31f52ccfbaf6220bba7a7c
doki <tunyum94@gmail.com>
2022-06-05 15:03:05 +0800

    custom function for segment tree

--- src/data_structures/segment_tree.rs
+++ src/data_structures/segment_tree.rs
@@ -1,27 +1,19 @@
 /// This stucture implements a segmented tree that
 /// can efficiently answer range queries on arrays.
-pub struct SegmentTree<T: Default + Ord + Copy> {
+pub struct SegmentTree<T: Default + Ord + Copy, F: Fn(T, T) -> T> {
     len: usize,
     buf: Vec<T>,
-    op: Ops,
+    op: F,
 }
 
-pub enum Ops {
-    Max,
-    Min,
-}
-
-impl<T: Default + Ord + Copy> SegmentTree<T> {
+impl<T: Default + Ord + Copy, F: Fn(T, T) -> T> SegmentTree<T, F> {
     /// function to build the tree
-    pub fn from_vec(arr: &[T], op: Ops) -> Self {
+    pub fn from_vec(arr: &[T], op: F) -> Self {
         let len = arr.len();
         let mut buf: Vec<T> = vec![T::default(); 2 * len];
         buf[len..(len + len)].clone_from_slice(&arr[0..len]);
         for i in (1..len).rev() {
-            buf[i] = match op {
-                Ops::Max => buf[2 * i].max(buf[2 * i + 1]),
-                Ops::Min => buf[2 * i].min(buf[2 * i + 1]),
-            };
+            buf[i] = op(buf[2 * i], buf[2 * i + 1]);
         }
         SegmentTree { len, buf, op }
     }
@@ -33,17 +25,11 @@ impl<T: Default + Ord + Copy> SegmentTree<T> {
         let mut res = self.buf[l];
         while l <= r {
             if l % 2 == 1 {
-                res = match self.op {
-                    Ops::Max => res.max(self.buf[l]),
-                    Ops::Min => res.min(self.buf[l]),
-                };
+                res = (self.op)(res, self.buf[l]);
                 l += 1;
             }
             if r % 2 == 0 {
-                res = match self.op {
-                    Ops::Max => res.max(self.buf[r]),
-                    Ops::Min => res.min(self.buf[r]),
-                };
+                res = (self.op)(res, self.buf[r]);
                 r -= 1;
             }
             l /= 2;
@@ -59,10 +45,7 @@ impl<T: Default + Ord + Copy> SegmentTree<T> {
         idx /= 2;
 
         while idx != 0 {
-            self.buf[idx] = match self.op {
-                Ops::Max => self.buf[2 * idx].max(self.buf[2 * idx + 1]),
-                Ops::Min => self.buf[2 * idx].min(self.buf[2 * idx + 1]),
-            };
+            self.buf[idx] = (self.op)(self.buf[2 * idx], self.buf[2 * idx + 1]);
             idx /= 2;
         }
     }
@@ -75,10 +58,12 @@ mod tests {
     #[test]
     fn it_works() {
         let vec = vec![1, 2, -4, 7, 3, -5, 6, 11, -20, 9, 14, 15, 5, 2, -8];
-        let min_seg_tree = SegmentTree::from_vec(&vec, Ops::Min);
+        let min_fn = |x1: i32, x2: i32| x1.min(x2);
+        let max_fn = |x1: i32, x2: i32| x1.max(x2);
+        let min_seg_tree = SegmentTree::from_vec(&vec, min_fn);
         assert_eq!(-5, min_seg_tree.query(4, 6));
         assert_eq!(-20, min_seg_tree.query(0, vec.len() - 1));
-        let mut max_seg_tree = SegmentTree::from_vec(&vec, Ops::Max);
+        let mut max_seg_tree = SegmentTree::from_vec(&vec, max_fn);
         assert_eq!(6, max_seg_tree.query(4, 6));
         assert_eq!(15, max_seg_tree.query(0, vec.len() - 1));
         max_seg_tree.update(6, 8);
