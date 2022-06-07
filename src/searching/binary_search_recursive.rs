23fb75c1fd930a983cf897344f2a67a7062e8891
Raj Mazumder <rajmazumder27.08.2001@gmail.com>
2022-06-07 11:42:21 +0530

    fixed binary search for descending order array

--- src/searching/binary_search_recursive.rs
+++ src/searching/binary_search_recursive.rs
@@ -10,11 +10,22 @@ pub fn binary_search_rec<T: Ord>(
         return None;
     }
 
+    let is_asc = list_of_items[0] < list_of_items[list_of_items.len() - 1];
+
     let middle: usize = left + (right - left) / 2;
-    match target.cmp(&list_of_items[middle]) {
-        Ordering::Less => binary_search_rec(list_of_items, target, left, &middle),
-        Ordering::Greater => binary_search_rec(list_of_items, target, &(middle + 1), right),
-        Ordering::Equal => Some(middle),
+
+    if is_asc {
+        match target.cmp(&list_of_items[middle]) {
+            Ordering::Less => binary_search_rec(list_of_items, target, left, &middle),
+            Ordering::Greater => binary_search_rec(list_of_items, target, &(middle + 1), right),
+            Ordering::Equal => Some(middle),
+        }
+    } else {
+        match target.cmp(&list_of_items[middle]) {
+            Ordering::Less => binary_search_rec(list_of_items, target, &(middle + 1), right),
+            Ordering::Greater => binary_search_rec(list_of_items, target, left, &middle),
+            Ordering::Equal => Some(middle),
+        }
     }
 }
 
@@ -43,7 +54,7 @@ mod tests {
     }
 
     #[test]
-    fn success_search_strings() {
+    fn success_search_strings_asc() {
         let say_hello_list = vec!["hi", "olá", "salut"];
         let right = say_hello_list.len();
         assert_eq!(
@@ -57,7 +68,21 @@ mod tests {
     }
 
     #[test]
-    fn fail_search_strings() {
+    fn success_search_strings_desc() {
+        let say_hello_list = vec!["salut", "olá", "hi"];
+        let right = say_hello_list.len();
+        assert_eq!(
+            binary_search_rec(&say_hello_list, &"hi", &LEFT, &right),
+            Some(2)
+        );
+        assert_eq!(
+            binary_search_rec(&say_hello_list, &"salut", &LEFT, &right),
+            Some(0)
+        );
+    }
+
+    #[test]
+    fn fail_search_strings_asc() {
         let say_hello_list = vec!["hi", "olá", "salut"];
         for target in &["adiós", "你好"] {
             assert_eq!(
@@ -68,8 +93,30 @@ mod tests {
     }
 
     #[test]
-    fn success_search_integers() {
+    fn fail_search_strings_desc() {
+        let say_hello_list = vec!["salut", "olá", "hi"];
+        for target in &["adiós", "你好"] {
+            assert_eq!(
+                binary_search_rec(&say_hello_list, target, &LEFT, &say_hello_list.len()),
+                None
+            );
+        }
+    }
+
+    #[test]
+    fn success_search_integers_asc() {
         let integers = vec![0, 10, 20, 30, 40, 50, 60, 70, 80, 90];
+        for (index, target) in integers.iter().enumerate() {
+            assert_eq!(
+                binary_search_rec(&integers, target, &LEFT, &integers.len()),
+                Some(index)
+            )
+        }
+    }
+
+    #[test]
+    fn success_search_integers_desc() {
+        let integers = vec![90, 80, 70, 60, 50, 40, 30, 20, 10, 0];
         for (index, target) in integers.iter().enumerate() {
             assert_eq!(
                 binary_search_rec(&integers, target, &LEFT, &integers.len()),
@@ -84,28 +131,6 @@ mod tests {
         for target in &[100, 444, 336] {
             assert_eq!(
                 binary_search_rec(&integers, target, &LEFT, &integers.len()),
-                None
-            );
-        }
-    }
-
-    #[test]
-    fn fail_search_unsorted_strings_list() {
-        let unsorted_strings = vec!["salut", "olá", "hi"];
-        for target in &["hi", "salut"] {
-            assert_eq!(
-                binary_search_rec(&unsorted_strings, target, &LEFT, &unsorted_strings.len()),
-                None
-            );
-        }
-    }
-
-    #[test]
-    fn fail_search_unsorted_integers_list() {
-        let unsorted_integers = vec![90, 80, 70, 60, 50, 40, 30, 20, 10, 0];
-        for target in &[0, 80, 90] {
-            assert_eq!(
-                binary_search_rec(&unsorted_integers, target, &LEFT, &unsorted_integers.len()),
                 None
             );
         }
