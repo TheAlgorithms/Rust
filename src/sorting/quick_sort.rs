de9ec0b18581af29c2875f95bb962d2a0ccf40f7
ahmed-deftoner <ahmedghtwhts786@gmail.com>
2022-09-06 20:53:58 +0500

    added tests

--- src/sorting/quick_sort.rs
+++ src/sorting/quick_sort.rs
@@ -1,5 +1,10 @@
 use std::cmp::PartialOrd;
 
+// Partition uses the last element as a pivot.
+// Compares elements with pivot in each iteration.
+// Swaps the values whenever an element smaller than pivot is encountered.
+// After iterating, it places the pivot in correct position.
+// Returns the correct position of pivot 
 pub fn partition<T: PartialOrd>(arr: &mut [T], lo: isize, hi: isize) -> isize {
     let pivot = hi as usize;
     let mut i = lo - 1;
@@ -23,6 +28,9 @@ pub fn partition<T: PartialOrd>(arr: &mut [T], lo: isize, hi: isize) -> isize {
     arr.swap(i as usize, pivot as usize);
     i
 }
+
+// Divides array based on a partition index
+// All consequent partitions are sorted in a divide and conquer fashion
 fn _quick_sort<T: Ord>(arr: &mut [T], lo: isize, hi: isize) {
     if lo < hi {
         let p = partition(arr, lo, hi);
@@ -30,7 +38,59 @@ fn _quick_sort<T: Ord>(arr: &mut [T], lo: isize, hi: isize) {
         _quick_sort(arr, p + 1, hi);
     }
 }
+
 pub fn quick_sort<T: Ord>(arr: &mut [T]) {
     let len = arr.len();
-    _quick_sort(arr, 0, (len - 1) as isize);
+    if len > 1 {
+        _quick_sort(arr, 0, (len - 1) as isize);
+    }
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        let mut res = vec![10, 8, 4, 3, 1, 9, 2, 7, 5, 6];
+        quick_sort(&mut res);
+        assert_eq!(res, vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }
+
+    #[test]
+    fn basic_string() {
+        let mut res = vec!["a", "bb", "d", "cc"];
+        quick_sort(&mut res);
+        assert_eq!(res, vec!["a", "bb", "cc", "d"]);
+    }
+
+    #[test]
+    fn empty() {
+        let mut res = Vec::<u8>::new();
+        quick_sort(&mut res);
+        assert_eq!(res, vec![]);
+    }
+
+    #[test]
+    fn one_element() {
+        let mut res = vec![1];
+        quick_sort(&mut res);
+        assert_eq!(res, vec![1]);
+    }
+
+    #[test]
+    fn pre_sorted() {
+        let mut res = vec![1, 2, 3, 4];
+        quick_sort(&mut res);
+        assert_eq!(res, vec![1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn reverse_sorted() {
+        let mut res = vec![4, 3, 2, 1];
+        quick_sort(&mut res);
+        assert_eq!(res, vec![1, 2, 3, 4]);
+    }
+}
+
