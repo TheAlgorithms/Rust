0dfdd5d3eb0f05f0561f7859f8c900ee709f5547
Gent Binaku <gbinaku@partners.accedian.com>
2022-09-20 13:01:03 +0200

    Resolved mods.rs

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -1,3 +1,4 @@
+mod amicable_numbers;
 mod armstrong_number;
 mod baby_step_giant_step;
 mod chinese_remainder_theorem;
@@ -30,6 +31,8 @@ mod square_root;
 mod trial_division;
 mod zellers_congruence_algorithm;
 
+
+pub use self::amicable_numbers::amicable_pairs_under_n;
 pub use self::armstrong_number::is_armstrong_number;
 pub use self::baby_step_giant_step::baby_step_giant_step;
 pub use self::chinese_remainder_theorem::chinese_remainder_theorem;
