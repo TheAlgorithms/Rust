51d152d6a55eb380b14dbc986d9ec1d1b0f54e8c
jerr-it <jrgls.no-reply@gmx.de>
2022-09-29 14:32:10 +0200

    Added palindrome check

--- src/string/mod.rs
+++ src/string/mod.rs
@@ -3,6 +3,7 @@ mod burrows_wheeler_transform;
 mod hamming_distance;
 mod knuth_morris_pratt;
 mod manacher;
+mod palindrome;
 mod rabin_karp;
 mod reverse;
 mod run_length_encoding;
@@ -15,6 +16,7 @@ pub use self::burrows_wheeler_transform::{
 pub use self::hamming_distance::hamming_distance;
 pub use self::knuth_morris_pratt::knuth_morris_pratt;
 pub use self::manacher::manacher;
+pub use self::palindrome::is_palindrome;
 pub use self::rabin_karp::rabin_karp;
 pub use self::reverse::reverse;
 pub use self::run_length_encoding::{run_length_decoding, run_length_encoding};
