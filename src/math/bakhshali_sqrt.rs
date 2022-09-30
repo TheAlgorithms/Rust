a38c55e2e48545fa98b54ff4eb008b1e08d7569c
archuser555 <98958302+archuser555@users.noreply.github.com>
2022-09-30 19:36:56 +0100

    Adding More Test's
    Yeah...

--- src/math/bakhshali_sqrt.rs
+++ src/math/bakhshali_sqrt.rs
@@ -1,8 +1,7 @@
-//Bakhshali Square Root Algorithm
+//Bakhshali Sqrt Algorithm
 
 //support f32 so please use 10.0 instead of 10
-//this is an ancient indian way to calculate square root, so don't wait very exact result
-
+//this is an ancient indian way to calculate square root
 pub fn bakhshali_sqrt(x: f32) -> f32 {
     let mut res: f32;
     let mut a: f32;
@@ -10,7 +9,9 @@ pub fn bakhshali_sqrt(x: f32) -> f32 {
 
     if (x == 0.0) || (x == 1.0) {
         return x;
-    } else if x < 0.0 {
+    }
+    
+    else if x < 0.0 {
         return f32::NAN;
     }
 
@@ -38,7 +39,16 @@ mod tests {
     #[test]
     pub fn test_bakhshali_sqrt() {
         // let's try get the sqrt of 10
-        let a: f32 = bakhshali_sqrt(9.0);
-        assert_eq!(a, 3.0); //Sqrt Of 9 Is 3
+        let a1: f32 = bakhshali_sqrt(9.0);
+        assert_eq!(a1, 3.0); //Sqrt Of 9 Is 3
+
+        let a2: f32 = bakhshali_sqrt(0.0);
+        assert_eq!(a2, 0.0); //Sqrt Of 0 Is 0
+
+        //let a3: f32 = bakhshali_sqrt(-1.0);
+        //assert_eq!(a3, f32::NAN); //I Get Error Here, Because In Rust, NAN != NAN For Unknow Reason... But It's True So...
+
+        let a4: f32 = bakhshali_sqrt(0.5);
+        assert_eq!(a4, 0.70710677); //Sqrt Of 0.5 Is 0.70710677
     }
 }
