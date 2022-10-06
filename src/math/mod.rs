30cb8ae6ae7c5a3f3ef11117983a96f2375cd07f
Utshaan <Utkarsh.thanvi@gmail.com>
2022-10-06 16:46:02 +0530

    Added collatz_sequence.rs

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -2,6 +2,7 @@ mod amicable_numbers;
 mod armstrong_number;
 mod baby_step_giant_step;
 mod chinese_remainder_theorem;
+mod collatz_sequence;
 mod extended_euclidean_algorithm;
 mod fast_fourier_transform;
 mod fast_power;
@@ -36,6 +37,7 @@ pub use self::amicable_numbers::amicable_pairs_under_n;
 pub use self::armstrong_number::is_armstrong_number;
 pub use self::baby_step_giant_step::baby_step_giant_step;
 pub use self::chinese_remainder_theorem::chinese_remainder_theorem;
+pub use self::collatz_sequence::sequence;
 pub use self::extended_euclidean_algorithm::extended_euclidean_algorithm;
 pub use self::fast_fourier_transform::{
     fast_fourier_transform, fast_fourier_transform_input_permutation,
