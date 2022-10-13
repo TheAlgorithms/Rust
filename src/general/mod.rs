cfa131b12eb4c59e15daa78d0d2f7c0aa3cc665b
Arjun31415 <arjunp0710@gmail.com>
2022-10-13 13:05:40 +0530

    Add Minimum Excluded Element (#404)

--- src/general/mod.rs
+++ src/general/mod.rs
@@ -3,14 +3,16 @@ mod fisher_yates_shuffle;
 mod hanoi;
 mod huffman_encoding;
 mod kmeans;
+mod mex;
 mod nqueens;
 mod two_sum;
-
 pub use self::convex_hull::convex_hull_graham;
 pub use self::fisher_yates_shuffle::fisher_yates_shuffle;
 pub use self::hanoi::hanoi;
 pub use self::huffman_encoding::{HuffmanDictionary, HuffmanEncoding};
 pub use self::kmeans::f32::kmeans as kmeans_f32;
 pub use self::kmeans::f64::kmeans as kmeans_f64;
+pub use self::mex::mex_using_set;
+pub use self::mex::mex_using_sort;
 pub use self::nqueens::nqueens;
 pub use self::two_sum::two_sum;
