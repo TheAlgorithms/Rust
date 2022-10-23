96398b11b01b26ed5d1e8f0a092f0eb7a80aef86
kazuyoshi-tech <kazuyoshi.kyushu@gmail.com>
2022-10-23 11:03:08 +0900

    rustfmt

--- src/math/ceil.rs
+++ src/math/ceil.rs
@@ -2,45 +2,45 @@
 // Source: https://en.wikipedia.org/wiki/Floor_and_ceiling_functions
 
 pub fn ceil(x: f64) -> f64 {
-  let x_round = x.round();
-  if (x_round * 10.0).round() < (x * 10.0).round() {
-    x_round + 1.0
-  } else {
-    x_round
-  }
+    let x_round = x.round();
+    if (x_round * 10.0).round() < (x * 10.0).round() {
+        x_round + 1.0
+    } else {
+        x_round
+    }
 }
 
 #[cfg(test)]
 mod tests {
-  use super::*;
-
-  #[test]
-  fn positive_decimal() {
-    let num = 1.10;
-    assert_eq!(ceil(num), num.ceil());
-  }
-
-  #[test]
-  fn positive_integer() {
-    let num = 1.00;
-    assert_eq!(ceil(num), num.ceil());
-  }
-
-  #[test]
-  fn negative_decimal() {
-    let num = -1.10;
-    assert_eq!(ceil(num), num.ceil());
-  }
-
-  #[test]
-  fn negative_integer() {
-    let num = -1.00;
-    assert_eq!(ceil(num), num.ceil());
-  }
+    use super::*;
 
     #[test]
-  fn zero() {
-    let num = 0.00;
-    assert_eq!(ceil(num), num.ceil());
-  }
+    fn positive_decimal() {
+        let num = 1.10;
+        assert_eq!(ceil(num), num.ceil());
+    }
+
+    #[test]
+    fn positive_integer() {
+        let num = 1.00;
+        assert_eq!(ceil(num), num.ceil());
+    }
+
+    #[test]
+    fn negative_decimal() {
+        let num = -1.10;
+        assert_eq!(ceil(num), num.ceil());
+    }
+
+    #[test]
+    fn negative_integer() {
+        let num = -1.00;
+        assert_eq!(ceil(num), num.ceil());
+    }
+
+    #[test]
+    fn zero() {
+        let num = 0.00;
+        assert_eq!(ceil(num), num.ceil());
+    }
 }
