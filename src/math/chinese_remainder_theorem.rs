63d39db28e964ab3eb0ae6e567abc707c3a042b8
Gent Binaku <gbinaku@partners.accedian.com>
2022-10-26 14:52:14 +0200

    Adding suppport for u64 and BigInt

--- src/math/chinese_remainder_theorem.rs
+++ src/math/chinese_remainder_theorem.rs
@@ -1,7 +1,25 @@
-use super::extended_euclidean_algorithm;
+use num_bigint::BigInt;
+use num_integer::Integer;
+use num_traits::{One, Zero};
 
-fn mod_inv(x: i32, n: i32) -> Option<i32> {
-    let (g, x, _) = extended_euclidean_algorithm(x, n);
+fn gcd(a: u64, b: u64) -> u64 {
+    if a == 0 {
+        return b;
+    }
+    if b == 0 {
+        return a;
+    }
+    if a == 0 && b == 0 {
+        return 0;
+    }
+    if b > a {
+        gcd(b, a)
+    } else {
+        gcd(b, a % b)
+    }
+}
+fn modinverse(x: u64, n: u64) -> Option<u64> {
+    let g = gcd(x, n);
     if g == 1 {
         Some((x % n + n) % n)
     } else {
@@ -9,14 +27,35 @@ fn mod_inv(x: i32, n: i32) -> Option<i32> {
     }
 }
 
-pub fn chinese_remainder_theorem(residues: &[i32], modulli: &[i32]) -> Option<i32> {
-    let prod = modulli.iter().product::<i32>();
+fn xmodinverse(x: &BigInt, n: &BigInt) -> Option<BigInt> {
+    let g = x.gcd(n);
+    if g == BigInt::one() {
+        Some((x % n + n) % n)
+    } else {
+        None
+    }
+}
+pub fn chinese_remainder_theorem(residues: &[u64], modulli: &[u64]) -> Option<u64> {
+    let prod = modulli.iter().product::<u64>();
 
     let mut sum = 0;
 
-    for (&residue, &modulus) in residues.iter().zip(modulli) {
+    for (residue, modulus) in residues.iter().zip(modulli) {
         let p = prod / modulus;
-        sum += residue * mod_inv(p, modulus)? * p
+        let d = modinverse(p, *modulus)?;
+        sum += residue * d * p;
+    }
+    Some(sum % prod)
+}
+pub fn xchinese_remainder_theorem(residues: &[BigInt], modulli: &[BigInt]) -> Option<BigInt> {
+    let prod: BigInt = modulli.iter().product();
+
+    let mut sum: BigInt = BigInt::zero();
+
+    for (residue, modulus) in residues.iter().zip(modulli) {
+        let p = prod.to_owned() / modulus;
+        let d = xmodinverse(&p, &*modulus)?;
+        sum += residue * d * p
     }
     Some(sum % prod)
 }
@@ -24,12 +63,34 @@ pub fn chinese_remainder_theorem(residues: &[i32], modulli: &[i32]) -> Option<i3
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn basic() {
         assert_eq!(chinese_remainder_theorem(&[3, 5, 7], &[2, 3, 1]), Some(5));
-        assert_eq!(chinese_remainder_theorem(&[1, 4, 6], &[3, 5, 7]), Some(34));
         assert_eq!(chinese_remainder_theorem(&[1, 4, 6], &[1, 2, 0]), None);
         assert_eq!(chinese_remainder_theorem(&[2, 5, 7], &[6, 9, 15]), None);
+    }
+    #[test]
+    fn xbasic() {
+        assert_eq!(
+            xchinese_remainder_theorem(
+                &[BigInt::from(3), BigInt::from(5), BigInt::from(7)],
+                &[BigInt::from(2), BigInt::from(3), BigInt::from(1)]
+            ),
+            Some(BigInt::from(5))
+        );
+        assert_eq!(
+            xchinese_remainder_theorem(
+                &[BigInt::from(1), BigInt::from(4), BigInt::from(6)],
+                &[BigInt::from(1), BigInt::from(2), BigInt::from(0)]
+            ),
+            None
+        );
+        assert_eq!(
+            xchinese_remainder_theorem(
+                &[BigInt::from(2), BigInt::from(5), BigInt::from(7)],
+                &[BigInt::from(6), BigInt::from(9), BigInt::from(15)]
+            ),
+            None
+        );
     }
 }
