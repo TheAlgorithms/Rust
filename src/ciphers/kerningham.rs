7e55adcbe63a65cede98700c80254425fc1f4ee3
Invader Zim <85027668+zim0369@users.noreply.github.com>
2022-10-30 13:51:27 +0530

    Add Brian Kerningham's algorithm to count set bits
    I'm not aware what more files I have to edit. Can anyone please tell me?

---
+++ src/ciphers/kerningham.rs
@@ -0,0 +1,23 @@
+pub fn kerningham(n: u32) -> i32 {
+    let mut count = 0;
+    let mut n = n;
+
+    while n > 0 {
+        n = n & (n - 1);
+        count += 1;
+    }
+
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn count_set_bits() {
+        assert_eq!(kerningham(0b_00000000000000000000000000001011), 3);
+        assert_eq!(kerningham(0b_00000000000000000000000010000000), 1);
+        assert_eq!(kerningham(0b_11111111111111111111111111111101), 31);
+    }
+}
