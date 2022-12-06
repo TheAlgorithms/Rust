fb377b33096683e09d023de55c704b22d42b8d43
SpiderMath <71999854+SpiderMath@users.noreply.github.com>
2022-12-07 00:50:14 +0530

    Switched from integers to float

--- src/math/abs.rs
+++ src/math/abs.rs
@@ -1,12 +1,12 @@
 /// This function returns the absolute value of a number.
 /// The absolute value of a number is the non-negative value of the number, regardless of its sign
 /// Wikipedia: https://en.wikipedia.org/wiki/Absolute_value
-pub fn abs(num: i64) -> u64 {
-    if num < 0 {
-        return -num as u64;
+pub fn abs(num: f64) -> f64 {
+    if num < 0.0 {
+        return -num;
     }
 
-    num as u64
+    num
 }
 
 #[cfg(test)]
@@ -15,16 +15,16 @@ mod test {
 
     #[test]
     fn negative_number() {
-        assert_eq!(420, abs(-420));
+        assert_eq!(420.0, abs(-420.0));
     }
 
     #[test]
     fn zero() {
-        assert_eq!(0, abs(0));
+        assert_eq!(0.0, abs(0.0));
     }
 
     #[test]
     fn positive_number() {
-        assert_eq!(69, abs(69));
+        assert_eq!(69.69, abs(69.69));
     }
 }
