810c7f609a7fe232059a218af7338aca50c846fd
agatekartik <agatekartik@gmail.com>
2023-03-26 16:29:08 +0530

    use idiomatic code in is_sorted()

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -58,21 +58,7 @@ pub fn is_sorted<T>(arr: &[T]) -> bool
 where
     T: cmp::PartialOrd,
 {
-    if arr.is_empty() {
-        return true;
-    }
-
-    let mut prev = &arr[0];
-
-    for item in arr.iter().skip(1) {
-        if prev > item {
-            return false;
-        }
-
-        prev = item;
-    }
-
-    true
+    arr.windows(2).all(|w| w[0] <= w[1])
 }
 
 #[cfg(test)]
