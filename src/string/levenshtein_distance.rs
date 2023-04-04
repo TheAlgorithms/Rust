b00aeb6590f23045613be85a3b608c181bbe1eb7
aesteve <arnaud.esteve@gmail.com>
2023-04-04 19:37:05 +0200

    Rework Levenshtein distance with more explanations

--- src/string/levenshtein_distance.rs
+++ src/string/levenshtein_distance.rs
@@ -1,41 +1,82 @@
 use std::cmp::min;
 
+// Let's imagine the distance between FROG and DOG (string1 = "FROG", string2 = "DOG")
+// The distance would be 2: Keep 'OG', remove F, and substitute 'R' for a 'D'
+//
+// In order to compute the distance, we are going to build the following matrix:
+//     |  ∅  |  F  |  R  |  O  |  G  |
+//  ∅  |  0  |  1  |  2  |  3  |  4  |
+//  D  |  1  |     |     |     |     |
+//  O  |  2  |     |     |     |     |
+//  G  |  3  |     |     |     |     |
+// Where:
+//  * ∅ indicates an empty String
+//  * each cell (i, j) in the matrix indicates the minimum distance between substrings string1[0..=j] and string2[0..=i]
+//      * (but considering we start with empty strings)
+// Above, we have filled the defaults:
+//   -> The distance between any string and an empty string is the string length
+// For example: the distance between String "F" and an empty String is 1, dist("FR", ∅) = 2, etc.
+// In the same fashion, vertically: dist("DOG", ∅) is 3
+// What we are interested in, is the last value at the bottom right of the matrix, which indicates dist("FROG", "DOG")
+//
+// How do we compute each cell in the matrix?
+//  -> Let's do this top-to-bottom, left-to-right
+// At each step we can either:
+//      * insert a char
+//      * delete a char
+//      * substitute a char
+// Say we're evaluating cell at index `(3, 2)` <=> `dist("FR", "DOG")`
+//  * "Inserting one char" means incrementing by one `dist("F", "DOG")` (cell at index: `(3, 1)`)
+//  * "Deleting one char" means incrementing by one `dist("FR", "DO")` (cell at index: `(2, 2)`)
+//  * "Substituting one char" means incrementing by one `dist("F", "DO")` (cell at index: `(2, 1)` if and only if chars differ (here 'R' != 'G'), otherwise we can just keep that distance
+// This would give the following:
+//     |  ∅  |  F  |  R  |  O  |  G  |
+//  ∅  |  0  |  1  |  2  |  3  |  4  |
+//  D  |  1  |  1  |  2  |  3  |  4  |
+//  O  |  2  |  2  |  2  |  2  |  3  |
+//  G  |  3  |  3  |  3  |  3  |  2  |
+//
+// This means when we evaluate a cell we only need the current row (the previous value), and the previous row.
+// We therefore don't need to keep the full matrix in memory, but only previous row
 pub fn levenshtein_distance(string1: &str, string2: &str) -> usize {
     if string1.is_empty() {
         return string2.len();
     }
+    let l1 = string1.len();
 
-    let mut d = Vec::with_capacity(string1.len());
-    for i in 0..=string1.len() {
-        d.push(i);
-    }
+    // Let's start by building the first row, in the example:
+    //     |  ∅  |  F  |  R  |  O  |  G  |
+    //  ∅  |  0  |  1  |  2  |  3  |  4  |
+    let mut prev_dist: Vec<usize> = (0..=l1).collect();
 
-    let mut j = 1;
-    for c2 in string2.chars() {
-        let mut previous_substitution_cost = d[0];
-        d[0] = j;
+    for (row, c2) in string2.chars().enumerate() {
+        // row starts at 0
+        let mut prev_substitution_cost = prev_dist[0]; // we'll keep a reference to cell (i-1, j-1)
+        prev_dist[0] = row + 1; // diff with empty string, since `row` starts at 0, it's `row + 1`
 
-        let mut i = 1;
-        for c1 in string1.chars() {
-            let deletion_cost = d[i - 1] + 1;
-            let insertion_cost = d[i] + 1;
-            let substitution_cost = previous_substitution_cost + usize::from(c1 != c2);
+        for (col, c1) in string1.chars().enumerate() {
+            // col starts at 0
+            let deletion_cost = prev_dist[col] + 1; // "on the left" in the matrix (i.e. the value we just computed)
+            let insertion_cost = prev_dist[col + 1] + 1; // "on the top" in the matrix (means previous)
+            let substitution_cost = prev_substitution_cost + usize::from(c1 != c2); // top-left + 1 if char differs, 0 otherwise
 
-            previous_substitution_cost = d[i];
-            d[i] = min3(deletion_cost, insertion_cost, substitution_cost);
+            prev_substitution_cost = prev_dist[col + 1]; // save the old value at (i-1, j-1)
 
-            i += 1;
+            // compute the minimum (uses a custom `min3` function for more readability)
+            prev_dist[col + 1] = min3(deletion_cost, insertion_cost, substitution_cost);
         }
-
-        j += 1;
     }
-
-    d[d.len() - 1]
+    prev_dist[l1]
 }
 
 #[cfg(test)]
 mod levenshtein_distance_should {
     use super::levenshtein_distance;
+
+    #[test]
+    fn test_doc_example() {
+        assert_eq!(2, levenshtein_distance("FROG", "DOG"));
+    }
 
     #[test]
     fn return_0_with_empty_strings() {
