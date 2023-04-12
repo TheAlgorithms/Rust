12054bc3282d7811ccd9c27103a20cbf6a4fa925
aesteve <arnaud.esteve@gmail.com>
2023-04-12 19:54:15 +0200

    Rework segment tree to be more generic and accept ranges

--- src/data_structures/segment_tree.rs
+++ src/data_structures/segment_tree.rs
@@ -1,50 +1,51 @@
+use std::cmp::min;
+use std::fmt::Debug;
+use std::ops::Range;
+
 /// This stucture implements a segmented tree that
 /// can efficiently answer range queries on arrays.
-pub struct SegmentTree<T: Default + Ord + Copy> {
+
+/// We need a reduction function for each segment or interval. It could be the min over this interval, the max, the sum, etc.
+
+pub struct SegmentTree<T: Debug + Default + Ord + Copy> {
     len: usize,
     buf: Vec<T>,
-    op: Ops,
+    merge: fn(T, T) -> T,
 }
 
-pub enum Ops {
-    Max,
-    Min,
-}
-
-impl<T: Default + Ord + Copy> SegmentTree<T> {
+impl<T: Debug + Default + Ord + Copy> SegmentTree<T> {
     /// function to build the tree
-    pub fn from_vec(arr: &[T], op: Ops) -> Self {
+    pub fn from_vec(arr: &[T], merge: fn(T, T) -> T) -> Self {
         let len = arr.len();
         let mut buf: Vec<T> = vec![T::default(); 2 * len];
         buf[len..(len + len)].clone_from_slice(&arr[0..len]);
         for i in (1..len).rev() {
-            buf[i] = match op {
-                Ops::Max => buf[2 * i].max(buf[2 * i + 1]),
-                Ops::Min => buf[2 * i].min(buf[2 * i + 1]),
-            };
+            let old = buf[2 * i];
+            let new = buf[2 * i + 1];
+            buf[i] = merge(old, new);
         }
-        SegmentTree { len, buf, op }
+        SegmentTree { len, buf, merge }
     }
 
-    /// function to get sum on interval [l, r]
-    pub fn query(&self, mut l: usize, mut r: usize) -> T {
-        l += self.len;
-        r += self.len;
-        let mut res = self.buf[l];
-        while l <= r {
+    /// query the range, will return None if the range is out of the array's boundaries
+    pub fn query(&self, range: Range<usize>) -> Option<T> {
+        let mut l = range.start + self.len;
+        let mut r = min(self.len, range.end) + self.len;
+        let mut res = None;
+        while l < r {
             if l % 2 == 1 {
-                res = match self.op {
-                    Ops::Max => res.max(self.buf[l]),
-                    Ops::Min => res.min(self.buf[l]),
-                };
+                res = Some(match res {
+                    None => self.buf[l],
+                    Some(old) => (self.merge)(old, self.buf[l]),
+                });
                 l += 1;
             }
-            if r % 2 == 0 {
-                res = match self.op {
-                    Ops::Max => res.max(self.buf[r]),
-                    Ops::Min => res.min(self.buf[r]),
-                };
+            if r % 2 == 1 {
                 r -= 1;
+                res = Some(match res {
+                    None => self.buf[r],
+                    Some(old) => (self.merge)(old, self.buf[r]),
+                });
             }
             l /= 2;
             r /= 2;
@@ -59,10 +60,7 @@ impl<T: Default + Ord + Copy> SegmentTree<T> {
         idx /= 2;
 
         while idx != 0 {
-            self.buf[idx] = match self.op {
-                Ops::Max => self.buf[2 * idx].max(self.buf[2 * idx + 1]),
-                Ops::Min => self.buf[2 * idx].min(self.buf[2 * idx + 1]),
-            };
+            self.buf[idx] = (self.merge)(self.buf[2 * idx], self.buf[2 * idx + 1]);
             idx /= 2;
         }
     }
@@ -71,17 +69,54 @@ impl<T: Default + Ord + Copy> SegmentTree<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::cmp::{max, min};
 
     #[test]
-    fn it_works() {
+    fn test_min_segments() {
+        let vec = vec![-30, 2, -4, 7, 3, -5, 6, 11, -20, 9, 14, 15, 5, 2, -8];
+        let min_seg_tree = SegmentTree::from_vec(&vec, min);
+        assert_eq!(Some(-5), min_seg_tree.query(4..7));
+        assert_eq!(Some(-30), min_seg_tree.query(0..vec.len()));
+        assert_eq!(Some(-30), min_seg_tree.query(0..2));
+        assert_eq!(Some(-4), min_seg_tree.query(1..3));
+        assert_eq!(Some(-5), min_seg_tree.query(1..7));
+        for (i, val) in vec.iter().enumerate() {
+            assert_eq!(Some(*val), min_seg_tree.query(i..(i + 1)));
+        }
+    }
+
+    #[test]
+    fn test_max_segments() {
         let vec = vec![1, 2, -4, 7, 3, -5, 6, 11, -20, 9, 14, 15, 5, 2, -8];
-        let min_seg_tree = SegmentTree::from_vec(&vec, Ops::Min);
-        assert_eq!(-5, min_seg_tree.query(4, 6));
-        assert_eq!(-20, min_seg_tree.query(0, vec.len() - 1));
-        let mut max_seg_tree = SegmentTree::from_vec(&vec, Ops::Max);
-        assert_eq!(6, max_seg_tree.query(4, 6));
-        assert_eq!(15, max_seg_tree.query(0, vec.len() - 1));
+        let mut max_seg_tree = SegmentTree::from_vec(&vec, max);
+        assert_eq!(Some(15), max_seg_tree.query(0..vec.len()));
+        assert_eq!(Some(6), max_seg_tree.query(4..7));
+        for (i, val) in vec.iter().enumerate() {
+            assert_eq!(Some(*val), max_seg_tree.query(i..(i + 1)));
+        }
         max_seg_tree.update(6, 8);
-        assert_eq!(8, max_seg_tree.query(4, 6));
+        assert_eq!(Some(8), max_seg_tree.query(4..7));
+    }
+
+    #[test]
+    fn test_sum_segments() {
+        let val_at_6 = 6;
+        let vec = vec![1, 2, -4, 7, 3, -5, val_at_6, 11, -20, 9, 14, 15, 5, 2, -8];
+        let mut sum_seg_tree = SegmentTree::from_vec(&vec, |a, b| a + b);
+        assert_eq!(
+            Some(vec.iter().sum::<i32>()),
+            sum_seg_tree.query(0..vec.len())
+        );
+        for (i, val) in vec.iter().enumerate() {
+            assert_eq!(Some(*val), sum_seg_tree.query(i..(i + 1)));
+        }
+        let sum_4_to_6 = sum_seg_tree.query(4..7);
+        assert_eq!(Some(4), sum_4_to_6);
+        let delta = 3;
+        sum_seg_tree.update(6, val_at_6 + delta);
+        assert_eq!(
+            sum_4_to_6.unwrap() + delta,
+            sum_seg_tree.query(4..7).unwrap()
+        );
     }
 }
