ca8e0c092f9de3516a7a3767825036802a7270bb
Kartikeya Agate <agatekartik@gmail.com>
2023-04-13 20:15:37 +0530

    use element matching instead of HashSet; performs better for smaller imputs

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -53,19 +53,29 @@ pub use self::stooge_sort::stooge_sort;
 pub use self::tim_sort::tim_sort;
 
 #[cfg(test)]
-use std::collections::HashSet;
-
-#[cfg(test)]
 use std::cmp;
 
 #[cfg(test)]
 pub fn have_same_elements<T>(a: &[T], b: &[T]) -> bool
 where
-    T: cmp::PartialOrd + cmp::Eq + std::hash::Hash
+    T: cmp::PartialOrd
+    // If HashSet is used
+    // T: cmp::PartialOrd + cmp::Eq + std::hash::Hash
 {
-    let set_a: HashSet<&T> = a.iter().collect();
-    let set_b: HashSet<&T> = b.iter().collect();
-    set_a == set_b
+    match a.len() == b.len() {
+        true => {
+            // This is O(n^2) but performs better on smaller data sizes
+            b.iter().all(|item| a.contains(item))
+
+            // This is O(n), performs well on larger data sizes
+            // let set_a: HashSet<&T> = a.iter().collect();
+            // let set_b: HashSet<&T> = b.iter().collect();
+            // set_a == set_b
+        },
+        false => {
+            false
+        },
+    }
 }
 
 
