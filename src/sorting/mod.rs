acb270e50b8ba706d12add886c25366d86fff605
Kartikeya Agate <agatekartik@gmail.com>
2023-04-14 15:02:08 +0530

    prefer HashSet over linear

--- src/sorting/mod.rs
+++ src/sorting/mod.rs
@@ -58,18 +58,21 @@ use std::cmp;
 #[cfg(test)]
 pub fn have_same_elements<T>(a: &[T], b: &[T]) -> bool
 where
-    T: cmp::PartialOrd, // If HashSet is used
-                        // T: cmp::PartialOrd + cmp::Eq + std::hash::Hash
+    // T: cmp::PartialOrd,
+    // If HashSet is used
+    T: cmp::PartialOrd + cmp::Eq + std::hash::Hash
 {
+    use std::collections::HashSet;
+
     match a.len() == b.len() {
         true => {
             // This is O(n^2) but performs better on smaller data sizes
-            b.iter().all(|item| a.contains(item))
+            //b.iter().all(|item| a.contains(item))
 
             // This is O(n), performs well on larger data sizes
-            // let set_a: HashSet<&T> = a.iter().collect();
-            // let set_b: HashSet<&T> = b.iter().collect();
-            // set_a == set_b
+            let set_a: HashSet<&T> = a.iter().collect();
+            let set_b: HashSet<&T> = b.iter().collect();
+            set_a == set_b
         }
         false => false,
     }
