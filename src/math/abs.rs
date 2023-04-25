8796ced2f41c6b474c0df14ad19fa381b6951d5f
Léana 江 <leana.jiang@icloud.com>
2023-04-25 14:34:52 +0200

    ref(abs): formated docs

--- src/math/abs.rs
+++ src/math/abs.rs
@@ -1,6 +1,7 @@
-/// This function returns the absolute value of a number.
-/// The absolute value of a number is the non-negative value of the number, regardless of its sign
-/// Wikipedia: https://en.wikipedia.org/wiki/Absolute_value
+/// This function returns the absolute value of a number.\
+/// The absolute value of a number is the non-negative value of the number, regardless of its sign.\
+///
+/// Wikipedia: <https://en.wikipedia.org/wiki/Absolute_value>
 pub fn abs(num: f64) -> f64 {
     if num < 0.0 {
         return -num;
