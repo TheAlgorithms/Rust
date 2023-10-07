5d457e3ce006c30bf24692f9b6b0165b09b5b927
Gyandeep Katiyar <137227305+Gyan172004@users.noreply.github.com>
2023-10-07 23:35:23 +0530

    Add Kandane's Algorithm and Nth Catalan Number (#536)

---
+++ src/general/kadane_algorithm.rs
@@ -0,0 +1,86 @@
+/**
+ * @file
+ * @brief Find the maximum subarray sum using Kadane's algorithm.(https://en.wikipedia.org/wiki/Maximum_subarray_problem)
+ *
+ * @details
+ * This program provides a function to find the maximum subarray sum in an array of integers
+ * using Kadane's algorithm.
+ *
+ * @param arr A slice of integers representing the array.
+ * @return The maximum subarray sum.
+ *
+ * @author [Gyandeep] (https://github.com/Gyan172004)
+ * @see Wikipedia - Maximum subarray problem
+ */
+
+/**
+ * Find the maximum subarray sum using Kadane's algorithm.
+ * @param arr A slice of integers representing the array.
+ * @return The maximum subarray sum.
+ */
+ fn max_sub_array(nums: Vec<i32>) -> i32 {
+    if nums.is_empty() {
+        return 0;
+    }
+
+    let mut max_current = nums[0];
+    let mut max_global = nums[0];
+
+    for i in 1..nums.len() {
+        max_current = std::cmp::max(nums[i], max_current + nums[i]);
+        if max_current > max_global {
+            max_global = max_current;
+        }
+    }
+    return max_global;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /**
+     * Test case for Kadane's algorithm with positive numbers.
+     */
+    #[test]
+    fn test_kadanes_algorithm_positive() {
+        let arr = [1, 2, 3, 4, 5];
+        assert_eq!(max_sub_array(arr.to_vec()), 15);
+    }
+
+    /**
+     * Test case for Kadane's algorithm with negative numbers.
+     */
+    #[test]
+    fn test_kadanes_algorithm_negative() {
+        let arr = [-2, -3, -4, -1, -2];
+        assert_eq!(max_sub_array(arr.to_vec()), -1);
+    }
+
+    /**
+     * Test case for Kadane's algorithm with mixed numbers.
+     */
+    #[test]
+    fn test_kadanes_algorithm_mixed() {
+        let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+        assert_eq!(max_sub_array(arr.to_vec()), 6);
+    }
+
+    /**
+     * Test case for Kadane's algorithm with an empty array.
+     */
+    #[test]
+    fn test_kadanes_algorithm_empty() {
+        let arr: [i32; 0] = [];
+        assert_eq!(max_sub_array(arr.to_vec()), 0);
+    }
+
+    /**
+     * Test case for Kadane's algorithm with a single positive number.
+     */
+    #[test]
+    fn test_kadanes_algorithm_single_positive() {
+        let arr = [10];
+        assert_eq!(max_sub_array(arr.to_vec()), 10);
+    }
+}
