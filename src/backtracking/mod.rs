6cc96b232da98b979eb339c942ad6d4bda0eadb4
Guizoul <guizoul.abdellah@ine.inpt.ma>
2023-10-08 16:11:52 +0100

    Implemented all Permutations using Backtracking Algorithm

--- src/backtracking/mod.rs
+++ src/backtracking/mod.rs
@@ -1,7 +1,9 @@
 mod all_combination_of_size_k;
 mod n_queens;
+mod permutations;
 mod sudoku;
 
 pub use all_combination_of_size_k::generate_all_combinations;
 pub use n_queens::n_queens_solver;
+pub use permutations::permute;
 pub use sudoku::Sudoku;
