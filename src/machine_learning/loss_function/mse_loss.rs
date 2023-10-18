cb1b83d45f1a097327bee7731324e5f0fe44cd95
Rakshit Kumar Singh <rakshitsingh421@gmail.com>
2023-10-19 01:23:26 +0530

    Add mse_loss (#582)

---
+++ src/machine_learning/loss_function/mse_loss.rs
@@ -0,0 +1,35 @@
+//! # Mean Square Loss Function
+//!
+//! The `mse_loss` function calculates the Mean Square Error loss, which is a
+//! robust loss function used in machine learning.
+//!
+//! ## Formula
+//!
+//! For a pair of actual and predicted values, represented as vectors `actual`
+//! and `predicted`, the Mean Square  loss is calculated as:
+//!
+//! - loss = `(actual - predicted)^2 / n_elements`.
+//!
+//! It returns the average loss by dividing the `total_loss` by total no. of
+//! elements.
+//!
+pub fn mse_loss(predicted: &Vec<f64>, actual: &[f64]) -> f64 {
+    let mut total_loss: f64 = 0.0;
+    for (p, a) in predicted.iter().zip(actual.iter()) {
+        let diff: f64 = p - a;
+        total_loss += diff * diff;
+    }
+    total_loss / (predicted.len() as f64)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mse_loss() {
+        let predicted_values: Vec<f64> = vec![1.0, 2.0, 3.0, 4.0];
+        let actual_values: Vec<f64> = vec![1.0, 3.0, 3.5, 4.5];
+        assert_eq!(mse_loss(&predicted_values, &actual_values), 0.375);
+    }
+}
--- src/machine_learning/mod.rs
+++ src/machine_learning/mod.rs
@@ -1,5 +1,7 @@
 mod linear_regression;
+mod loss_function;
 mod optimization;
 
 pub use self::linear_regression::linear_regression;
+pub use self::loss_function::mse_loss;
 pub use self::optimization::gradient_descent;
