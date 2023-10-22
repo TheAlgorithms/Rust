e9e5f75a3bdf4756fbd8e689acc92c5e7a8276dc
Ruchira Naskar <96806888+RuchNas-Pottah@users.noreply.github.com>
2023-10-22 20:23:50 +0530

    Updated Moore's_Voting_Algorithm.rs
    Changed main to tests

--- src/searching/Moore/Moore's_Voting_Algorithm.rs
+++ src/searching/Moore/Moore's_Voting_Algorithm.rs
@@ -1,49 +1,5 @@
-use std::io;
+/*
 
-struct Solution {
-    solver: fn(&Vec<i32>) -> i32,
-}
-
-impl Solution {
-    fn new() -> Solution {
-        Solution {
-            solver: Solution::moore_voting,
-        }
-    }
-
-    fn moore_voting(arr: &Vec<i32>) -> i32 {
-        let n = arr.len();
-        let mut cnt = 0; // initializing cnt
-        let mut ele = 0; // initializing ele
-
-        for i in 0..n {
-            if cnt == 0 {
-                cnt = 1;
-                ele = arr[i];
-            } else if arr[i] == ele {
-                cnt += 1;
-            } else {
-                cnt -= 1;
-            }
-        }
-
-        let cnt_check = arr.iter().filter(|&&x| x == ele).count();
-
-        if cnt_check > (n / 2) {
-            ele
-        } else {
-            -1
-        }
-    }
-
-    fn caller_func(&self, arr: &Vec<i32>) -> i32 {
-        (self.solver)(arr)
-    }
-}
-
-fn main() {
-
-    /*
     Moore's voting algorithm finds out the strictly majority-occurring element
     without using extra space
     and O(n) + O(n) time complexity
@@ -80,30 +36,50 @@ fn main() {
 
     Now, this ele should be the majority element if there's any
     To check, a quick O(n) loop is run to check if the count of ele is >(n/2), n being the length of the array
-    */
     
-    let mut input = String::new();
-    println!("Number of elements: ");
-    io::stdin().read_line(&mut input).expect("");
-    let n: usize = input.trim().parse().expect("");
+    -1 is returned when no such element is found.
+    
+*/
 
-    let mut arr = vec![0; n];
+pub fn moore_voting(arr: &Vec<i32>) -> i32 {
+    let n = arr.len();
+    let mut cnt = 0; // initializing cnt
+    let mut ele = 0; // initializing ele
 
-    println!("Enter array elements: ");
-    input.clear();
-    io::stdin().read_line(&mut input).expect("");
-    let elements: Vec<i32> = input
-        .split_whitespace()
-        .map(|s| s.parse().expect(""))
-        .collect();
-    arr.copy_from_slice(&elements);
-
-    let soln = Solution::new();
-    let solution = soln.caller_func(&arr);
-
-    if solution == -1 {
-        println!("No element is present >(n/2) times.");
-    } else {
-        println!("{} is present >(n/2) times.", solution);
+    for i in 0..n {
+        if cnt == 0 {
+            cnt = 1;
+            ele = arr[i];
+        } 
+        else if arr[i] == ele {
+            cnt += 1;
+        } 
+        else {
+            cnt -= 1;
+        }
     }
+
+    let cnt_check = arr.iter().filter(|&&x| x == ele).count();
+
+    if cnt_check > (n / 2) {
+        ele
+    } else {
+        -1
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_moore_voting() {
+    
+        let arr1: Vec<i32> = vec![9, 1, 8, 1, 1];
+        assert!(moore_voting(arr1),1);
+        let arr2: Vec<i32> = vec![1,2,3,4];
+        assert!(moore_voting(arr2),-1);
+
+    }
+    
 }
