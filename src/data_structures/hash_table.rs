a8519411afcc8b427c487e99770d6ca2e9f81f5b
BogdanCiocea <bogdanandreiciocea@gmail.com>
2023-11-23 16:43:36 +0200

    good to go now

--- src/data_structures/hash_table.rs
+++ src/data_structures/hash_table.rs
@@ -6,6 +6,11 @@ pub struct HashTable<K, V> {
     count: usize,
 }
 
+impl<K: Hashable + std::cmp::PartialEq, V> Default for HashTable<K, V> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
 pub trait Hashable {
     fn hash(&self) -> usize;
 }
@@ -14,15 +19,12 @@ impl<K: Hashable + std::cmp::PartialEq, V> HashTable<K, V> {
     pub fn new() -> HashTable<K, V> {
         let initial_capacity = 3000;
         let mut elements = Vec::with_capacity(initial_capacity);
-        
+
         for _ in 0..initial_capacity {
             elements.push(LinkedList::new());
         }
 
-        HashTable {
-            elements,
-            count: 0,
-        }
+        HashTable { elements, count: 0 }
     }
 
     pub fn insert(&mut self, key: K, value: V) {
@@ -36,24 +38,27 @@ impl<K: Hashable + std::cmp::PartialEq, V> HashTable<K, V> {
 
     pub fn search(&self, key: K) -> Option<&V> {
         let index = key.hash() % self.elements.len();
-        self.elements[index].iter().find(|(k, _)| *k == key).map(|(_, v)| v)
+        self.elements[index]
+            .iter()
+            .find(|(k, _)| *k == key)
+            .map(|(_, v)| v)
     }
 
     fn resize(&mut self) {
         let new_size = self.elements.len() * 2;
         let mut new_elements = Vec::with_capacity(new_size);
-    
+
         for _ in 0..new_size {
             new_elements.push(LinkedList::new());
         }
-    
+
         for old_list in self.elements.drain(..) {
             for (key, value) in old_list {
                 let new_index = key.hash() % new_size;
                 new_elements[new_index].push_back((key, value));
             }
         }
-    
+
         self.elements = new_elements;
     }
 }
