1e026bf07760bba185b99886e09067b671c3d3db
vil02 <vil02@o2.pl>
2023-11-23 18:59:18 +0100

    refactor: remove redundant branch

--- src/math/prime_factors.rs
+++ src/math/prime_factors.rs
@@ -4,9 +4,6 @@ pub fn prime_factors(n: u64) -> Vec<u64> {
     let mut i = 2;
     let mut n = n;
     let mut factors = Vec::new();
-    if n == 0 || n == 1 {
-        return factors;
-    }
     while i * i <= n {
         if n % i != 0 {
             if i != 2 {
