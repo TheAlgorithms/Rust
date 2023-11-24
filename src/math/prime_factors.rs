1e6e8a37c0717e6ad7bfe4fd7d956fce36bdd514
Piotr Idzik <65706193+vil02@users.noreply.github.com>
2023-11-24 17:58:53 +0100

    Fix `prime_factors` (#624)

--- src/math/prime_factors.rs
+++ src/math/prime_factors.rs
@@ -4,13 +4,6 @@ pub fn prime_factors(n: u64) -> Vec<u64> {
     let mut i = 2;
     let mut n = n;
     let mut factors = Vec::new();
-    if n == 0 {
-        return factors;
-    }
-    if n == 1 {
-        factors.push(1);
-        return factors;
-    }
     while i * i <= n {
         if n % i != 0 {
             if i != 2 {
@@ -34,6 +27,7 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(prime_factors(0), vec![]);
+        assert_eq!(prime_factors(1), vec![]);
         assert_eq!(prime_factors(11), vec![11]);
         assert_eq!(prime_factors(25), vec![5, 5]);
         assert_eq!(prime_factors(33), vec![3, 11]);
