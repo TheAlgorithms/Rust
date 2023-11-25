0b39e8f1d8ae0279baa01d1f18ec63a21f030afa
Harsh Kumar <61012869+cyrixninja@users.noreply.github.com>
2023-11-25 09:01:21 +0530

    Fixed Issues

--- src/math/sum_of_harmonic_series.rs
+++ src/math/sum_of_harmonic_series.rs
@@ -5,7 +5,11 @@
 // Wikipedia Reference  :  https://en.wikipedia.org/wiki/Interquartile_range
 // Other References     :  https://the-algorithms.com/algorithm/sum-of-harmonic-series?lang=python
 
-pub fn sum_of_harmonic_progression(first_term: f64, common_difference: f64, number_of_terms: i32) -> f64 {
+pub fn sum_of_harmonic_progression(
+    first_term: f64,
+    common_difference: f64,
+    number_of_terms: i32,
+) -> f64 {
     let mut arithmetic_progression = vec![1.0 / first_term];
     let mut current_term = 1.0 / first_term;
 
@@ -14,7 +18,10 @@ pub fn sum_of_harmonic_progression(first_term: f64, common_difference: f64, numb
         arithmetic_progression.push(current_term);
     }
 
-    let harmonic_series: Vec<f64> = arithmetic_progression.into_iter().map(|step| 1.0 / step).collect();
+    let harmonic_series: Vec<f64> = arithmetic_progression
+        .into_iter()
+        .map(|step| 1.0 / step)
+        .collect();
     harmonic_series.iter().sum()
 }
 
@@ -25,6 +32,9 @@ mod tests {
     #[test]
     fn test_sum_of_harmonic_progression() {
         assert_eq!(sum_of_harmonic_progression(1.0 / 2.0, 2.0, 2), 0.75);
-        assert_eq!(sum_of_harmonic_progression(1.0 / 5.0, 5.0, 5), 0.45666666666666667);
+        assert_eq!(
+            sum_of_harmonic_progression(1.0 / 5.0, 5.0, 5),
+            0.45666666666666667
+        );
     }
 }
