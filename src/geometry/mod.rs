7d2aa9e8be79cd23c36aa99cbfa66b520b132035
Vinícius de Oliveira <88348878+ViniciussdeOliveira@users.noreply.github.com>
2023-12-05 15:28:57 -0300

    Add algorithm to find a number of points in a polygon area (fixes #628) (#629)

--- src/geometry/mod.rs
+++ src/geometry/mod.rs
@@ -2,10 +2,12 @@ mod closest_points;
 mod graham_scan;
 mod jarvis_scan;
 mod point;
+mod polygon_points;
 mod segment;
 
 pub use self::closest_points::closest_points;
 pub use self::graham_scan::graham_scan;
 pub use self::jarvis_scan::jarvis_march;
 pub use self::point::Point;
+pub use self::polygon_points::lattice_points;
 pub use self::segment::Segment;
