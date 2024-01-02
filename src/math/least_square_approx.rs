bcc9e32a7bb010debd6d50cd0e5bebe7595e638a
mihaubuhai <119884187+mihaubuhai@users.noreply.github.com>
2024-01-02 12:17:51 +0200

    Add trigonometric functions (#643)

--- src/math/least_square_approx.rs
+++ src/math/least_square_approx.rs
@@ -9,40 +9,49 @@
 ///
 /// degree -> degree of the polynomial
 ///
-pub fn least_square_approx(points: &[(f64, f64)], degree: i32) -> Vec<f64> {
+pub fn least_square_approx<T: Into<f64> + Copy, U: Into<f64> + Copy>(
+    points: &[(T, U)],
+    degree: i32,
+) -> Option<Vec<f64>> {
     use nalgebra::{DMatrix, DVector};
 
     /* Used for rounding floating numbers */
-    fn round_to_decimals(value: f64, decimals: u32) -> f64 {
-        let multiplier = 10f64.powi(decimals as i32);
+    fn round_to_decimals(value: f64, decimals: i32) -> f64 {
+        let multiplier = 10f64.powi(decimals);
         (value * multiplier).round() / multiplier
     }
+
+    /* Casting the data parsed to this function to f64 (as some points can have decimals) */
+    let vals: Vec<(f64, f64)> = points
+        .iter()
+        .map(|(x, y)| ((*x).into(), (*y).into()))
+        .collect();
+    /* Because of collect we need the Copy Trait for T and U */
 
     /* Computes the sums in the system of equations */
     let mut sums = Vec::<f64>::new();
     for i in 1..=(2 * degree + 1) {
-        sums.push(points.iter().map(|(x, _)| x.powi(i - 1)).sum());
+        sums.push(vals.iter().map(|(x, _)| x.powi(i - 1)).sum());
     }
 
-    let mut free_col = Vec::<f64>::new();
     /* Compute the free terms column vector */
+    let mut free_col = Vec::<f64>::new();
     for i in 1..=(degree + 1) {
-        free_col.push(points.iter().map(|(x, y)| y * (x.powi(i - 1))).sum());
+        free_col.push(vals.iter().map(|(x, y)| y * (x.powi(i - 1))).sum());
     }
     let b = DVector::from_row_slice(&free_col);
 
-    let size = (degree + 1) as usize;
     /* Create and fill the system's matrix */
-    let a = DMatrix::from_fn(size, size, |i, j| sums[i + j]);
+    let size = (degree + 1) as usize;
+    let a = DMatrix::from_fn(size, size, |i, j| sums[degree as usize + i - j]);
 
     /* Solve the system of equations: A * x = b */
     match a.qr().solve(&b) {
         Some(x) => {
-            let mut rez: Vec<f64> = x.iter().map(|x| round_to_decimals(*x, 5)).collect();
-            rez.reverse();
-            rez
+            let rez: Vec<f64> = x.iter().map(|x| round_to_decimals(*x, 5)).collect();
+            Some(rez)
         }
-        None => Vec::new(),
+        None => None, //<-- The system cannot be solved (badly conditioned system's matrix)
     }
 }
 
@@ -65,7 +74,10 @@ mod tests {
             (5.1, 8.4),
         ];
 
-        assert_eq!(least_square_approx(&points, 1), [-0.49069, 10.44898]);
+        assert_eq!(
+            least_square_approx(&points, 1),
+            Some(vec![-0.49069, 10.44898])
+        );
     }
 
     #[test]
@@ -83,7 +95,9 @@ mod tests {
 
         assert_eq!(
             least_square_approx(&points, 5),
-            [0.00603, -0.21304, 2.79929, -16.53468, 40.29473, -19.35771]
+            Some(vec![
+                0.00603, -0.21304, 2.79929, -16.53468, 40.29473, -19.35771
+            ])
         );
     }
 
@@ -96,6 +110,6 @@ mod tests {
             (0.7051, 3.49716601),
         ];
 
-        assert_eq!(least_square_approx(&points, 2), [1.0, 0.0, 3.0]);
+        assert_eq!(least_square_approx(&points, 2), Some(vec![1.0, 0.0, 3.0]));
     }
 }
