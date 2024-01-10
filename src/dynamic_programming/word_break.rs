59a3748f7e5d63c0b4bfd019fe9b258ff4771830
Mihnea Tudor Crețu <59806467+tudor-cretu@users.noreply.github.com>
2024-01-10 20:41:57 +0000

    Removed my implementation for Trie, added the implementation already present in data_structures

--- src/dynamic_programming/word_break.rs
+++ src/dynamic_programming/word_break.rs
@@ -9,57 +9,7 @@
 // available words for the current position in the string.
 
 use std::collections::HashMap;
-
-struct Trie {
-    children: HashMap<char, Box<Trie>>,
-    is_word: bool,
-}
-
-impl Trie {
-    pub fn new() -> Self {
-        Trie {
-            children: HashMap::new(),
-            is_word: false,
-        }
-    }
-
-    pub fn insert(&mut self, word: &str) {
-        let mut node = self;
-        for c in word.chars() {
-            node = node
-                .children
-                .entry(c)
-                .or_insert_with(|| Box::new(Trie::new()));
-        }
-        node.is_word = true;
-    }
-
-    pub fn search(&self, s: &str, start: usize, memo: &mut Vec<Option<bool>>) -> bool {
-        if start >= s.len() {
-            return true;
-        }
-
-        if let Some(res) = memo[start] {
-            return res;
-        }
-
-        let mut node = self;
-        for (i, c) in s[start..].chars().enumerate() {
-            if let Some(n) = node.children.get(&c) {
-                node = n;
-                if node.is_word && self.search(s, start + i + 1, memo) {
-                    memo[start] = Some(true);
-                    return true;
-                }
-            } else {
-                break;
-            }
-        }
-
-        memo[start] = Some(false);
-        false
-    }
-}
+use crate::data_structures::Trie; 
 
 pub fn word_break(s: &str, word_dict: Vec<&str>) -> bool {
     let mut trie = Trie::new();
