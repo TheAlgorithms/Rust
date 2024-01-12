4c1c2c25a4cab5109f6b78dc51df930d9e594c79
Neacșu Adrian <75613031+Adin3@users.noreply.github.com>
2024-01-12 12:36:04 +0000

    updated directory.md

--- src/string/lipogram.rs
+++ src/string/lipogram.rs
@@ -17,9 +17,16 @@ pub fn is_lipogram(lipogram_str: &str) -> (bool, String) {
         }
     }
 
-    if excluded_letters.len() != 0 {
-        let excluded_letters_joined = excluded_letters.iter().map(|&c| c.to_string()).collect::<Vec<String>>().join(", ");
-        let result = format!("{}{}", "The sentence is a lipogram that excludes the letters: ", excluded_letters_joined);
+    if !excluded_letters.is_empty() {
+        let excluded_letters_joined = excluded_letters
+            .iter()
+            .map(|&c| c.to_string())
+            .collect::<Vec<String>>()
+            .join(", ");
+        let result = format!(
+            "{}{}",
+            "The sentence is a lipogram that excludes the letters: ", excluded_letters_joined
+        );
         (true, result)
     } else {
         (false, String::from("The sentence is not a lipogram"))
@@ -32,30 +39,69 @@ mod tests {
 
     #[test]
     fn test_lipogram_invalid1() {
-        assert_eq!(is_lipogram("The quick brown fox jumps over the lazy dog"), (false, String::from("The sentence is not a lipogram")));
+        assert_eq!(
+            is_lipogram("The quick brown fox jumps over the lazy dog"),
+            (false, String::from("The sentence is not a lipogram"))
+        );
     }
 
     #[test]
     fn test_lipogram_invalid2() {
-        assert_eq!(is_lipogram("Jackdaws love my big sphinx of quartz"), (false, String::from("The sentence is not a lipogram")));
-        assert_eq!(is_lipogram("abcdefghijklmnopqrstuvwxyz"), (false, String::from("The sentence is not a lipogram")));
-        assert_eq!(is_lipogram("Five quacking zephyrs jolt my wax bed"), (false, String::from("The sentence is not a lipogram")));
+        assert_eq!(
+            is_lipogram("Jackdaws love my big sphinx of quartz"),
+            (false, String::from("The sentence is not a lipogram"))
+        );
+        assert_eq!(
+            is_lipogram("abcdefghijklmnopqrstuvwxyz"),
+            (false, String::from("The sentence is not a lipogram"))
+        );
+        assert_eq!(
+            is_lipogram("Five quacking zephyrs jolt my wax bed"),
+            (false, String::from("The sentence is not a lipogram"))
+        );
     }
 
     #[test]
-    fn test_lipogram_invalid3() {
-        assert_eq!(is_lipogram(""), (false, String::from("The sentence is empty")));
+    fn test_lipogram_empty() {
+        assert_eq!(
+            is_lipogram(""),
+            (false, String::from("The sentence is empty"))
+        );
     }
 
     #[test]
     fn test_lipogram_valid1() {
-        assert_eq!(is_lipogram("abcdefghijklmnopqrstuvwxy"), (true, String::from("The sentence is a lipogram that excludes the letters: z")));
+        assert_eq!(
+            is_lipogram("abcdefghijklmnopqrstuvwxy"),
+            (
+                true,
+                String::from("The sentence is a lipogram that excludes the letters: z")
+            )
+        );
     }
 
     #[test]
     fn test_lipogram_valid2() {
-        assert_eq!(is_lipogram("The quick brown fox jumped over the lazy dog"), (true, String::from("The sentence is a lipogram that excludes the letters: s")));
-        assert_eq!(is_lipogram("The brown fox jumped over the lazy dog with a brick"), (true, String::from("The sentence is a lipogram that excludes the letters: q, s")));
-        assert_eq!(is_lipogram("The brown cat jumped over the lazy dog with a brick"), (true, String::from("The sentence is a lipogram that excludes the letters: f, q, s, x")));
+        assert_eq!(
+            is_lipogram("The quick brown fox jumped over the lazy dog"),
+            (
+                true,
+                String::from("The sentence is a lipogram that excludes the letters: s")
+            )
+        );
+        assert_eq!(
+            is_lipogram("The brown fox jumped over the lazy dog with a brick"),
+            (
+                true,
+                String::from("The sentence is a lipogram that excludes the letters: q, s")
+            )
+        );
+        assert_eq!(
+            is_lipogram("The brown cat jumped over the lazy dog with a brick"),
+            (
+                true,
+                String::from("The sentence is a lipogram that excludes the letters: f, q, s, x")
+            )
+        );
     }
 }
--- src/string/pangram.rs
+++ src/string/pangram.rs
@@ -7,7 +7,7 @@
 pub fn is_pangram(pangram_str: &str) -> (bool, &str) {
     let alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-    if lipogram_str.is_empty() {
+    if pangram_str.is_empty() {
         return (false, "The sentence is empty");
     }
 
@@ -30,35 +30,60 @@ mod tests {
 
     #[test]
     fn test_panagram_invalid1() {
-        assert_eq!(is_pangram("This is not a pangram"), (false, "The sentence is not a pangram"));
+        assert_eq!(
+            is_pangram("This is not a pangram"),
+            (false, "The sentence is not a pangram")
+        );
     }
 
     #[test]
     fn test_panagram_invalid2() {
-        assert_eq!(is_pangram("today is a good day"), (false, "The sentence is not a pangram"));
-        assert_eq!(is_pangram(""), (false, "The sentence is not a pangram"));
-        assert_eq!(is_pangram("this is almost a pangram but it does not have bcfghjkqwxy and the last letter"), (false, "The sentence is not a pangram"));
+        assert_eq!(
+            is_pangram("today is a good day"),
+            (false, "The sentence is not a pangram")
+        );
+        assert_eq!(
+            is_pangram(
+                "this is almost a pangram but it does not have bcfghjkqwxy and the last letter"
+            ),
+            (false, "The sentence is not a pangram")
+        );
     }
 
     #[test]
-    fn test_panagram_invalid2() {
+    fn test_panagram_empty() {
         assert_eq!(is_pangram(""), (false, "The sentence is empty"));
     }
 
     #[test]
     fn test_panagram_valid1() {
-        assert_eq!(is_pangram("The quick brown fox jumps over the lazy dog"), (true, "The sentence is a pangram"));
+        assert_eq!(
+            is_pangram("The quick brown fox jumps over the lazy dog"),
+            (true, "The sentence is a pangram")
+        );
     }
 
     #[test]
     fn test_panagram_valid2() {
-        assert_eq!(is_pangram("A mad boxer shot a quick, gloved jab to the jaw of his dizzy opponent"), (true, "The sentence is a pangram"));
-        assert_eq!(is_pangram("Amazingly few discotheques provide jukeboxes"), (true, "The sentence is a pangram"));
-        assert_eq!(is_pangram("How vexingly quick daft zebras jump"), (true, "The sentence is a pangram"));
+        assert_eq!(
+            is_pangram("A mad boxer shot a quick, gloved jab to the jaw of his dizzy opponent"),
+            (true, "The sentence is a pangram")
+        );
+        assert_eq!(
+            is_pangram("Amazingly few discotheques provide jukeboxes"),
+            (true, "The sentence is a pangram")
+        );
+        assert_eq!(
+            is_pangram("How vexingly quick daft zebras jump"),
+            (true, "The sentence is a pangram")
+        );
     }
 
     #[test]
     fn test_panagram_valid3() {
-        assert_eq!(is_pangram("Mr. Jock, TV quiz PhD, bags few lynx"), (true, "The sentence is a perfect pangram"));
+        assert_eq!(
+            is_pangram("Mr. Jock, TV quiz PhD, bags few lynx"),
+            (true, "The sentence is a perfect pangram")
+        );
     }
 }
