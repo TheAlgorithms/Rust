557119bf6daada2044a454f7d50b100e95bf7a67
Victor Moisa <moisa.victor.gabriel@gmail.com>
2024-01-25 14:08:25 +0200

    Added requested test and attempted to change use of clone

--- src/sorting/strand_sort.rs
+++ src/sorting/strand_sort.rs
@@ -5,40 +5,35 @@ pub fn strand_sort(list: &LinkedList<i32>) -> LinkedList<i32> {
         return LinkedList::new();
     }
 
-    let mut ip = list.clone();
+    let mut ip = list.iter().cloned().collect::<LinkedList<i32>>();
     let mut op = LinkedList::new();
 
     while !ip.is_empty() {
         let mut sublist = LinkedList::new();
         sublist.push_back(ip.pop_front().unwrap());
 
-        let mut iter = ip.split_off(0);
-        while let Some(val) = iter.pop_front() {
+        let mut temp_list = LinkedList::new();
+        while let Some(val) = ip.pop_front() {
             if val >= *sublist.back().unwrap() {
                 sublist.push_back(val);
             } else {
-                ip.push_back(val);
+                temp_list.push_back(val);
             }
         }
 
-        // Merge current sublist into output
         let mut merged = LinkedList::new();
-        while !op.is_empty() || !sublist.is_empty() {
-            match (op.front(), sublist.front()) {
-                (Some(&op_val), Some(&sub_val)) if op_val <= sub_val => {
-                    merged.push_back(op.pop_front().unwrap());
-                }
-                (_, Some(_)) => {
-                    merged.push_back(sublist.pop_front().unwrap());
-                }
-                (Some(_), _) => {
-                    merged.push_back(op.pop_front().unwrap());
-                }
-                (None, None) => break,
+        while let (Some(&op_val), Some(&sub_val)) = (op.front(), sublist.front()) {
+            if op_val <= sub_val {
+                merged.push_back(op.pop_front().unwrap());
+            } else {
+                merged.push_back(sublist.pop_front().unwrap());
             }
         }
+        merged.append(&mut op);
+        merged.append(&mut sublist);
 
         op = merged;
+        ip = temp_list;
     }
 
     op
@@ -96,5 +91,12 @@ mod tests {
         let ip = LinkedList::from([5, 4, 3, 2, 1]);
         let result = strand_sort(&ip);
         assert_eq!(result, LinkedList::from([1, 2, 3, 4, 5]));
+    }
+
+    #[test]
+    fn test_strand_sort_interleaved_merge() {
+        let ip = LinkedList::from([4, 1, 5, 2, 6, 3]);
+        let result = strand_sort(&ip);
+        assert_eq!(result, LinkedList::from([1, 2, 3, 4, 5, 6]));
     }
 }
