2243c0a803d4d39536d6b7e271c9cf4b7216a91b
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-03-16 18:59:31 +0530

    Handle unicode characters in `duval_algorithm` (#685)
    * fix: unicode issue with the duval_algorithm
    
    * refactor: reduce code duplication in tests of `duval_algorithm`
    
    ---------
    
    Co-authored-by: vil02 <65706193+vil02@users.noreply.github.com>

--- src/string/duval_algorithm.rs
+++ src/string/duval_algorithm.rs
@@ -1,6 +1,6 @@
 // A string is called simple (or a Lyndon word), if it is strictly smaller than any of its own nontrivial suffixes.
 // Duval (1983) developed an algorithm for finding the standard factorization that runs in linear time and constant space. Source: https://en.wikipedia.org/wiki/Lyndon_word
-fn factorization_with_duval(s: &[u8]) -> Vec<String> {
+fn factorization_with_duval(s: Vec<char>) -> Vec<String> {
     let n = s.len();
     let mut i = 0;
     let mut factorization: Vec<String> = Vec::new();
@@ -19,7 +19,7 @@ fn factorization_with_duval(s: &[u8]) -> Vec<String> {
         }
 
         while i <= k {
-            factorization.push(String::from_utf8(s[i..i + j - k].to_vec()).unwrap());
+            factorization.push(s[i..i + j - k].iter().collect::<String>());
             i += j - k;
         }
     }
@@ -28,36 +28,37 @@ fn factorization_with_duval(s: &[u8]) -> Vec<String> {
 }
 
 pub fn duval_algorithm(s: &str) -> Vec<String> {
-    return factorization_with_duval(s.as_bytes());
+    return factorization_with_duval(s.chars().collect::<Vec<char>>());
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[test]
-    fn test_duval_multiple() {
-        let text = "abcdabcdababc";
-        assert_eq!(duval_algorithm(text), ["abcd", "abcd", "ababc"]);
+    macro_rules! test_duval_algorithm {
+        ($($name:ident: $inputs:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (text, expected) = $inputs;
+                assert_eq!(duval_algorithm(text), expected);
+            }
+        )*
+        }
     }
 
-    #[test]
-    fn test_duval_all() {
-        let text = "aaa";
-        assert_eq!(duval_algorithm(text), ["a", "a", "a"]);
-    }
-
-    #[test]
-    fn test_duval_single() {
-        let text = "ababb";
-        assert_eq!(duval_algorithm(text), ["ababb"]);
+    test_duval_algorithm! {
+        multiple: ("abcdabcdababc", ["abcd", "abcd", "ababc"]),
+        all: ("aaa", ["a", "a", "a"]),
+        single: ("ababb", ["ababb"]),
+        unicode: ("അഅഅ", ["അ", "അ", "അ"]),
     }
 
     #[test]
     fn test_factorization_with_duval_multiple() {
         let text = "abcdabcdababc";
         assert_eq!(
-            factorization_with_duval(text.as_bytes()),
+            factorization_with_duval(text.chars().collect::<Vec<char>>()),
             ["abcd", "abcd", "ababc"]
         );
     }
