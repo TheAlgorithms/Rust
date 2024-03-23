0c2a73d599dc7dc804840bb4c8467d27d6ed7a91
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-03-23 22:18:03 +0700

    feat: implement Huber loss

---
+++ src/machine_learning/loss_function/huber_loss.rs
@@ -0,0 +1,39 @@
+/// Computes the Huber loss between arrays of true and predicted values.
+///
+/// # Arguments
+///
+/// * `y_true` - An array of true values.
+/// * `y_pred` - An array of predicted values.
+/// * `delta` - The threshold parameter that controls the linear behavior of the loss function.
+///
+/// # Returns
+///
+/// The average Huber loss for all pairs of true and predicted values.
+pub fn huber_loss(y_true: &[f64], y_pred: &[f64], delta: f64) -> f64 {
+    let loss: f64 = y_true
+        .iter()
+        .zip(y_pred.iter())
+        .map(|(&true_val, &pred_val)| {
+            let residual = (true_val - pred_val).abs();
+            match residual {
+                r if r <= delta => 0.5 * r.powi(2),
+                _ => delta * residual - 0.5 * delta.powi(2),
+            }
+        })
+        .sum();
+    loss / (y_pred.len() as f64)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_huber_loss() {
+        let y_true = vec![10.0, 8.0, 12.0];
+        let y_pred = vec![9.0, 7.0, 11.0];
+        let delta = 1.0;
+        let expected_loss = 0.5;
+        assert_eq!(huber_loss(&y_true, &y_pred, delta), expected_loss);
+    }
+}
--- src/machine_learning/loss_function/mod.rs
+++ src/machine_learning/loss_function/mod.rs
@@ -1,9 +1,11 @@
 mod hinge_loss;
+mod huber_loss;
 mod kl_divergence_loss;
 mod mean_absolute_error_loss;
 mod mean_squared_error_loss;
 
 pub use self::hinge_loss::hng_loss;
+pub use self::huber_loss::huber_loss;
 pub use self::kl_divergence_loss::kld_loss;
 pub use self::mean_absolute_error_loss::mae_loss;
 pub use self::mean_squared_error_loss::mse_loss;
