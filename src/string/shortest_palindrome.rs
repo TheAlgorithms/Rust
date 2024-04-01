50647f78b88dbbb80a1e6e9cf27f65a75d06d33c
mengfansheng <mengfansheng@uniontech.com>
2024-04-01 10:31:01 +0800

    add support for characters

--- src/string/shortest_palindrome.rs
+++ src/string/shortest_palindrome.rs
@@ -5,13 +5,13 @@ Prefix Functions and KPM. Source：https://oi-wiki.org/string/kmp/
 */
 
 pub fn shortest_palindrome(s: &str) -> String {
-    if s.is_empty() || !s.is_ascii() {
-        return s.to_string();
+    if s.is_empty() {
+        return "".to_string();
     }
-
+    
     let p_chars: Vec<char> = s.chars().collect();
-    let mut suffix = vec![0; s.len()];
-    for i in 1..s.len() {
+    let mut suffix = vec![0; s.chars().count()];
+    for i in 1..s.chars().count() {
         let mut j = suffix[i - 1];
         while j > 0 && p_chars[j] != p_chars[i] {
             j = suffix[j - 1];
@@ -19,7 +19,7 @@ pub fn shortest_palindrome(s: &str) -> String {
         suffix[i] = j + if p_chars[j] == p_chars[i] { 1 } else { 0 };
     }
 
-    let mut dp = vec![0; s.len()];
+    let mut dp = vec![0; s.chars().count()];
     let mut s_chars: Vec<char> = s.chars().rev().collect();
     dp[0] = if p_chars[0] == s_chars[0] { 1 } else { 0 };
     for i in 1..s_chars.len() {
@@ -29,8 +29,8 @@ pub fn shortest_palindrome(s: &str) -> String {
         }
         dp[i] = j + if s_chars[i] == p_chars[j] { 1 } else { 0 };
     }
-
-    s_chars.append(&mut p_chars[dp[s.len() - 1]..s.len()].to_vec());
+    
+    s_chars.append(&mut p_chars[dp[s.chars().count() - 1]..s.chars().count()].to_vec());
     s_chars.iter().collect()
 }
 
@@ -53,5 +53,5 @@ test_shortest_palindrome! {
     extend_left_1: ("aacecaaa", "aaacecaaa"),
     extend_left_2: ("abcd", "dcbabcd"),
     unicode_1: ("അ", "അ"),
-    unicode_2: ("牛", "牛"),
+    unicode_2: ("a牛", "牛a牛"),
 }
