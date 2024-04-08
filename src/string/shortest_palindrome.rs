4263994ef7666256c79f1d7681652f12a9cecd66
mengfansheng <mengfansheng@uniontech.com>
2024-04-08 14:24:28 +0800

    encapsulated func

--- src/string/shortest_palindrome.rs
+++ src/string/shortest_palindrome.rs
@@ -10,6 +10,17 @@ pub fn shortest_palindrome(s: &str) -> String {
     }
 
     let p_chars: Vec<char> = s.chars().collect();
+    let suffix = raw_suffix_function(s, p_chars.clone());
+
+    let mut s_chars: Vec<char> = s.chars().rev().collect();
+    // The prefix of the original string matches the suffix of the flipped string.
+    let dp = invert_suffix_function(s, p_chars.clone(), s_chars.clone(), suffix);
+
+    s_chars.append(&mut p_chars[dp[s.chars().count() - 1]..s.chars().count()].to_vec());
+    s_chars.iter().collect()
+}
+
+pub fn raw_suffix_function(s: &str, p_chars: Vec<char>) -> Vec<usize> {
     let mut suffix = vec![0; s.chars().count()];
     for i in 1..s.chars().count() {
         let mut j = suffix[i - 1];
@@ -18,20 +29,25 @@ pub fn shortest_palindrome(s: &str) -> String {
         }
         suffix[i] = j + if p_chars[j] == p_chars[i] { 1 } else { 0 };
     }
+    suffix
+}
 
+pub fn invert_suffix_function(
+    s: &str,
+    p_chars: Vec<char>,
+    s_chars: Vec<char>,
+    suffix: Vec<usize>,
+) -> Vec<usize> {
     let mut dp = vec![0; s.chars().count()];
-    let mut s_chars: Vec<char> = s.chars().rev().collect();
     dp[0] = if p_chars[0] == s_chars[0] { 1 } else { 0 };
-    for i in 1..s_chars.len() {
+    for i in 1..s.chars().count() {
         let mut j = dp[i - 1];
         while j > 0 && s_chars[i] != p_chars[j] {
             j = suffix[j - 1];
         }
         dp[i] = j + if s_chars[i] == p_chars[j] { 1 } else { 0 };
     }
-
-    s_chars.append(&mut p_chars[dp[s.chars().count() - 1]..s.chars().count()].to_vec());
-    s_chars.iter().collect()
+    dp
 }
 
 macro_rules! test_shortest_palindrome {
