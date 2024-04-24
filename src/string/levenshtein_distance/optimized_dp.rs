8a58b4b49e40fe0c2254dc699923975684645365
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-04-24 08:57:33 +0700

    chore: format code with `fmt`

--- src/string/levenshtein_distance/optimized_dp.rs
+++ src/string/levenshtein_distance/optimized_dp.rs
@@ -124,29 +124,65 @@ mod tests {
 
     #[test]
     fn equal_strings() {
-        assert_eq!(0, optimized_levenshtein_distance("Hello, world!", "Hello, world!"));
-        assert_eq!(0, optimized_levenshtein_distance("Hello, world!", "Hello, world!"));
-        assert_eq!(0, optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#1"));
-        assert_eq!(0, optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#1"));
+        assert_eq!(
+            0,
+            optimized_levenshtein_distance("Hello, world!", "Hello, world!")
+        );
+        assert_eq!(
+            0,
+            optimized_levenshtein_distance("Hello, world!", "Hello, world!")
+        );
+        assert_eq!(
+            0,
+            optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#1")
+        );
+        assert_eq!(
+            0,
+            optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#1")
+        );
     }
 
     #[test]
     fn one_edit_difference() {
-        assert_eq!(1, optimized_levenshtein_distance("Hello, world!", "Hell, world!"));
-        assert_eq!(1, optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#2"));
-        assert_eq!(1, optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#10"));
-        assert_eq!(1, optimized_levenshtein_distance("Hello, world!", "Hell, world!"));
-        assert_eq!(1, optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#2"));
-        assert_eq!(1, optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#10"));
+        assert_eq!(
+            1,
+            optimized_levenshtein_distance("Hello, world!", "Hell, world!")
+        );
+        assert_eq!(
+            1,
+            optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#2")
+        );
+        assert_eq!(
+            1,
+            optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#10")
+        );
+        assert_eq!(
+            1,
+            optimized_levenshtein_distance("Hello, world!", "Hell, world!")
+        );
+        assert_eq!(
+            1,
+            optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#2")
+        );
+        assert_eq!(
+            1,
+            optimized_levenshtein_distance("Test_Case_#1", "Test_Case_#10")
+        );
     }
 
     #[test]
     fn several_differences() {
         assert_eq!(2, optimized_levenshtein_distance("My Cat", "My Case"));
-        assert_eq!(7, optimized_levenshtein_distance("Hello, world!", "Goodbye, world!"));
+        assert_eq!(
+            7,
+            optimized_levenshtein_distance("Hello, world!", "Goodbye, world!")
+        );
         assert_eq!(6, optimized_levenshtein_distance("Test_Case_#3", "Case #3"));
         assert_eq!(2, optimized_levenshtein_distance("My Cat", "My Case"));
-        assert_eq!(7, optimized_levenshtein_distance("Hello, world!", "Goodbye, world!"));
+        assert_eq!(
+            7,
+            optimized_levenshtein_distance("Hello, world!", "Goodbye, world!")
+        );
         assert_eq!(6, optimized_levenshtein_distance("Test_Case_#3", "Case #3"));
     }
 
