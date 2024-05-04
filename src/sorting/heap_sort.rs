dd945019967bf536a5fe23b1e4923005559055c7
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-05-04 20:28:38 +0700

    ref: rewrite tests

--- src/sorting/heap_sort.rs
+++ src/sorting/heap_sort.rs
@@ -79,41 +79,35 @@ pub fn heap_sort<T: Ord>(arr: &mut [T], ascending: bool) {
 
 #[cfg(test)]
 mod tests {
-    const HEAP_SORT_TEST_CASES: &[&[isize]] = &[
-        &[],
-        &[5],
-        &[1, 2, 3, 4, 5],
-        &[9, 8, 7, 6, 5],
-        &[8, 3, 1, 5, 7],
-        &[5, 5, 5, 5, 5],
-    ];
+    use crate::sorting::{have_same_elements, heap_sort, is_descending_sorted, is_sorted};
 
-    macro_rules! heap_sort_tests {
-        ($function:ident) => {
-            mod $function {
-                use super::*;
-                use crate::sorting::is_descending_sorted;
-                use crate::sorting::is_sorted;
-
-                fn run_test_case(input: &[isize]) {
-                    let mut arr_asc = input.to_vec();
-                    super::super::$function(&mut arr_asc, true);
-                    assert!(is_sorted(&arr_asc));
-
-                    let mut arr_desc = input.to_vec();
-                    super::super::$function(&mut arr_desc, false);
-                    assert!(is_descending_sorted(&arr_desc));
-                }
-
+    macro_rules! test_heap_sort {
+        ($($name:ident: $input:expr,)*) => {
+            $(
                 #[test]
-                fn test_heap_sort() {
-                    for &input in HEAP_SORT_TEST_CASES.iter() {
-                        run_test_case(input);
-                    }
+                fn $name() {
+                    let input_array = $input;
+                    let mut arr_asc = input_array.clone();
+                    heap_sort(&mut arr_asc, true);
+                    assert!(is_sorted(&arr_asc) && have_same_elements(&arr_asc, &input_array));
+
+                    let mut arr_dsc = input_array.clone();
+                    heap_sort(&mut arr_dsc, false);
+                    assert!(is_descending_sorted(&arr_dsc) && have_same_elements(&arr_dsc, &input_array));
                 }
-            }
-        };
+            )*
+        }
     }
 
-    heap_sort_tests!(heap_sort);
+    test_heap_sort! {
+        empty_array: Vec::<i32>::new(),
+        single_element_array: vec![5],
+        sorted: vec![1, 2, 3, 4, 5],
+        sorted_desc: vec![5, 4, 3, 2, 1, 0],
+        basic_0: vec![9, 8, 7, 6, 5],
+        basic_1: vec![8, 3, 1, 5, 7],
+        basic_2: vec![4, 5, 7, 1, 2, 3, 2, 8, 5, 4, 9, 9, 100, 1, 2, 3, 6, 4, 3],
+        duplicated_elements: vec![5, 5, 5, 5, 5],
+        strings: vec!["aa", "a", "ba", "ab"],
+    }
 }
