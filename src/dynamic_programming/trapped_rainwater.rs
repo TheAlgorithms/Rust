d5051501a6f1759af8913cc14b98cafd5d3a820b
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-05-14 08:19:08 +0700

    ref: change `Vec<u32` to `[u32]`

--- src/dynamic_programming/trapped_rainwater.rs
+++ src/dynamic_programming/trapped_rainwater.rs
@@ -9,26 +9,14 @@
 /// # Returns
 ///
 /// The total amount of trapped rainwater.
-pub fn trapped_rainwater(elevation_map: Vec<u32>) -> u32 {
+pub fn trapped_rainwater(elevation_map: &[u32]) -> u32 {
     if elevation_map.is_empty() {
         return 0;
     }
 
-    let mut left_max = vec![0; elevation_map.len()];
-    let mut right_max = vec![0; elevation_map.len()];
+    let left_max = calculate_max(elevation_map, true);
+    let right_max = calculate_max(elevation_map, false);
     let mut water_trapped = 0;
-
-    // Calculate left_max array
-    left_max[0] = elevation_map[0];
-    for i in 1..elevation_map.len() {
-        left_max[i] = left_max[i - 1].max(elevation_map[i]);
-    }
-
-    // Calculate right_max array
-    right_max[elevation_map.len() - 1] = elevation_map[elevation_map.len() - 1];
-    for i in (0..(elevation_map.len() - 1)).rev() {
-        right_max[i] = right_max[i + 1].max(elevation_map[i]);
-    }
 
     // Calculate trapped water
     for i in 0..elevation_map.len() {
@@ -36,6 +24,37 @@ pub fn trapped_rainwater(elevation_map: Vec<u32>) -> u32 {
     }
 
     water_trapped
+}
+
+/// Calculates the maximum array of the given elevation map based on the direction.
+///
+/// # Arguments
+///
+/// * `elevation_map` - A reference to a slice representing the elevation map where each element
+///   represents the height of the terrain at that position.
+/// * `direction` - A boolean indicating whether to calculate the maximum array from left to right (`true`)
+///   or from right to left (`false`).
+///
+/// # Returns
+///
+/// A vector representing the maximum array.
+fn calculate_max(elevation_map: &[u32], direction: bool) -> Vec<u32> {
+    let mut max_array = vec![0; elevation_map.len()];
+    match direction {
+        true => {
+            max_array[0] = elevation_map[0];
+            for i in 1..elevation_map.len() {
+                max_array[i] = max_array[i - 1].max(elevation_map[i]);
+            }
+        }
+        false => {
+            max_array[elevation_map.len() - 1] = elevation_map[elevation_map.len() - 1];
+            for i in (0..(elevation_map.len() - 1)).rev() {
+                max_array[i] = max_array[i + 1].max(elevation_map[i]);
+            }
+        }
+    }
+    max_array
 }
 
 #[cfg(test)]
@@ -48,7 +67,7 @@ mod tests {
                 #[test]
                 fn $name() {
                     let (elevation_map, expected_trapped_water) = $test_case;
-                    assert_eq!(trapped_rainwater(elevation_map), expected_trapped_water);
+                    assert_eq!(trapped_rainwater(&elevation_map), expected_trapped_water);
                 }
             )*
         };
@@ -56,35 +75,35 @@ mod tests {
 
     trapped_rainwater_tests! {
         test_trapped_rainwater_basic: (
-            vec![0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1],
+            [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1],
             6
         ),
         test_bucket: (
-            vec![5, 1, 5],
+            [5, 1, 5],
             4
         ),
         test_skewed_bucket: (
-            vec![4, 1, 5],
+            [4, 1, 5],
             3
         ),
         test_trapped_rainwater_empty: (
-            Vec::new(),
+            [],
             0
         ),
         test_trapped_rainwater_flat: (
-            vec![0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
             0
         ),
         test_trapped_rainwater_no_trapped_water: (
-            vec![1, 1, 2, 4, 0, 0, 0],
+            [1, 1, 2, 4, 0, 0, 0],
             0
         ),
         test_trapped_rainwater_single_elevation_map: (
-            vec![5],
+            [5],
             0
         ),
         test_trapped_rainwater_large_elevation_map_difference: (
-            vec![5, 1, 6, 1, 7, 1, 8],
+            [5, 1, 6, 1, 7, 1, 8],
             15
         ),
     }
