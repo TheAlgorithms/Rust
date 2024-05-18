74818609c6487fa09e939b53a860bc6e8945b6d0
Gilles Meunier <sellig6792@gmail.com>
2024-05-18 19:24:20 +0200

    Apply requested changes

--- src/geometry/ramer_douglas_peucker.rs
+++ src/geometry/ramer_douglas_peucker.rs
@@ -36,16 +36,27 @@ fn perpendicular_distance(p: &Point, a: &Point, b: &Point) -> f64 {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_perpendicular_distance() {
-        let a = Point::new(0.0, 0.0);
-        let b = Point::new(0.0, 3.0);
-        let p = Point::new(4.0, 0.0);
-        assert_eq!(perpendicular_distance(&p, &a, &b), 4.0);
+    macro_rules! test_perpendicular_distance {
+        ($($name:ident: $test_case:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (p, a, b, expected) = $test_case;
+                    assert_eq!(perpendicular_distance(&p, &a, &b), expected);
+                    assert_eq!(perpendicular_distance(&p, &b, &a), expected);
+                }
+            )*
+        };
+    }
+
+    test_perpendicular_distance! {
+        basic: (Point::new(4.0, 0.0), Point::new(0.0, 0.0), Point::new(0.0, 3.0), 4.0),
+        basic_shifted_1: (Point::new(4.0, 1.0), Point::new(0.0, 1.0), Point::new(0.0, 4.0), 4.0),
+        basic_shifted_2: (Point::new(2.0, 1.0), Point::new(-2.0, 1.0), Point::new(-2.0, 4.0), 4.0),
     }
 
     #[test]
-    fn test_ramer_douglas_peucker() {
+    fn test_ramer_douglas_peucker_polygon() {
         let a = Point::new(0.0, 0.0);
         let b = Point::new(1.0, 0.0);
         let c = Point::new(2.0, 0.0);
@@ -70,17 +81,35 @@ mod tests {
     }
 
     #[test]
-    fn test_polygon_chain() {
+    fn test_ramer_douglas_peucker_polygonal_chain() {
         let a = Point::new(0., 0.);
         let b = Point::new(2., 0.5);
         let c = Point::new(3., 3.);
         let d = Point::new(6., 3.);
         let e = Point::new(8., 4.);
 
-        let polygon = vec![a.clone(), b, c, d, e.clone()];
+        let points = vec![a.clone(), b, c, d, e.clone()];
 
         let epsilon = 3.; // The epsilon is quite large, so the result will be a single line
-        let result = ramer_douglas_peucker(&polygon, epsilon);
+        let result = ramer_douglas_peucker(&points, epsilon);
         assert_eq!(result, vec![a, e]);
+    }
+
+    #[test]
+    fn test_less_than_three_points() {
+        let a = Point::new(0., 0.);
+        let b = Point::new(1., 1.);
+
+        let epsilon = 0.1;
+
+        assert_eq!(ramer_douglas_peucker(&[], epsilon), vec![]);
+        assert_eq!(
+            ramer_douglas_peucker(&[a.clone()], epsilon),
+            vec![a.clone()]
+        );
+        assert_eq!(
+            ramer_douglas_peucker(&[a.clone(), b.clone()], epsilon),
+            vec![a, b]
+        );
     }
 }
