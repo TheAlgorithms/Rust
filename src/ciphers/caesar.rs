465fa59c0a7a6ef637ed617780147f39d47f04a9
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2024-05-19 14:53:06 -0300

    Improvement in Caesar algorithm testing

--- src/ciphers/caesar.rs
+++ src/ciphers/caesar.rs
@@ -60,98 +60,58 @@ fn shift_char(c: char, rotation: isize) -> char {
 mod tests {
     use super::*;
 
-    #[test]
-    fn empty() {
-        assert_eq!(caesar("", 13), "");
+    macro_rules! test_caesar {
+        ($($name:ident: $test_case:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (text, rotation, expected) = $test_case;
+
+                    match rotation {
+                        0..=25 => {
+                            // Test forward rotation
+                            match caesar(&text, rotation) {
+                                Ok(result) => assert_eq!(result, expected),
+                                Err(e) => panic!("Unexpected error: {}", e),
+                            }
+
+                            // Test backward rotation
+                            let backward_rotation = if rotation == 0 { 0 } else { 26 - rotation };
+                            match caesar(&expected, backward_rotation) {
+                                Ok(result) => assert_eq!(result, text),
+                                Err(e) => assert_eq!(e, ERROR_MESSAGE),
+                            }
+                        },
+                        _ => {
+                            // If rotation is invalid, test that the function returns the specific error message
+                            match caesar(&text, rotation) {
+                                Ok(_) => panic!("Expected an error but got an Ok result"),
+                                Err(e) => assert_eq!(e, ERROR_MESSAGE),
+                            }
+                        }
+                    }
+                }
+            )*
+        };
     }
 
-    #[test]
-    fn rot_13() {
-        assert_eq!(caesar("rust", 13), "ehfg");
-    }
-
-    #[test]
-    fn unicode() {
-        assert_eq!(caesar("attack at dawn 攻", 5), "fyyfhp fy ifbs 攻");
-    }
-
-    #[test]
-    fn rotation_within_alphabet_range() {
-        assert_eq!(caesar("Hello, World!", 3), "Khoor, Zruog!");
-    }
-
-    #[test]
-    fn rotation_at_alphabet_beginning() {
-        assert_eq!(caesar("Hello, World!", 0), "Hello, World!");
-    }
-
-    #[test]
-    fn rotation_at_alphabet_end() {
-        assert_eq!(caesar("Hello, World!", 25), "Gdkkn, Vnqkc!");
-    }
-
-    #[test]
-    fn encryption_and_decryption() {
-        let original_text = "The quick brown fox jumps over the lazy dog.";
-        let rotation = 5;
-        let encrypted_text = caesar(original_text, rotation);
-
-        assert_eq!(caesar(&encrypted_text, 26 - rotation), original_text);
-    }
-
-    #[test]
-    fn empty_input() {
-        assert_eq!(caesar("", 10), "");
-    }
-
-    #[test]
-    fn input_non_alphabetic_characters() {
-        assert_eq!(caesar("12345!@#$%", 3), "12345!@#$%");
-    }
-
-    #[test]
-    fn input_uppercase_letters() {
-        assert_eq!(
-            caesar("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 1),
-            "BCDEFGHIJKLMNOPQRSTUVWXYZA"
-        );
-    }
-
-    #[test]
-    fn input_mixed_case() {
-        assert_eq!(caesar("HeLlO WoRlD", 7), "OlSsV DvYsK");
-    }
-
-    #[test]
-    fn input_with_whitespace() {
-        assert_eq!(caesar("Hello, World!", 13), "Uryyb, Jbeyq!");
-    }
-
-    #[test]
-    fn input_with_special_characters() {
-        assert_eq!(
-            caesar("Hello!@#$%^&*()_+World", 4),
-            "Lipps!@#$%^&*()_+Asvph"
-        );
-    }
-
-    #[test]
-    fn input_with_numbers() {
-        assert_eq!(caesar("Abcd1234XYZ", 10), "Klmn1234HIJ");
-    }
-
-    #[test]
-    fn large_rotation() {
-        assert_eq!(caesar("Large rotation", 139), "Ujapn axcjcrxw");
-    }
-
-    #[test]
-    fn large_rotation_with_big_input() {
-        assert_eq!(caesar("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices ante, at gravida ante. Quisque luctus, ligula nec dictum facilisis, elit leo luctus arcu, ut auctor sapien turpis ut mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vel orci sit amet sem efficitur sagittis a quis augue. Donec semper quam tincidunt hendrerit cursus. Duis placerat gravida diam, in interdum purus dapibus in.", 139), "Uxanv rybdv mxuxa brc jvnc, lxwbnlcncda jmryrblrwp nurc. Ldajkrcda jl ducarlnb jwcn, jc pajermj jwcn. Zdrbzdn udlcdb, urpduj wnl mrlcdv ojlrurbrb, nurc unx udlcdb jald, dc jdlcxa bjyrnw cdayrb dc vjdarb. Xalr ejardb wjcxzdn ynwjcrkdb nc vjpwrb mrb yjacdarnwc vxwcnb, wjblncda armrldudb vdb. Wduuj enu xalr brc jvnc bnv noorlrcda bjprccrb j zdrb jdpdn. Mxwnl bnvyna zdjv crwlrmdwc qnwmanarc ldabdb. Mdrb yujlnajc pajermj mrjv, rw rwcnamdv ydadb mjyrkdb rw.");
-    }
-
-    #[test]
-    fn very_large_rotation_with_big_input() {
-        assert_eq!(caesar("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices ante, at gravida ante. Quisque luctus, ligula nec dictum facilisis, elit leo luctus arcu, ut auctor sapien turpis ut mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vel orci sit amet sem efficitur sagittis a quis augue. Donec semper quam tincidunt hendrerit cursus. Duis placerat gravida diam, in interdum purus dapibus in.", 345876), "Jmpck gnqsk bmjmp qgr ykcr, amlqcarcrsp ybgngqagle cjgr. Aspyzgrsp ya sjrpgacq ylrc, yr epytgby ylrc. Osgqosc jsarsq, jgesjy lca bgarsk dyagjgqgq, cjgr jcm jsarsq ypas, sr ysarmp qyngcl rspngq sr kyspgq. Mpag typgsq lyrmosc nclyrgzsq cr kyelgq bgq nyprspgclr kmlrcq, lyqacrsp pgbgasjsq ksq. Lsjjy tcj mpag qgr ykcr qck cddgagrsp qyegrrgq y osgq ysesc. Bmlca qckncp osyk rglagbslr fclbpcpgr aspqsq. Bsgq njyacpyr epytgby bgyk, gl glrcpbsk nspsq byngzsq gl.");
+    test_caesar! {
+        empty_text: ("", 13, ""),
+        rot_13: ("rust", 13, "ehfg"),
+        unicode: ("attack at dawn 攻", 5, "fyyfhp fy ifbs 攻"),
+        rotation_within_alphabet_range: ("Hello, World!", 3, "Khoor, Zruog!"),
+        no_rotation: ("Hello, World!", 0, "Hello, World!"),
+        negative_rotation: ("Hello, World!", -5, "Hello, World!"),
+        rotation_at_alphabet_end: ("Hello, World!", 25, "Gdkkn, Vnqkc!"),
+        longer: ("The quick brown fox jumps over the lazy dog.", 5, "Ymj vznhp gwtbs ktc ozrux tajw ymj qfed itl."),
+        non_alphabetic_characters: ("12345!@#$%", 3, "12345!@#$%"),
+        uppercase_letters: ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 1, "BCDEFGHIJKLMNOPQRSTUVWXYZA"),
+        mixed_case: ("HeLlO WoRlD", 7, "OlSsV DvYsK"),
+        with_whitespace: ("Hello, World!", 13, "Uryyb, Jbeyq!"),
+        with_special_characters: ("Hello!@#$%^&*()_+World", 4, "Lipps!@#$%^&*()_+Asvph"),
+        with_numbers: ("Abcd1234XYZ", 10, "Klmn1234HIJ"),
+        large_rotation: ("Large rotation", 139, "Ujapn axcjcrxw"),
+        large_rotation_with_big_input: ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices ante, at gravida ante. Quisque luctus, ligula nec dictum facilisis, elit leo luctus arcu, ut auctor sapien turpis ut mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vel orci sit amet sem efficitur sagittis a quis augue. Donec semper quam tincidunt hendrerit cursus. Duis placerat gravida diam, in interdum purus dapibus in.", 139, "Uxanv rybdv mxuxa brc jvnc, lxwbnlcncda jmryrblrwp nurc. Ldajkrcda jl ducarlnb jwcn, jc pajermj jwcn. Zdrbzdn udlcdb, urpduj wnl mrlcdv ojlrurbrb, nurc unx udlcdb jald, dc jdlcxa bjyrnw cdayrb dc vjdarb. Xalr ejardb wjcxzdn ynwjcrkdb nc vjpwrb mrb yjacdarnwc vxwcnb, wjblncda armrldudb vdb. Wduuj enu xalr brc jvnc bnv noorlrcda bjprccrb j zdrb jdpdn. Mxwnl bnvyna zdjv crwlrmdwc qnwmanarc ldabdb. Mdrb yujlnajc pajermj mrjv, rw rwcnamdv ydadb mjyrkdb rw."),
+        very_large_rotation_with_big_input: ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices ante, at gravida ante. Quisque luctus, ligula nec dictum facilisis, elit leo luctus arcu, ut auctor sapien turpis ut mauris. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla vel orci sit amet sem efficitur sagittis a quis augue. Donec semper quam tincidunt hendrerit cursus. Duis placerat gravida diam, in interdum purus dapibus in.", 345876, "Jmpck gnqsk bmjmp qgr ykcr, amlqcarcrsp ybgngqagle cjgr. Aspyzgrsp ya sjrpgacq ylrc, yr epytgby ylrc. Osgqosc jsarsq, jgesjy lca bgarsk dyagjgqgq, cjgr jcm jsarsq ypas, sr ysarmp qyngcl rspngq sr kyspgq. Mpag typgsq lyrmosc nclyrgzsq cr kyelgq bgq nyprspgclr kmlrcq, lyqacrsp pgbgasjsq ksq. Lsjjy tcj mpag qgr ykcr qck cddgagrsp qyegrrgq y osgq ysesc. Bmlca qckncp osyk rglagbslr fclbpcpgr aspqsq. Bsgq njyacpyr epytgby bgyk, gl glrcpbsk nspsq byngzsq gl."),
     }
 }
