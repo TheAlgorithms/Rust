769638a4cb9a63662cb1e0d4f7df246ebc0890cb
Haswanth Dasiga <haswanth@Ramachandras-MacBook-Pro.local>
2024-05-28 18:04:31 -0700

    Add Support for Multiple Dimensions & Replace Enum with Function Pointers for KNN Implementation
    This commit
    1. Replaces the Enum used for Distance Computations with Function Pointers
    2. Adds Support for Multiple Dimensions. Previoulsy the algorithm was only considering a two dimensional data. New change lets the input be n-dimension's data.
    3. Eucledian Distance Formula is applied for n-dimension data.

--- src/machine_learning/optimization/k_nearest_neighbors.rs
+++ src/machine_learning/optimization/k_nearest_neighbors.rs
@@ -1,50 +1,44 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use crate::math::{abs, square_root};
+use num_traits::pow;
 use std::{
     cmp,
     collections::{BTreeMap, HashMap},
 };
-
-use crate::math::{abs, square_root};
 #[derive(Debug, Clone)]
 pub struct Point {
-    x: f64,        // x-axis
-    y: f64,        // y-axis
-    label: String, // A label denotes the classification of the data. For instance, 'Football' might be labeled as 'Sport'
+    data: Vec<f64>,
+    label: String,
 }
 
 impl Point {
-    pub fn build(x: f64, y: f64, label: String) -> Self {
-        Self { x, y, label }
+    pub fn build(data: Vec<f64>, label: String) -> Self {
+        Self { data, label }
     }
 }
 
-pub enum DistanceMeasurementFormula {
-    EucledianDistance, // The Euclidean Distance Formula will be employed to calculate the distance between two points
-}
+fn eucledian_distance(source_point: &Point, destination_point: &Point) -> f64 {
+    let mut distance = 0.0;
 
-impl DistanceMeasurementFormula {
-    fn distance(&self, source_point: &Point, destination_point: &Point) -> f64 {
-        match self {
-            Self::EucledianDistance => Self::eucledian_distance(source_point, destination_point),
+    if destination_point.data.len() == source_point.data.len() {
+        for i in 0..destination_point.data.len() {
+            distance += pow(
+                destination_point.data.get(i).unwrap() - source_point.data.get(i).unwrap(),
+                2,
+            );
         }
     }
-
-    fn eucledian_distance(source_point: &Point, destination_point: &Point) -> f64 {
-        let distance = square_root(
-            (destination_point.x - source_point.x) * (destination_point.x - source_point.x)
-                + (destination_point.y - source_point.y) * (destination_point.y - source_point.y),
-        );
-        abs(distance)
-    }
+    distance = square_root(distance);
+    abs(distance)
 }
 
 pub fn classify_using_knn(
     neighbors: Vec<&Point>, // The training data which essentially consists of a set of points on the X-Y axis represented as vector of Points,
     input_point: Point,     // The input point requiring classification
     k: usize, // The value of 'K'. For example, if K equals 4, classification is determined by the majority vote among the 4 nearest neighbors
-    distance_computation: DistanceMeasurementFormula, // An enum employed to specify the technique/formula for calculating the distance between two points
+    distance_computation: fn(source_point: &Point, destination_point: &Point) -> f64, // An enum employed to specify the technique/formula for calculating the distance between two points
 ) -> Point {
     // fetch k nearest neighbors for the input_point
     let k_nearest_neighbors =
@@ -54,7 +48,7 @@ pub fn classify_using_knn(
     let label = find_majority_label(k_nearest_neighbors).to_owned();
 
     //update the input point with the right label after classification
-    Point::build(input_point.x, input_point.y, label)
+    Point::build(input_point.data, label)
 }
 
 // This method identifies the label that appears most frequently among the k nearest neighbors
@@ -83,7 +77,7 @@ fn fetch_k_nearest_neighbors<'a>(
     neighbors: &'a Vec<&'a Point>,
     input_point: &'a Point,
     k: &usize,
-    distance_computation: DistanceMeasurementFormula,
+    distance_computation: fn(source_point: &Point, destination_point: &Point) -> f64,
 ) -> Vec<&'a Point> {
     //calculate the distance to all neighbors from input_point.
     let distance_map: BTreeMap<i64, &Point> =
@@ -98,12 +92,12 @@ fn fetch_k_nearest_neighbors<'a>(
 fn calculate_distance_to_neighbors<'a>(
     neighbors: &'a Vec<&'a Point>,
     input_point: &'a Point,
-    distance_computation: DistanceMeasurementFormula,
+    distance_computation: fn(source_point: &Point, destination_point: &Point) -> f64,
 ) -> BTreeMap<i64, &'a Point> {
     let mut distance_map: BTreeMap<i64, &Point> = BTreeMap::new();
 
     for neighbor in neighbors {
-        let distance: f64 = distance_computation.distance(input_point, neighbor);
+        let distance: f64 = distance_computation(input_point, neighbor);
         //multiplying by 100 since floating point numbers cant be keys. Any precission more than 2 digits is ignored
         distance_map.insert((distance * 100.0) as i64, neighbor);
     }
@@ -126,168 +120,164 @@ fn return_k_closest_neighbors<'a>(
 mod tests {
     use super::*;
 
-    fn construct_point_data(x: f64, y: f64, label: String) -> Point {
-        Point { x, y, label }
+    fn construct_point_data(data: Vec<f64>, label: String) -> Point {
+        Point { data, label }
     }
 
     #[test]
     fn test_fetch_k_nearest_neighbors() {
-        let point_1 = construct_point_data(2.0, 2.2, String::from("B"));
+        let point_1 = construct_point_data(vec![2.0, 2.2], String::from("B"));
 
-        let point_2 = construct_point_data(3.0, 2.2, String::from("A"));
+        let point_2 = construct_point_data(vec![3.0, 2.2], String::from("A"));
 
-        let point_3 = construct_point_data(1.0, 2.2, String::from("A"));
+        let point_3 = construct_point_data(vec![1.0, 2.2], String::from("A"));
 
-        let point_4 = construct_point_data(0.1, 2.2, String::from("B"));
+        let point_4 = construct_point_data(vec![0.1, 2.2], String::from("B"));
 
-        let point_5 = construct_point_data(0.2, 2.2, String::from("B"));
+        let point_5 = construct_point_data(vec![0.2, 2.2], String::from("B"));
 
         let neighbors: Vec<&Point> = vec![&point_1, &point_2, &point_3, &point_4, &point_5];
-        let input_point = construct_point_data(1.0, 2.2, String::new());
+        let input_point = construct_point_data(vec![1.0, 2.2], String::new());
         let k: usize = 2;
-        let result = fetch_k_nearest_neighbors(
-            &neighbors,
-            &input_point,
-            &k,
-            DistanceMeasurementFormula::EucledianDistance,
-        );
+        let result = fetch_k_nearest_neighbors(&neighbors, &input_point, &k, eucledian_distance);
         assert_eq!(result.len(), 2);
         let test_point_1 = &point_5;
-        assert_eq!(result.get(1).unwrap().x, test_point_1.x);
+        assert_eq!(result.get(1).unwrap().data, test_point_1.data);
+    }
+
+    #[test]
+    fn test_fetch_k_nearest_neighbors_for_3_dimension_data() {
+        let point_1 = construct_point_data(vec![1.0, 2.2, 2.0], String::from("B"));
+
+        let point_2 = construct_point_data(vec![3.0, 2.2, 1.0], String::from("A"));
+
+        let point_3 = construct_point_data(vec![1.0, 2.2, 1.0], String::from("A"));
+
+        let point_4 = construct_point_data(vec![0.1, 2.2, 1.0], String::from("B"));
+
+        let point_5 = construct_point_data(vec![0.2, 2.2, 1.0], String::from("B"));
+
+        let neighbors: Vec<&Point> = vec![&point_1, &point_2, &point_3, &point_4, &point_5];
+        let input_point = construct_point_data(vec![1.0, 2.2, 3.0], String::new());
+        let k: usize = 2;
+        let result = fetch_k_nearest_neighbors(&neighbors, &input_point, &k, eucledian_distance);
+        assert_eq!(result.len(), 2);
+        let test_point_1 = &point_1;
+        assert_eq!(result.first().unwrap().data, test_point_1.data);
+        let test_point_2 = &point_3;
+        assert_eq!(result.get(1).unwrap().data, test_point_2.data);
     }
 
     #[test]
     fn test_fetch_k_nearest_neighbors_for_negative_signed_input() {
-        let point_1 = construct_point_data(2.0, 2.2, String::from("B"));
+        let point_1 = construct_point_data(vec![2.0, 2.2], String::from("B"));
 
-        let point_2 = construct_point_data(3.0, 2.2, String::from("A"));
+        let point_2 = construct_point_data(vec![3.0, 2.2], String::from("A"));
 
-        let point_3 = construct_point_data(1.0, 2.2, String::from("A"));
+        let point_3 = construct_point_data(vec![1.0, 2.2], String::from("A"));
 
-        let point_4 = construct_point_data(0.1, 2.2, String::from("B"));
+        let point_4 = construct_point_data(vec![0.1, 2.2], String::from("B"));
 
-        let point_5 = construct_point_data(0.2, 2.2, String::from("B"));
+        let point_5 = construct_point_data(vec![0.2, 2.2], String::from("B"));
 
         let neighbors: Vec<&Point> = vec![&point_1, &point_2, &point_3, &point_4, &point_5];
-        let input_point = construct_point_data(-1.0, -2.2, String::new());
+        let input_point = construct_point_data(vec![-1.0, -2.2], String::new());
         let k: usize = 2;
-        let result = fetch_k_nearest_neighbors(
-            &neighbors,
-            &input_point,
-            &k,
-            DistanceMeasurementFormula::EucledianDistance,
-        );
+        let result = fetch_k_nearest_neighbors(&neighbors, &input_point, &k, eucledian_distance);
         assert_eq!(result.len(), 2);
 
         let first_closest_point = &point_4;
-        assert_eq!(result.first().unwrap().x, first_closest_point.x);
+        assert_eq!(result.first().unwrap().data, first_closest_point.data);
 
         let second_closest_point = &point_5;
-        assert_eq!(result.get(1).unwrap().x, second_closest_point.x);
+        assert_eq!(result.get(1).unwrap().data, second_closest_point.data);
     }
 
     #[test]
     fn test_fetch_k_nearest_neighbors_for_duplicate_inputs() {
-        let point_1 = construct_point_data(0.0, 2.2, String::from("B"));
+        let point_1 = construct_point_data(vec![0.0, 2.2], String::from("B"));
 
-        let point_2 = construct_point_data(0.0, 2.2, String::from("A"));
+        let point_2 = construct_point_data(vec![0.0, 2.2], String::from("A"));
 
-        let point_3 = construct_point_data(0.0, 2.2, String::from("A"));
+        let point_3 = construct_point_data(vec![0.0, 2.2], String::from("A"));
 
-        let point_4 = construct_point_data(0.0, 2.2, String::from("B"));
+        let point_4 = construct_point_data(vec![0.0, 2.2], String::from("B"));
 
-        let point_5 = construct_point_data(0.0, 2.2, String::from("B"));
+        let point_5 = construct_point_data(vec![0.0, 2.2], String::from("B"));
 
         let neighbors: Vec<&Point> = vec![&point_1, &point_2, &point_3, &point_4, &point_5];
-        let input_point = construct_point_data(-1.0, -2.2, String::new());
+        let input_point = construct_point_data(vec![-1.0, -2.2], String::new());
         let k: usize = 2;
 
-        let result = fetch_k_nearest_neighbors(
-            &neighbors,
-            &input_point,
-            &k,
-            DistanceMeasurementFormula::EucledianDistance,
-        );
+        let result = fetch_k_nearest_neighbors(&neighbors, &input_point, &k, eucledian_distance);
         assert_eq!(result.len(), 1);
 
         let first_closest_point = &point_4;
-        assert_eq!(result.first().unwrap().x, first_closest_point.x);
+        assert_eq!(result.first().unwrap().data, first_closest_point.data);
     }
 
     #[test]
     fn test_classify_input_for_2_nearest_neighbors() {
-        let point_1 = construct_point_data(1.0, 2.2, String::from("B"));
+        let point_1 = construct_point_data(vec![1.0, 2.2], String::from("B"));
 
-        let point_2 = construct_point_data(1.0, 2.2, String::from("A"));
+        let point_2 = construct_point_data(vec![1.0, 2.2], String::from("A"));
 
-        let point_3 = construct_point_data(2.0, 2.2, String::from("A"));
+        let point_3 = construct_point_data(vec![2.0, 2.2], String::from("A"));
 
-        let point_4 = construct_point_data(3.0, 2.2, String::from("A"));
+        let point_4 = construct_point_data(vec![3.0, 2.2], String::from("A"));
 
-        let point_5 = construct_point_data(4.0, 2.2, String::from("A"));
+        let point_5 = construct_point_data(vec![4.0, 2.2], String::from("A"));
 
         let neighbors: Vec<&Point> = vec![&point_1, &point_2, &point_3, &point_4, &point_5];
 
-        let input_point = construct_point_data(-1.0, -2.2, String::new());
+        let input_point = construct_point_data(vec![-1.0, -2.2], String::new());
 
-        let classified_input_point = classify_using_knn(
-            neighbors,
-            input_point.clone(),
-            2,
-            DistanceMeasurementFormula::EucledianDistance,
-        );
+        let classified_input_point =
+            classify_using_knn(neighbors, input_point.clone(), 2, eucledian_distance);
 
         assert_eq!(classified_input_point.clone().label, point_2.label);
     }
 
     #[test]
     fn test_classify_input_for_4_nearest_neighbors() {
-        let point_1 = construct_point_data(0.0, 2.2, String::from("B"));
+        let point_1 = construct_point_data(vec![0.0, 2.2], String::from("B"));
 
-        let point_2 = construct_point_data(1.0, 2.2, String::from("A"));
+        let point_2 = construct_point_data(vec![1.0, 2.2], String::from("A"));
 
-        let point_3 = construct_point_data(2.0, 2.2, String::from("B"));
+        let point_3 = construct_point_data(vec![2.0, 2.2], String::from("B"));
 
-        let point_4 = construct_point_data(3.0, 2.2, String::from("B"));
+        let point_4 = construct_point_data(vec![3.0, 2.2], String::from("B"));
 
-        let point_5 = construct_point_data(4.0, 2.2, String::from("A"));
+        let point_5 = construct_point_data(vec![4.0, 2.2], String::from("A"));
 
         let neighbors: Vec<&Point> = vec![&point_1, &point_2, &point_3, &point_4, &point_5];
 
-        let input_point = construct_point_data(-1.0, -2.2, String::new());
+        let input_point = construct_point_data(vec![-1.0, -2.2], String::new());
 
-        let classified_input_point = classify_using_knn(
-            neighbors,
-            input_point.clone(),
-            4,
-            DistanceMeasurementFormula::EucledianDistance,
-        );
+        let classified_input_point =
+            classify_using_knn(neighbors, input_point.clone(), 4, eucledian_distance);
 
         assert_eq!(classified_input_point.clone().label, String::from("B"));
     }
 
     #[test]
     fn test_classify_input_for_equal_majority() {
-        let point_1 = construct_point_data(0.0, 2.2, String::from("B"));
+        let point_1 = construct_point_data(vec![0.0, 2.2], String::from("B"));
 
-        let point_2 = construct_point_data(1.0, 2.2, String::from("A"));
+        let point_2 = construct_point_data(vec![1.0, 2.2], String::from("A"));
 
-        let point_3 = construct_point_data(2.0, 2.2, String::from("A"));
+        let point_3 = construct_point_data(vec![2.0, 2.2], String::from("A"));
 
-        let point_4 = construct_point_data(3.0, 2.2, String::from("B"));
+        let point_4 = construct_point_data(vec![3.0, 2.2], String::from("B"));
 
-        let point_5 = construct_point_data(4.0, 2.2, String::from("C"));
+        let point_5 = construct_point_data(vec![4.0, 2.2], String::from("C"));
 
         let neighbors: Vec<&Point> = vec![&point_1, &point_2, &point_3, &point_4, &point_5];
 
-        let input_point = construct_point_data(-1.0, -2.2, String::new());
+        let input_point = construct_point_data(vec![-1.0, -2.2], String::new());
 
-        let classified_input_point = classify_using_knn(
-            neighbors,
-            input_point.clone(),
-            4,
-            DistanceMeasurementFormula::EucledianDistance,
-        );
+        let classified_input_point =
+            classify_using_knn(neighbors, input_point.clone(), 4, eucledian_distance);
 
         assert_ne!(classified_input_point.clone().label, String::from("C"));
     }
