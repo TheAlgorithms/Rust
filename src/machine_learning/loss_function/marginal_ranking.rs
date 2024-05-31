d94e3ef9019ab830b7df707f9fb26c3d290256ad
Javier Kauer <kauer.javier01@gmail.com>
2024-05-31 14:18:38 +0200

    Feat: finished implementation and changed type of input

--- src/machine_learning/loss_function/marginal_ranking.rs
+++ src/machine_learning/loss_function/marginal_ranking.rs
@@ -13,4 +13,11 @@
 // It returns the average loss by dividing the `total_loss` by total no. of
 // elements.
 
-pub fn mrg_ranking_loss(x_first: &[f64], x_second: &[f64], margin: f64, y_true: i8) -> f64 {}
+pub fn mrg_ranking_loss(x_first: &[f64], x_second: &[f64], margin: f64, y_true: f64) -> f64 {
+    let mut total_loss: f64 = 0.0;
+    for (f, s) in x_first.iter().zip(x_second.iter()) {
+        let loss: f64 = (margin - y_true * (f - s)).max(0.0);
+        total_loss += loss;
+    }
+    total_loss / (x_first.len() as f64)
+}
