cd8240d98b28e49b57fce59ecd559827d585c794
Javier Kauer <kauer.javier01@gmail.com>
2024-06-03 22:30:46 +0200

    Feat: implementation of the negative log likelihood

--- src/machine_learning/loss_function/negative_log_likelihood.rs
+++ src/machine_learning/loss_function/negative_log_likelihood.rs
@@ -13,4 +13,11 @@
 // It returns the average loss by dividing the `total_loss` by total no. of
 // elements.
 
-pub fn neg_log_likelihood(y_true: &[f64], y_pred: &[f64]) -> f64 {}
+pub fn neg_log_likelihood(y_true: &[f64], y_pred: &[f64]) -> f64 {
+    let mut total_loss: f64 = 0.0;
+    for (p, a) in y_pred.iter().zip(y_true.iter()) {
+        let loss: f64 = -a * p.ln() - (1.0 - a) * (1.0 - p).ln();
+        total_loss += loss;
+    }
+    total_loss / (y_pred.len() as f64)
+}
