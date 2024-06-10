a513a55e8b13cc6e922b89b909ed2f3b0c4f405c
Jerrit Gläsker <jrgls.no-reply@gmx.de>
2024-06-10 15:30:31 +0200

    Cargo fmt

--- src/math/minkowski_difference.rs
+++ src/math/minkowski_difference.rs
@@ -5,7 +5,6 @@
 ///
 /// This metric is useful in the field of machine learning to judge the similarity between data points.
 /// It's also employed for collision detection in game engines.
-
 use num_traits::Pow;
 
 pub struct Point {
@@ -29,9 +28,11 @@ pub fn minkowski_difference(a: &Point, b: &Point, order: u32) -> Result<f64, Par
 
     let order = order as f64;
 
-    let sum = a.x.iter()
+    let sum =
+        a.x.iter()
             .zip(b.x.iter())
-            .map(|(x, y)| (x - y).abs().pow(order)).sum::<f64>();
+            .map(|(x, y)| (x - y).abs().pow(order))
+            .sum::<f64>();
 
     Ok(sum.pow(1.0 / order))
 }
@@ -47,8 +48,12 @@ mod tests {
 
         assert_eq!(minkowski_difference(&a, &b, 1), Ok(2.0));
 
-        let a = Point { x: vec![1.0, 2.0, 3.0, 4.0] };
-        let b = Point { x: vec![5.0, 6.0, 7.0, 8.0] };
+        let a = Point {
+            x: vec![1.0, 2.0, 3.0, 4.0],
+        };
+        let b = Point {
+            x: vec![5.0, 6.0, 7.0, 8.0],
+        };
 
         assert_eq!(minkowski_difference(&a, &b, 2), Ok(8.0));
     }
@@ -56,8 +61,13 @@ mod tests {
     #[test]
     fn test_minkowski_difference_different_dimensions() {
         let a = Point { x: vec![1.0, 1.0] };
-        let b = Point { x: vec![2.0, 2.0, 2.0] };
+        let b = Point {
+            x: vec![2.0, 2.0, 2.0],
+        };
 
-        assert_eq!(minkowski_difference(&a, &b, 1), Err(ParameterError::DifferentDimensions));
+        assert_eq!(
+            minkowski_difference(&a, &b, 1),
+            Err(ParameterError::DifferentDimensions)
+        );
     }
 }
