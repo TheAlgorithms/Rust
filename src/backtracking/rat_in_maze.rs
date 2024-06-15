cd8a1b5c04fc169a7b94d159ca983a9dbaf4bbef
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-06-15 18:41:51 +0700

    ref: change `maze` representation from `Vec<Vec<usize>>` to `Vec<Vec<bool>>`

--- src/backtracking/rat_in_maze.rs
+++ src/backtracking/rat_in_maze.rs
@@ -23,17 +23,17 @@
 /// taken by the rat, enabling us to reconstruct the solution and visualize the
 /// rat's journey through the maze.
 pub fn find_path_in_maze(
-    maze: &[Vec<usize>],
+    maze: &[Vec<bool>],
     start_x: usize,
     start_y: usize,
-) -> Option<Vec<Vec<usize>>> {
+) -> Option<Vec<Vec<bool>>> {
     let mut maze_instance = Maze::new(maze.to_owned());
     maze_instance.find_path(start_x, start_y)
 }
 
 /// Represents a maze.
 struct Maze {
-    maze: Vec<Vec<usize>>,
+    maze: Vec<Vec<bool>>,
 }
 
 impl Maze {
@@ -49,7 +49,7 @@ impl Maze {
     /// # Returns
     ///
     /// A new Maze instance.
-    fn new(maze: Vec<Vec<usize>>) -> Self {
+    fn new(maze: Vec<Vec<bool>>) -> Self {
         Maze { maze }
     }
 
@@ -81,8 +81,8 @@ impl Maze {
     /// # Returns
     ///
     /// A solution matrix if a path is found or None if not found.
-    fn find_path(&mut self, start_x: usize, start_y: usize) -> Option<Vec<Vec<usize>>> {
-        let mut solution = vec![vec![0; self.width()]; self.height()];
+    fn find_path(&mut self, start_x: usize, start_y: usize) -> Option<Vec<Vec<bool>>> {
+        let mut solution = vec![vec![false; self.width()]; self.height()];
         if self.solve(start_x as isize, start_y as isize, &mut solution) {
             Some(solution)
         } else {
@@ -101,14 +101,14 @@ impl Maze {
     /// # Returns
     ///
     /// A boolean indicating whether a solution was found.
-    fn solve(&self, x: isize, y: isize, solution: &mut [Vec<usize>]) -> bool {
+    fn solve(&self, x: isize, y: isize, solution: &mut [Vec<bool>]) -> bool {
         if x == (self.height() as isize - 1) && y == (self.width() as isize - 1) {
-            solution[x as usize][y as usize] = 1;
+            solution[x as usize][y as usize] = true;
             return true;
         }
 
         if self.is_valid(x, y, solution) {
-            solution[x as usize][y as usize] = 1;
+            solution[x as usize][y as usize] = true;
 
             for &(dx, dy) in &Self::MOVES {
                 if self.solve(x + dx, y + dy, solution) {
@@ -117,7 +117,7 @@ impl Maze {
             }
 
             // If none of the directions lead to the solution, backtrack
-            solution[x as usize][y as usize] = 0;
+            solution[x as usize][y as usize] = false;
             return false;
         }
         false
@@ -134,13 +134,13 @@ impl Maze {
     /// # Returns
     ///
     /// A boolean indicating whether the position is valid.
-    fn is_valid(&self, x: isize, y: isize, solution: &[Vec<usize>]) -> bool {
+    fn is_valid(&self, x: isize, y: isize, solution: &[Vec<bool>]) -> bool {
         x >= 0
             && y >= 0
             && x < self.height() as isize
             && y < self.width() as isize
-            && self.maze[x as usize].get(y as usize) == Some(&1)
-            && solution[x as usize][y as usize] == 0
+            && self.maze[x as usize].get(y as usize) == Some(&true)
+            && !solution[x as usize][y as usize]
     }
 }
 
@@ -156,7 +156,7 @@ mod tests {
                     let solution = find_path_in_maze($maze, $start_x, $start_y);
                     assert_eq!(solution, $expected);
                     if let Some(expected_solution) = &$expected {
-                        assert_eq!(expected_solution[$start_x][$start_y], 1);
+                        assert_eq!(expected_solution[$start_x][$start_y], true);
                     }
                 }
             )*
@@ -165,86 +165,100 @@ mod tests {
 
     test_find_path_in_maze! {
         maze_with_solution_5x5: 0, 0, &[
-            vec![1, 0, 1, 0, 0],
-            vec![1, 1, 0, 1, 0],
-            vec![0, 1, 1, 1, 0],
-            vec![0, 0, 0, 1, 1],
-            vec![0, 1, 0, 0, 1],
+            vec![true, false, true, false, false],
+            vec![true, true, false, true, false],
+            vec![false, true, true, true, false],
+            vec![false, false, false, true, true],
+            vec![false, true, false, false, true],
         ], Some(vec![
-            vec![1, 0, 0, 0, 0],
-            vec![1, 1, 0, 0, 0],
-            vec![0, 1, 1, 1, 0],
-            vec![0, 0, 0, 1, 1],
-            vec![0, 0, 0, 0, 1],
+            vec![true, false, false, false, false],
+            vec![true, true, false, false, false],
+            vec![false, true, true, true, false],
+            vec![false, false, false, true, true],
+            vec![false, false, false, false, true],
         ]),
         maze_with_solution_6x6: 0, 0, &[
-            vec![1, 0, 1, 0, 1, 0],
-            vec![1, 1, 0, 1, 0, 1],
-            vec![0, 1, 1, 1, 1, 0],
-            vec![0, 0, 0, 1, 1, 1],
-            vec![0, 1, 0, 0, 1, 0],
-            vec![1, 1, 1, 1, 1, 1],
+            vec![true, false, true, false, true, false],
+            vec![true, true, false, true, false, true],
+            vec![false, true, true, true, true, false],
+            vec![false, false, false, true, true, true],
+            vec![false, true, false, false, true, false],
+            vec![true, true, true, true, true, true],
         ], Some(vec![
-            vec![1, 0, 0, 0, 0, 0],
-            vec![1, 1, 0, 0, 0, 0],
-            vec![0, 1, 1, 1, 1, 0],
-            vec![0, 0, 0, 0, 1, 0],
-            vec![0, 0, 0, 0, 1, 0],
-            vec![0, 0, 0, 0, 1, 1],
+            vec![true, false, false, false, false, false],
+            vec![true, true, false, false, false, false],
+            vec![false, true, true, true, true, false],
+            vec![false, false, false, false, true, false],
+            vec![false, false, false, false, true, false],
+            vec![false, false, false, false, true, true],
         ]),
         maze_with_solution_8x8: 0, 0, &[
-            vec![1, 0, 0, 0, 0, 0, 0, 1],
-            vec![1, 1, 0, 1, 1, 1, 0, 0],
-            vec![0, 1, 1, 1, 0, 0, 0, 0],
-            vec![0, 0, 0, 1, 0, 1, 1, 0],
-            vec![0, 1, 0, 1, 1, 1, 0, 1],
-            vec![1, 0, 1, 0, 0, 1, 1, 1],
-            vec![0, 0, 1, 1, 1, 0, 1, 1],
-            vec![1, 1, 1, 0, 1, 1, 1, 1],
+            vec![true, false, false, false, false, false, false, true],
+            vec![true, true, false, true, true, true, false, false],
+            vec![false, true, true, true, false, false, false, false],
+            vec![false, false, false, true, false, true, true, false],
+            vec![false, true, false, true, true, true, false, true],
+            vec![true, false, true, false, false, true, true, true],
+            vec![false, false, true, true, true, false, true, true],
+            vec![true, true, true, false, true, true, true, true],
         ], Some(vec![
-            vec![1, 0, 0, 0, 0, 0, 0, 0],
-            vec![1, 1, 0, 0, 0, 0, 0, 0],
-            vec![0, 1, 1, 1, 0, 0, 0, 0],
-            vec![0, 0, 0, 1, 0, 0, 0, 0],
-            vec![0, 0, 0, 1, 1, 1, 0, 0],
-            vec![0, 0, 0, 0, 0, 1, 1, 1],
-            vec![0, 0, 0, 0, 0, 0, 0, 1],
-            vec![0, 0, 0, 0, 0, 0, 0, 1],
+            vec![true, false, false, false, false, false, false, false],
+            vec![true, true, false, false, false, false, false, false],
+            vec![false, true, true, true, false, false, false, false],
+            vec![false, false, false, true, false, false, false, false],
+            vec![false, false, false, true, true, true, false, false],
+            vec![false, false, false, false, false, true, true, true],
+            vec![false, false, false, false, false, false, false, true],
+            vec![false, false, false, false, false, false, false, true],
         ]),
         maze_without_solution_4x4: 0, 0, &[
-            vec![1, 0, 0, 0],
-            vec![1, 1, 0, 0],
-            vec![0, 1, 1, 0],
-            vec![0, 0, 0, 1],
-        ], None::<Vec<Vec<usize>>>,
+            vec![true, false, false, false],
+            vec![true, true, false, false],
+            vec![false, false, true, false],
+            vec![false, false, false, true],
+        ], None::<Vec<Vec<bool>>>,
         maze_no_solution_possible_loops: 0, 0, &[
-            vec![1, 1, 1, 1],
-            vec![1, 1, 1, 1],
-            vec![1, 1, 1, 0],
-            vec![1, 1, 0, 1],
-        ], None::<Vec<Vec<usize>>>,
+            vec![true, true, true, true],
+            vec![true, true, true, true],
+            vec![true, true, true, false],
+            vec![true, true, false, true],
+        ], None::<Vec<Vec<bool>>>,
         maze_with_start_outside: 5, 0, &[
-            vec![1, 0, 0, 0],
-            vec![1, 1, 0, 0],
-            vec![0, 1, 1, 0],
-            vec![0, 0, 1, 1],
-        ], None::<Vec<Vec<usize>>>,
+            vec![true, false, false, false],
+            vec![true, true, false, false],
+            vec![false, true, true, false],
+            vec![false, false, true, true],
+        ], None::<Vec<Vec<bool>>>,
         unproper_maze_with_solution: 0, 0, &[
-            vec![1],
-            vec![1, 1],
-            vec![1, 1, 1],
-            vec![1, 1, 1, 1]
+            vec![true],
+            vec![true, true],
+            vec![true, true, true],
+            vec![true, true, true, true]
         ], Some(vec![
-            vec![1, 0, 0, 0],
-            vec![1, 1, 0, 0],
-            vec![0, 1, 1, 0],
-            vec![0, 0, 1, 1]
+            vec![true, false, false, false],
+            vec![true, true, false, false],
+            vec![false, true, true, false],
+            vec![false, false, true, true]
         ]),
         unproper_maze_no_solution_possible_loops: 0, 0, &[
-            vec![1, 1, 1, 1],
-            vec![1, 1],
-            vec![1, 1, 0],
-            vec![0, 0, 1],
-        ], None::<Vec<Vec<usize>>>,
+            vec![true, true, true, true],
+            vec![true, true],
+            vec![true, true, false],
+            vec![false, false, true],
+        ], None::<Vec<Vec<bool>>>,
+        maze_non_square_with_solution: 0, 0, &[
+            vec![true, false, true, true],
+            vec![true, true, true, false],
+            vec![false, true, true, true],
+        ], Some(vec![
+            vec![true, false, false, false],
+            vec![true, true, true, false],
+            vec![false, false, true, true],
+        ]),
+        maze_non_square_without_solution: 0, 0, &[
+            vec![true, false, true, true],
+            vec![true, false, true, false],
+            vec![false, true, false, true],
+        ], None::<Vec<Vec<bool>>>,
     }
 }
