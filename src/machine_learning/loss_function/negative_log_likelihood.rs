8ef77e8925b7a71cefb77d901627a4282c372fc9
Javier Kauer <kauer.javier01@gmail.com>
2024-07-03 12:44:45 +0200

    Fix: changed return type to Result instead of Option

--- src/machine_learning/loss_function/negative_log_likelihood.rs
+++ src/machine_learning/loss_function/negative_log_likelihood.rs
@@ -15,14 +15,21 @@
 //
 // https://towardsdatascience.com/cross-entropy-negative-log-likelihood-and-all-that-jazz-47a95bd2e81
 
-pub fn neg_log_likelihood(y_true: &[f64], y_pred: &[f64]) -> Option<f64> {
+pub fn neg_log_likelihood(
+    y_true: &[f64],
+    y_pred: &[f64],
+) -> Result<f64, NegativeLogLikelihoodLossError> {
     // Checks if the length of the actual and predicted values are equal
-    if y_true.len() != y_pred.len() || y_pred.is_empty() {
-        return None;
+    if y_true.len() != y_pred.len() {
+        return Err(NegativeLogLikelihoodLossError::InputsHaveDifferentLength);
+    }
+    // Checks if the inputs are empty
+    if y_pred.is_empty() {
+        return Err(NegativeLogLikelihoodLossError::EmptyInputs);
     }
     // Checks values are between 0 and 1
     if !are_all_values_in_range(y_true) || !are_all_values_in_range(y_pred) {
-        return None;
+        return Err(NegativeLogLikelihoodLossError::InvalidValues);
     }
 
     let mut total_loss: f64 = 0.0;
@@ -30,7 +37,14 @@ pub fn neg_log_likelihood(y_true: &[f64], y_pred: &[f64]) -> Option<f64> {
         let loss: f64 = -a * p.ln() - (1.0 - a) * (1.0 - p).ln();
         total_loss += loss;
     }
-    Some(total_loss / (y_pred.len() as f64))
+    Ok(total_loss / (y_pred.len() as f64))
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum NegativeLogLikelihoodLossError {
+    InputsHaveDifferentLength,
+    EmptyInputs,
+    InvalidValues,
 }
 
 fn are_all_values_in_range(values: &[f64]) -> bool {
@@ -39,6 +53,7 @@ fn are_all_values_in_range(values: &[f64]) -> bool {
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
@@ -47,7 +62,7 @@ mod tests {
         let predicted_values: Vec<f64> = vec![0.9, 0.1, 0.8];
         assert_eq!(
             neg_log_likelihood(&actual_values, &predicted_values),
-            Some(0.14462152754328741)
+            Ok(0.14462152754328741)
         );
     }
 
@@ -55,41 +70,59 @@ mod tests {
     fn test_neg_log_likelihood_invalid_length0() {
         let actual_values: Vec<f64> = vec![1.0, 0.0, 1.0];
         let predicted_values: Vec<f64> = vec![0.9, 0.1];
-        assert_eq!(neg_log_likelihood(&actual_values, &predicted_values), None);
+        assert_eq!(
+            neg_log_likelihood(&actual_values, &predicted_values),
+            Err(NegativeLogLikelihoodLossError::InputsHaveDifferentLength)
+        );
     }
 
     #[test]
     fn test_neg_log_likelihood_invalid_length1() {
         let actual_values: Vec<f64> = vec![1.0, 0.0];
         let predicted_values: Vec<f64> = vec![0.9, 0.1, 0.8];
-        assert_eq!(neg_log_likelihood(&actual_values, &predicted_values), None);
+        assert_eq!(
+            neg_log_likelihood(&actual_values, &predicted_values),
+            Err(NegativeLogLikelihoodLossError::InputsHaveDifferentLength)
+        );
     }
 
     #[test]
     fn test_neg_log_likelihood_invalid_values() {
         let actual_values: Vec<f64> = vec![1.0, 0.0, 1.0];
         let predicted_values: Vec<f64> = vec![1.1, 0.1, 0.8];
-        assert_eq!(neg_log_likelihood(&actual_values, &predicted_values), None);
+        assert_eq!(
+            neg_log_likelihood(&actual_values, &predicted_values),
+            Err(NegativeLogLikelihoodLossError::InvalidValues)
+        );
     }
 
     #[test]
     fn test_neg_log_likelihood_empty_prediction() {
         let actual_values: Vec<f64> = vec![1.0, 0.0, 1.0];
         let predicted_values: Vec<f64> = vec![];
-        assert_eq!(neg_log_likelihood(&actual_values, &predicted_values), None);
+        assert_eq!(
+            neg_log_likelihood(&actual_values, &predicted_values),
+            Err(NegativeLogLikelihoodLossError::EmptyInputs)
+        );
     }
 
     #[test]
     fn test_neg_log_likelihood_negative_values0() {
         let actual_values: Vec<f64> = vec![-1.0, 0.0, 1.0];
         let predicted_values: Vec<f64> = vec![0.9, 0.1, 0.8];
-        assert_eq!(neg_log_likelihood(&actual_values, &predicted_values), None);
+        assert_eq!(
+            neg_log_likelihood(&actual_values, &predicted_values),
+            Err(NegativeLogLikelihoodLossError::InvalidValues)
+        );
     }
 
     #[test]
     fn test_neg_log_likelihood_negative_values1() {
         let actual_values: Vec<f64> = vec![1.0, 0.0, 1.0];
         let predicted_values: Vec<f64> = vec![0.9, 0.1, -0.8];
-        assert_eq!(neg_log_likelihood(&actual_values, &predicted_values), None);
+        assert_eq!(
+            neg_log_likelihood(&actual_values, &predicted_values),
+            Err(NegativeLogLikelihoodLossError::InvalidValues)
+        );
     }
 }
