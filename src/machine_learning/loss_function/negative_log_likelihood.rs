171ef934f6a93bbfccafa22c8b2c7dfd47f05527
Javier Kauer <kauer.javier01@gmail.com>
2024-07-04 18:18:20 +0200

    Fix: fixed clippy issue and wrapped tests

--- src/machine_learning/loss_function/negative_log_likelihood.rs
+++ src/machine_learning/loss_function/negative_log_likelihood.rs
@@ -51,42 +51,48 @@ fn are_all_values_in_range(values: &[f64]) -> bool {
     values.iter().all(|&x| (0.0..=1.0).contains(&x))
 }
 
-macro_rules! test_with_wrong_inputs {
-    ($($name:ident: $inputs:expr,)*) => {
-        $(
-            #[test]
-            fn $name() {
-                let (values_a, values_b, expected_error) = $inputs;
-                assert_eq!(neg_log_likelihood(&values_a, &values_b), expected_error);
-                assert_eq!(neg_log_likelihood(&values_b, &values_a), expected_error);
-            }
-        )*
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! test_with_wrong_inputs {
+        ($($name:ident: $inputs:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (values_a, values_b, expected_error) = $inputs;
+                    assert_eq!(neg_log_likelihood(&values_a, &values_b), expected_error);
+                    assert_eq!(neg_log_likelihood(&values_b, &values_a), expected_error);
+                }
+            )*
+        }
     }
-}
-test_with_wrong_inputs! {
-    different_length: (vec![0.9, 0.0, 0.8], vec![0.9, 0.1], Err(NegativeLogLikelihoodLossError::InputsHaveDifferentLength)),
-    different_length_one_empty: (vec![], vec![0.9, 0.1], Err(NegativeLogLikelihoodLossError::InputsHaveDifferentLength)),
-    value_greater_than_1: (vec![1.1, 0.0, 0.8], vec![0.1, 0.2, 0.3], Err(NegativeLogLikelihoodLossError::InvalidValues)),
-    value_greater_smaller_than_0: (vec![0.9, 0.0, -0.1], vec![0.1, 0.2, 0.3], Err(NegativeLogLikelihoodLossError::InvalidValues)),
-    empty_input: (vec![], vec![], Err(NegativeLogLikelihoodLossError::EmptyInputs)),
-}
 
-macro_rules! test_neg_log_likelihood {
-    ($($name:ident: $inputs:expr,)*) => {
-        $(
-            #[test]
-            fn $name() {
-                let (actual_values, predicted_values, expected) = $inputs;
-                assert_eq!(neg_log_likelihood(&actual_values, &predicted_values).unwrap(), expected);
-            }
-        )*
+    test_with_wrong_inputs! {
+        different_length: (vec![0.9, 0.0, 0.8], vec![0.9, 0.1], Err(NegativeLogLikelihoodLossError::InputsHaveDifferentLength)),
+        different_length_one_empty: (vec![], vec![0.9, 0.1], Err(NegativeLogLikelihoodLossError::InputsHaveDifferentLength)),
+        value_greater_than_1: (vec![1.1, 0.0, 0.8], vec![0.1, 0.2, 0.3], Err(NegativeLogLikelihoodLossError::InvalidValues)),
+        value_greater_smaller_than_0: (vec![0.9, 0.0, -0.1], vec![0.1, 0.2, 0.3], Err(NegativeLogLikelihoodLossError::InvalidValues)),
+        empty_input: (vec![], vec![], Err(NegativeLogLikelihoodLossError::EmptyInputs)),
     }
-}
 
-test_neg_log_likelihood! {
-    set_0: (vec![1.0, 0.0, 1.0], vec![0.9, 0.1, 0.8], 0.14462152754328741),
-    set_1: (vec![1.0, 0.0, 1.0], vec![0.1, 0.2, 0.3], 1.2432338162113972),
-    set_2: (vec![0.0, 1.0, 0.0], vec![0.1, 0.2, 0.3], 0.6904911240102196),
+    macro_rules! test_neg_log_likelihood {
+        ($($name:ident: $inputs:expr,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    let (actual_values, predicted_values, expected) = $inputs;
+                    assert_eq!(neg_log_likelihood(&actual_values, &predicted_values).unwrap(), expected);
+                }
+            )*
+        }
+    }
 
-    set_3: (vec![1.0, 0.0, 1.0, 0.0], vec![0.9, 0.1, 0.8, 0.2], 0.164252033486018),
+    test_neg_log_likelihood! {
+        set_0: (vec![1.0, 0.0, 1.0], vec![0.9, 0.1, 0.8], 0.14462152754328741),
+        set_1: (vec![1.0, 0.0, 1.0], vec![0.1, 0.2, 0.3], 1.2432338162113972),
+        set_2: (vec![0.0, 1.0, 0.0], vec![0.1, 0.2, 0.3], 0.6904911240102196),
+
+        set_3: (vec![1.0, 0.0, 1.0, 0.0], vec![0.9, 0.1, 0.8, 0.2], 0.164252033486018),
+    }
 }
