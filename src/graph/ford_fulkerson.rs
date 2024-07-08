0d22dabe84b2a401230721d6ff0b2310ff1c3d7a
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-07-08 12:18:05 +0700

    ref: refactor ford fulkerson - Add custom error types - Add some edge tests

--- src/graph/ford_fulkerson.rs
+++ src/graph/ford_fulkerson.rs
@@ -2,20 +2,22 @@
 //!
 //! The maximum flow problem involves determining the maximum amount of flow that can be sent from a source vertex to a sink vertex
 //! in a directed weighted graph, subject to capacity constraints on the edges.
-//!
-//! The following is the simple idea of the Ford-Fulkerson algorithm:
-//!
-//! 1. Start with the initial flow as 0.
-//! 2. While there exists an augmenting path from the source to the sink:
-//!     - Find an augmenting path using any path-finding algorithm, such as breadth-first search or depth-first search.
-//!     - Determine the amount of flow that can be sent along the augmenting path, which is the minimum residual capacity along the edges of the path.
-//!     - Increase the flow along the augmenting path by the determined amount.
-//! 3. Return the maximum flow and residual graph.
 
 use std::collections::VecDeque;
 
+/// Enum representing the possible errors that can occur when running the Ford-Fulkerson algorithm.
+#[derive(Debug, PartialEq)]
+pub enum FordFulkersonError {
+    /// Error indicating that the graph is empty or has no edges.
+    EmptyGraph,
+    /// Error indicating that the source vertex is out of bounds.
+    InvalidSource,
+    /// Error indicating that the sink vertex is out of bounds.
+    InvalidSink,
+}
+
 /// Performs a Breadth-First Search (BFS) on the residual graph to find an augmenting path
-/// from the source vertex `source` to the sink vertex `sink`.
+/// from the source vertex `source` to thie sink vertex `sink`.
 ///
 /// # Arguments
 ///
@@ -27,7 +29,7 @@ use std::collections::VecDeque;
 /// # Returns
 ///
 /// Returns `true` if an augmenting path is found from `source` to `sink`, `false` otherwise.
-fn bfs(graph: &[Vec<i32>], source: usize, sink: usize, parent: &mut [i32]) -> bool {
+fn bfs(graph: &[Vec<isize>], source: usize, sink: usize, parent: &mut [isize]) -> bool {
     let mut visited = vec![false; graph.len()];
     visited[source] = true;
     parent[source] = -1;
@@ -39,7 +41,7 @@ fn bfs(graph: &[Vec<i32>], source: usize, sink: usize, parent: &mut [i32]) -> bo
         for (previous_vertex, &capacity) in graph[current_vertex].iter().enumerate() {
             if !visited[previous_vertex] && capacity > 0 {
                 visited[previous_vertex] = true;
-                parent[previous_vertex] = current_vertex as i32;
+                parent[previous_vertex] = current_vertex as isize;
                 if previous_vertex == sink {
                     return true;
                 }
@@ -63,13 +65,29 @@ fn bfs(graph: &[Vec<i32>], source: usize, sink: usize, parent: &mut [i32]) -> bo
 /// # Returns
 ///
 /// Returns the maximum flow and the residual graph
-pub fn ford_fulkerson(graph: &[Vec<i32>], source: usize, sink: usize) -> (i32, Vec<Vec<i32>>) {
+pub fn ford_fulkerson(
+    graph: &[Vec<isize>],
+    source: usize,
+    sink: usize,
+) -> Result<isize, FordFulkersonError> {
+    if graph.is_empty() || graph[0].is_empty() {
+        return Err(FordFulkersonError::EmptyGraph);
+    }
+
+    if source >= graph.len() {
+        return Err(FordFulkersonError::InvalidSource);
+    }
+
+    if sink >= graph.len() {
+        return Err(FordFulkersonError::InvalidSink);
+    }
+
     let mut residual_graph = graph.to_owned();
     let mut parent = vec![-1; graph.len()];
     let mut max_flow = 0;
 
     while bfs(&residual_graph, source, sink, &mut parent) {
-        let mut path_flow = i32::MAX;
+        let mut path_flow = isize::MAX;
         let mut previous_vertex = sink;
 
         while previous_vertex != source {
@@ -89,7 +107,7 @@ pub fn ford_fulkerson(graph: &[Vec<i32>], source: usize, sink: usize) -> (i32, V
         max_flow += path_flow;
     }
 
-    (max_flow, residual_graph)
+    Ok(max_flow)
 }
 
 #[cfg(test)]
@@ -101,80 +119,56 @@ mod tests {
             $(
                 #[test]
                 fn $name() {
-                    let (graph, source, sink, expected_flow, expected_residual_graph) = $tc;
-                    let (max_flow, residual_graph) = ford_fulkerson(&graph, source, sink);
-                    assert_eq!(max_flow, expected_flow);
-                    assert_eq!(residual_graph, expected_residual_graph);
+                    let (graph, source, sink, expected_result) = $tc;
+                    let result = ford_fulkerson(&graph, source, sink);
+                    assert_eq!(result, expected_result);
                 }
             )*
         };
     }
 
     test_max_flow! {
-        test_example_1: (
+        test_empty_graph: (
+            vec![],
+            0,
+            0,
+            Err(FordFulkersonError::EmptyGraph),
+        ),
+        test_graph_with_empty_edge: (
             vec![
-                vec![0, 12, 0, 13, 0, 0],
-                vec![0, 0, 10, 0, 0, 0],
-                vec![0, 0, 0, 13, 3, 15],
-                vec![0, 0, 7, 0, 15, 0],
-                vec![0, 0, 6, 0, 0, 17],
-                vec![0, 0, 0, 0, 0, 0],
+                vec![],
             ],
             0,
             5,
-            23,
-            vec![
-                vec![0, 2, 0, 0, 0, 0],
-                vec![10, 0, 0, 0, 0, 0],
-                vec![0, 10, 0, 18, 3, 0],
-                vec![13, 0, 2, 0, 7, 0],
-                vec![0, 0, 6, 8, 0, 9],
-                vec![0, 0, 15, 0, 8, 0],
-            ],
+            Err(FordFulkersonError::EmptyGraph),
         ),
-        test_example_2: (
+        test_invalid_source: (
             vec![
-                vec![0, 4, 0, 3, 0, 0],
-                vec![0, 0, 4, 0, 8, 0],
-                vec![0, 0, 0, 3, 0, 2],
-                vec![0, 0, 0, 0, 6, 0],
-                vec![0, 0, 6, 0, 0, 6],
+                vec![0, 8, 0, 0, 3, 0],
+                vec![0, 0, 9, 0, 0, 0],
+                vec![0, 0, 0, 0, 7, 2],
+                vec![0, 0, 0, 0, 0, 5],
+                vec![0, 0, 7, 4, 0, 0],
+                vec![0, 0, 0, 0, 0, 0],
+            ],
+            6,
+            5,
+            Err(FordFulkersonError::InvalidSource),
+        ),
+        test_invalid_sink: (
+            vec![
+                vec![0, 8, 0, 0, 3, 0],
+                vec![0, 0, 9, 0, 0, 0],
+                vec![0, 0, 0, 0, 7, 2],
+                vec![0, 0, 0, 0, 0, 5],
+                vec![0, 0, 7, 4, 0, 0],
                 vec![0, 0, 0, 0, 0, 0],
             ],
             0,
-            5,
-            7,
-            vec![
-                vec![0, 0, 0, 0, 0, 0],
-                vec![4, 0, 2, 0, 6, 0],
-                vec![0, 2, 0, 3, 0, 0],
-                vec![3, 0, 0, 0, 3, 0],
-                vec![0, 2, 6, 3, 0, 1],
-                vec![0, 0, 2, 0, 5, 0],
-            ],
+            6,
+            Err(FordFulkersonError::InvalidSink),
         ),
-        test_example_3: (
-            vec![
-                vec![0, 10, 0, 10, 0, 0],
-                vec![0, 0, 4, 2, 8, 0],
-                vec![0, 0, 0, 0, 0, 10],
-                vec![0, 0, 0, 0, 9, 0],
-                vec![0, 0, 6, 0, 0, 10],
-                vec![0, 0, 0, 0, 0, 0],
-            ],
-            0,
-            5,
-            19,
-            vec![
-                vec![0, 0, 0, 1, 0, 0],
-                vec![10, 0, 0, 2, 2, 0],
-                vec![0, 4, 0, 0, 5, 1],
-                vec![9, 0, 0, 0, 0, 0],
-                vec![0, 6, 1, 9, 0, 0],
-                vec![0, 0, 9, 0, 10, 0],
-            ],
-        ),
-        test_example_4: (
+        test_graph_with_small_flow: (
             vec![
                 vec![0, 8, 0, 0, 3, 0],
                 vec![0, 0, 9, 0, 0, 0],
@@ -185,17 +179,35 @@ mod tests {
             ],
             0,
             5,
-            6,
-            vec![
-                vec![0, 5, 0, 0, 0, 0],
-                vec![3, 0, 6, 0, 0, 0],
-                vec![0, 3, 0, 0, 6, 0],
-                vec![0, 0, 0, 0, 4, 1],
-                vec![3, 0, 8, 0, 0, 0],
-                vec![0, 0, 2, 4, 0, 0],
-            ],
+            Ok(6),
         ),
-        test_example_5: (
+        test_graph_with_medium_flow: (
+            vec![
+                vec![0, 10, 0, 10, 0, 0],
+                vec![0, 0, 4, 2, 8, 0],
+                vec![0, 0, 0, 0, 0, 10],
+                vec![0, 0, 0, 0, 9, 0],
+                vec![0, 0, 6, 0, 0, 10],
+                vec![0, 0, 0, 0, 0, 0],
+            ],
+            0,
+            5,
+            Ok(19),
+        ),
+        test_graph_with_large_flow: (
+            vec![
+                vec![0, 12, 0, 13, 0, 0],
+                vec![0, 0, 10, 0, 0, 0],
+                vec![0, 0, 0, 13, 3, 15],
+                vec![0, 0, 7, 0, 15, 0],
+                vec![0, 0, 6, 0, 0, 17],
+                vec![0, 0, 0, 0, 0, 0],
+            ],
+            0,
+            5,
+            Ok(23),
+        ),
+        test_complex_graph: (
             vec![
                 vec![0, 16, 13, 0, 0, 0],
                 vec![0, 0, 10, 12, 0, 0],
@@ -206,17 +218,9 @@ mod tests {
             ],
             0,
             5,
-            23,
-            vec![
-                vec![0, 4, 2, 0, 0, 0],
-                vec![12, 0, 10, 0, 0, 0],
-                vec![11, 4, 0, 0, 3, 0],
-                vec![0, 12, 9, 0, 7, 1],
-                vec![0, 0, 11, 0, 0, 0],
-                vec![0, 0, 0, 19, 4, 0],
-            ],
+            Ok(23),
         ),
-        test_example_6: (
+        test_disconnected_graph: (
             vec![
                 vec![0, 0, 0, 0],
                 vec![0, 0, 0, 1],
@@ -225,13 +229,67 @@ mod tests {
             ],
             0,
             3,
-            0,
+            Ok(0),
+        ),
+        test_unconnected_sink: (
             vec![
-                vec![0, 0, 0, 0],
-                vec![0, 0, 0, 1],
-                vec![0, 0, 0, 1],
-                vec![0, 0, 0, 0],
+                vec![0, 4, 0, 3, 0, 0],
+                vec![0, 0, 4, 0, 8, 0],
+                vec![0, 0, 0, 3, 0, 2],
+                vec![0, 0, 0, 0, 6, 0],
+                vec![0, 0, 6, 0, 0, 6],
+                vec![0, 0, 0, 0, 0, 0],
             ],
+            0,
+            5,
+            Ok(7),
+        ),
+        test_no_edges: (
+            vec![
+                vec![0, 0, 0],
+                vec![0, 0, 0],
+                vec![0, 0, 0],
+            ],
+            0,
+            2,
+            Ok(0),
+        ),
+        test_single_vertex: (
+            vec![
+                vec![0],
+            ],
+            0,
+            0,
+            Ok(0),
+        ),
+        test_negative_capacity: (
+            vec![
+                vec![0, -10, 0],
+                vec![0, 0, -5],
+                vec![0, 0, 0],
+            ],
+            0,
+            2,
+            Ok(0),
+        ),
+        test_self_loop: (
+            vec![
+                vec![10, 0],
+                vec![0, 0],
+            ],
+            0,
+            1,
+            Ok(0),
+        ),
+        test_same_source_sink: (
+            vec![
+                vec![0, 10, 10],
+                vec![0, 0, 10],
+                vec![0, 0, 0],
+            ],
+            0,
+            0,
+            Ok(0),
         ),
     }
 }
