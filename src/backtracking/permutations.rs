b807c5d852b6c130023193218ab6826e2fe14c49
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-09-01 15:25:24 +0700

    ref: permutations of collection of duplicates should be distinct

--- src/backtracking/permutations.rs
+++ src/backtracking/permutations.rs
@@ -1,33 +1,34 @@
-//! This module provides a function to generate all possible permutations
-//! of a given collection of distinct integers using a backtracking algorithm.
+//! This module provides a function to generate all possible distinct permutations
+//! of a given collection of integers using a backtracking algorithm.
 
-/// Generates all possible permutations of a given vector of distinct integers.
+/// Generates all possible distinct permutations of a given vector of integers.
 ///
 /// # Arguments
 ///
-/// * `nums` - A vector of distinct integers.
+/// * `nums` - A vector of integers.
 ///
 /// # Returns
 ///
-/// A vector containing all possible permutations of the input vector.
-pub fn permute(nums: Vec<isize>) -> Vec<Vec<isize>> {
+/// A vector containing all possible distinct permutations of the input vector.
+pub fn permute(mut nums: Vec<isize>) -> Vec<Vec<isize>> {
     let mut permutations = Vec::new();
     let mut current = Vec::new();
     let mut used = vec![false; nums.len()];
 
+    nums.sort();
     generate(&nums, &mut current, &mut used, &mut permutations);
 
     permutations
 }
 
-/// Helper function for the `permute` function to generate permutations recursively.
+/// Helper function for the `permute` function to generate distinct permutations recursively.
 ///
 /// # Arguments
 ///
-/// * `nums` - A reference to the original vector of distinct integers.
+/// * `nums` - A reference to the original vector of integers.
 /// * `current` - A mutable reference to the vector holding the current permutation.
 /// * `used` - A mutable reference to a vector tracking which elements are used.
-/// * `permutations` - A mutable reference to the vector holding all generated permutations.
+/// * `permutations` - A mutable reference to the vector holding all generated distinct permutations.
 fn generate(
     nums: &Vec<isize>,
     current: &mut Vec<isize>,
@@ -41,6 +42,10 @@ fn generate(
 
     for idx in 0..nums.len() {
         if used[idx] {
+            continue;
+        }
+
+        if idx > 0 && nums[idx] == nums[idx - 1] && !used[idx - 1] {
             continue;
         }
 
@@ -71,7 +76,7 @@ mod tests {
     }
 
     permute_tests! {
-        test_permute_123: (vec![1, 2, 3], vec![
+        test_permute_basic: (vec![1, 2, 3], vec![
             vec![1, 2, 3],
             vec![1, 3, 2],
             vec![2, 1, 3],
@@ -84,18 +89,15 @@ mod tests {
         test_permute_duplicates: (vec![1, 1, 2], vec![
             vec![1, 1, 2],
             vec![1, 2, 1],
-            vec![1, 1, 2],
-            vec![1, 2, 1],
-            vec![2, 1, 1],
             vec![2, 1, 1],
         ]),
         test_permute_negative: (vec![-1, -2, -3], vec![
-            vec![-1, -2, -3],
-            vec![-1, -3, -2],
-            vec![-2, -1, -3],
-            vec![-2, -3, -1],
-            vec![-3, -1, -2],
             vec![-3, -2, -1],
+            vec![-3, -1, -2],
+            vec![-2, -3, -1],
+            vec![-2, -1, -3],
+            vec![-1, -3, -2],
+            vec![-1, -2, -3],
         ]),
         test_permute_mixed: (vec![-1, 0, 1], vec![
             vec![-1, 0, 1],
