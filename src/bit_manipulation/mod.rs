5e2d1e266a9be3467ad94e97b2cd88994ba58764
Truong Nhan Nguyen <80200848+sozelfist@users.noreply.github.com>
2024-09-12 01:47:33 +0700

    Implement n bits gray code (#787)
    feat: add `n_bits_gray_code.rs`

--- src/bit_manipulation/mod.rs
+++ src/bit_manipulation/mod.rs
@@ -1,7 +1,9 @@
 mod counting_bits;
 mod highest_set_bit;
+mod n_bits_gray_code;
 mod sum_of_two_integers;
 
 pub use counting_bits::count_set_bits;
 pub use highest_set_bit::find_highest_set_bit;
+pub use n_bits_gray_code::generate_gray_code;
 pub use sum_of_two_integers::add_two_integers;
