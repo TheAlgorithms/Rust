24af67ff065235a54ed252b0b429737e7c679201
Truong Nhan Nguyen <nhantruongnguyen123@gmail.com>
2024-09-25 15:46:32 +0700

    chore(test): add some test cases

--- src/string/duval_algorithm.rs
+++ src/string/duval_algorithm.rs
@@ -74,8 +74,8 @@ mod test {
     }
 
     test_duval_algorithm! {
-        multiple: ("abcdabcdababc", ["abcd", "abcd", "ababc"]),
-        all: ("aaa", ["a", "a", "a"]),
+        repeating_with_suffix: ("abcdabcdababc", ["abcd", "abcd", "ababc"]),
+        single_repeating_char: ("aaa", ["a", "a", "a"]),
         single: ("ababb", ["ababb"]),
         unicode: ("അഅഅ", ["അ", "അ", "അ"]),
         empty_string: ("", Vec::<String>::new()),
@@ -84,5 +84,14 @@ mod test {
         long_repeating: ("aaaaaa", ["a", "a", "a", "a", "a", "a"]),
         mixed_repeating: ("ababcbabc", ["ababcbabc"]),
         non_repeating_sorted: ("abcdefg", ["abcdefg"]),
+        alternating_increasing: ("abababab", ["ab", "ab", "ab", "ab"]),
+        long_repeating_lyndon: ("abcabcabcabc", ["abc", "abc", "abc", "abc"]),
+        decreasing_order: ("zyxwvutsrqponm", ["z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m"]),
+        alphanumeric_mixed: ("a1b2c3a1", ["a", "1b2c3a", "1"]),
+        special_characters: ("a@b#c$d", ["a", "@b", "#c$d"]),
+        unicode_complex: ("αβγδ", ["αβγδ"]),
+        long_string_performance: (&"a".repeat(1_000_000), vec!["a"; 1_000_000]),
+        palindrome_repeating_prefix: ("abccba", ["abccb", "a"]),
+        interrupted_lyndon: ("abcxabc", ["abcx", "abc"]),
     }
 }
