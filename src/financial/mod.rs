e6392530689cd29cb4744774ab0525ac8bc705ee
alireza_sariri <alirezasariri78@gmail.com>
2024-10-30 10:14:55 +0330

    run fmt command

--- src/financial/mod.rs
+++ src/financial/mod.rs
@@ -1,5 +1,4 @@
-mod price_plus_tax;
 mod present_value;
-pub use price_plus_tax::price_plus_tax;
+mod price_plus_tax;
 pub use present_value::present_value;
-
+pub use price_plus_tax::price_plus_tax;
