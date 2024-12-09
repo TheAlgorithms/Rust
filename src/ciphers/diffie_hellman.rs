d791d0a0e862507ebb14d945c8faae648d1aab8b
Quentin Santos <qsantos@qsantos.fr>
2024-12-09 19:43:51 +0100

    Remove lazy_static from dependencies

--- src/ciphers/diffie_hellman.rs
+++ src/ciphers/diffie_hellman.rs
@@ -2,52 +2,50 @@
 // RFC 3526 - More Modular Exponential (MODP) Diffie-Hellman groups for
 // Internet Key Exchange (IKE) https://tools.ietf.org/html/rfc3526
 
-use lazy_static;
 use num_bigint::BigUint;
 use num_traits::{Num, Zero};
 use std::{
     collections::HashMap,
+    sync::LazyLock,
     time::{SystemTime, UNIX_EPOCH},
 };
 
-// Using lazy static to initialize statics that require code to be executed at runtime.
-lazy_static! {
-    // A map of predefined prime numbers for different bit lengths, as specified in RFC 3526
-    static ref PRIMES: HashMap<u8, BigUint> = {
-        let mut m:HashMap<u8, BigUint> = HashMap::new();
-        m.insert(
-            // 1536-bit
-            5,
-            BigUint::parse_bytes(
-                b"FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1\
-                    29024E088A67CC74020BBEA63B139B22514A08798E3404DD\
-                    EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245\
-                    E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED\
-                    EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D\
-                    C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F\
-                    83655D23DCA3AD961C62F356208552BB9ED529077096966D\
-                    670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF",
-                16
-            ).unwrap()
-        );
-        m.insert(
-            // 2048-bit
-            14,
-            BigUint::parse_bytes(
-                b"FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1\
+// A map of predefined prime numbers for different bit lengths, as specified in RFC 3526
+static PRIMES: LazyLock<HashMap<u8, BigUint>> = LazyLock::new(|| {
+    let mut m:HashMap<u8, BigUint> = HashMap::new();
+    m.insert(
+        // 1536-bit
+        5,
+        BigUint::parse_bytes(
+            b"FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1\
                 29024E088A67CC74020BBEA63B139B22514A08798E3404DD\
                 EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245\
                 E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED\
                 EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D\
                 C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F\
                 83655D23DCA3AD961C62F356208552BB9ED529077096966D\
-                670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B\
-                E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9\
-                DE2BCBF6955817183995497CEA956AE515D2261898FA0510\
-                15728E5A8AACAA68FFFFFFFFFFFFFFFF",
-                16
-            ).unwrap()
-        );
+                670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF",
+            16
+        ).unwrap()
+    );
+    m.insert(
+        // 2048-bit
+        14,
+        BigUint::parse_bytes(
+            b"FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1\
+            29024E088A67CC74020BBEA63B139B22514A08798E3404DD\
+            EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245\
+            E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED\
+            EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D\
+            C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F\
+            83655D23DCA3AD961C62F356208552BB9ED529077096966D\
+            670C354E4ABC9804F1746C08CA18217C32905E462E36CE3B\
+            E39E772C180E86039B2783A2EC07A28FB5C55DF06F4C52C9\
+            DE2BCBF6955817183995497CEA956AE515D2261898FA0510\
+            15728E5A8AACAA68FFFFFFFFFFFFFFFF",
+            16
+        ).unwrap()
+    );
 
     m.insert(
     // 3072-bit
@@ -188,8 +186,7 @@ lazy_static! {
         ).unwrap()
     );
     m
-    };
-}
+});
 
 /// Generating random number, should use num_bigint::RandomBits if possible.
 fn rand() -> usize {
