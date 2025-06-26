96f52b00d59f7e7ff19dd4a0d66b1c74ad20130d
D V <144282650+dabevlohn@users.noreply.github.com>
2025-06-26 11:58:16 +0300

    Update bubble_sort.rs
    some tests added

--- src/sorting/bubble_sort.rs
+++ src/sorting/bubble_sort.rs
@@ -1,3 +1,5 @@
+/// This function works only with integers
+/// For ordering float values use https://crates.io/crates/float-ord
 pub fn bubble_sort<T: Ord>(arr: &mut [T]) {
     if arr.is_empty() {
         return;
@@ -21,6 +23,8 @@ mod tests {
     use super::*;
     use crate::sorting::have_same_elements;
     use crate::sorting::is_sorted;
+    use rand::rng;
+    use rand::seq::SliceRandom;
 
     #[test]
     fn descending() {
@@ -42,6 +46,36 @@ mod tests {
     #[test]
     fn empty() {
         let mut ve3: Vec<usize> = vec![];
+        let cloned = ve3.clone();
+        bubble_sort(&mut ve3);
+        assert!(is_sorted(&ve3) && have_same_elements(&ve3, &cloned));
+    }
+    #[test]
+    fn duplicate() {
+        let mut ve3: Vec<usize> = vec![2, 3, 3, 5, 7, 1, 1];
+        let cloned = ve3.clone();
+        bubble_sort(&mut ve3);
+        assert!(is_sorted(&ve3) && have_same_elements(&ve3, &cloned));
+    }
+    #[test]
+    fn negative() {
+        let mut ve3: Vec<i32> = (-10..-1).rev().collect();
+        let cloned = ve3.clone();
+        bubble_sort(&mut ve3);
+        assert!(is_sorted(&ve3) && have_same_elements(&ve3, &cloned));
+    }
+    #[test]
+    fn withnull() {
+        let mut ve3 = vec![2, 3, -3, 5, 7, 0, 1];
+        let cloned = ve3.clone();
+        bubble_sort(&mut ve3);
+        assert!(is_sorted(&ve3) && have_same_elements(&ve3, &cloned));
+    }
+    /// Takes much time 
+    #[test]
+    fn long() {
+        let mut ve3: Vec<i32> = (10..39000).collect();
+        ve3.shuffle(&mut rng());
         let cloned = ve3.clone();
         bubble_sort(&mut ve3);
         assert!(is_sorted(&ve3) && have_same_elements(&ve3, &cloned));
