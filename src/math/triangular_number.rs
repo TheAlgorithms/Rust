7d41d09a81bb82d0ceb203d6558a5308c7f6262c
WinterPancake <niedzielski-work@protonmail.com>
2025-07-03 19:05:01 -0500

    Added a new math algorithm for triangular numbers name triangular_number.rs

--- src/math/triangular_number.rs
+++ src/math/triangular_number.rs
@@ -3,7 +3,7 @@
 
 pub fn triangular_number(n: u64) -> u64 {
     let m: u64 = (n | 1) * ((n + 1) / 2);
-    return m;
+    m
 }
 
 #[cfg(test)]
@@ -23,16 +23,8 @@ mod tests {
     }
 
     test_triangular_number! {
-        input_0: (1, 1),
-        input_1: (2, 3),
-        input_2: (3, 6),
-        input_3: (4, 10),
-        input_4: (5, 15),
         input_5: (6, 21),
-        input_6: (7, 28),
-        input_7: (8, 36),
-        input_8: (9, 45),
         input_9: (10, 55),
-        input_10: (11, 66),
+        input_10: (100, 5050),
     }
 }
