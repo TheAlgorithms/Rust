bc623f3950f0a51a320770b5731521f2445b1c7d
WinterPancake <niedzielski-work@protonmail.com>
2025-07-07 12:36:38 -0500

    Fixed typo

--- src/math/triangular_number.rs
+++ src/math/triangular_number.rs
@@ -2,7 +2,7 @@
 // Wikipedia Reference : https://en.wikipedia.org/wiki/Triangular_number
 
 // This program provides a function to calculate the nth triangular number defined by T_n = 1 + 2 +
-// ... + n = (n^2 + 2)/ = n(n + 1)/2 = (n + 1) choose 2.
+// ... + n = (n^2 + 2)/2 = n(n + 1)/2 = (n + 1) choose 2.
 
 //returns the nth triangular number
 pub fn triangular_number(n: u64) -> u64 {
