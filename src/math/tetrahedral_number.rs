032de856f667ece8a7891a59d673dde71ced0496
WinterPancake <niedzielski-work@protonmail.com>
2025-07-07 12:26:35 -0500

    Added comments explaining what the module provides. Removed BigUint for being overkill. Removed the use and returning of the variable m. Renamed the names of the test cases to something more logical.

--- src/math/tetrahedral_number.rs
+++ src/math/tetrahedral_number.rs
@@ -1,9 +1,12 @@
 // Tetrahedral Numbers: Function to the Nth Tetrahedral Number
 // Wikipedia Reference : https://en.wikipedia.org/wiki/Tetrahedral_number
-use num_bigint::BigUint;
-pub fn tetrahedral_number(n: u64) -> BigUint {
-    let m: BigUint = (((n) * (n + 1) * (n + 2)) / 6).into();
-    m
+//
+// This program provides a function to calculate the nth triangular
+// number defined by T_n = 1 + 2 + ... + n = (n(n + 1)(n + 2))/6 =
+// (n + 2) choose 3.
+
+pub fn tetrahedral_number(n: u64) -> u64 {
+   ((n) * (n + 1) * (n + 2)) / 6
 }
 
 #[cfg(test)]
@@ -23,8 +26,11 @@ mod tests {
     }
 
     test_tetrahedral_number! {
-        input_6: (6, BigUint::from(56u32)),
-        input_8: (8, BigUint::from(120u32)),
-        input_34: (34, BigUint::from(7140u32)),
+        input_0: (0, 0),
+        input_1: (1, 1),
+        input_2: (6, 56),
+        input_3: (8, 120),
+        input_4: (11, 286),
+        input_5: (34, 7140),
     }
 }
