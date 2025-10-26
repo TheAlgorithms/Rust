9019283341ab77e9a50d2a6f2d04680224efab4a
omi <sowmithbasak13@gmail.com>
2025-10-26 23:46:40 +0530

    added npv, compound interest and payback period

---
+++ src/financial/compound_interest.rs
@@ -0,0 +1,24 @@
+// compound interest is given by A = P(1+r/n)^nt
+// where: A = Final Amount, P = Principal Amount, r = rate of interest,
+// n = number of times interest is compounded per year and t = time (in years)
+
+pub fn compound_interest(princpal: f64, rate: f64, comp_per_year: u32, years: f64) -> f64 {
+    let amount = princpal * (1.00 + rate / comp_per_year as f64).powf(comp_per_year as f64 * years);
+    return amount;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compound_interest() {
+        let principal = 1000.0;
+        let rate = 0.05; // 5% annual interest
+        let times_per_year = 4; // interest compounded quarterly
+        let years = 2.0; // 2 years tenure
+        let result = compound_interest(principal, rate, times_per_year, years);
+        assert!((result - 1104.486).abs() < 0.001); // expected value rounded up to 3 decimal
+                                                    // places
+    }
+}
--- src/financial/mod.rs
+++ src/financial/mod.rs
@@ -1,2 +1,8 @@
+mod compound_interest;
+mod npv;
+mod payback;
 mod present_value;
+pub use compound_interest::compound_interest;
+pub use npv::npv;
+pub use payback;
 pub use present_value::present_value;
