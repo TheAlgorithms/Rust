c646d9bdd32b31affecf6b59eae9bfb55c3e6526
Ali Alimohammadi <41567902+AliAlimohammadi@users.noreply.github.com>
2026-01-31 02:02:21 -0800

    fix: allowing missing punctuation for clippy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(clippy::doc_paragraphs_missing_punctuation)]
 pub mod backtracking;
 pub mod big_integer;
 pub mod bit_manipulation;
@@ -19,22 +20,3 @@ pub mod searching;
 pub mod signal_analysis;
 pub mod sorting;
 pub mod string;
-
-#[cfg(test)]
-mod tests {
-    use super::sorting;
-    #[test]
-    fn quick_sort() {
-        //descending
-        let mut ve1 = vec![6, 5, 4, 3, 2, 1];
-        sorting::quick_sort(&mut ve1);
-
-        assert!(sorting::is_sorted(&ve1));
-
-        //pre-sorted
-        let mut ve2 = vec![1, 2, 3, 4, 5, 6];
-        sorting::quick_sort(&mut ve2);
-
-        assert!(sorting::is_sorted(&ve2));
-    }
-}
