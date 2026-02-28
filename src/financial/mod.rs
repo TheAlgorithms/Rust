72e1a51f149531eca6072624c30ac41eace8fa02
Ali Alimohammadi <pishrafte1@yahoo.com>
2026-02-27 19:50:39 -0800

    feat: add exponential moving average to financial

--- src/financial/mod.rs
+++ src/financial/mod.rs
@@ -1,5 +1,6 @@
 mod compound_interest;
 mod equated_monthly_installments;
+mod exponential_moving_average;
 mod finance_ratios;
 mod npv;
 mod npv_sensitivity;
@@ -9,6 +10,7 @@ mod treynor_ratio;
 
 pub use self::compound_interest::compound_interest;
 pub use self::equated_monthly_installments::equated_monthly_installments;
+pub use self::exponential_moving_average::exponential_moving_average;
 pub use self::finance_ratios::{
     debt_to_equity, earnings_per_sale, gross_profit_margin, return_on_investment,
 };
