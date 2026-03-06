fb8e82afcc5035e006dbdc93803d97d51f55c3b3
Ali Alimohammadi <pishrafte1@yahoo.com>
2026-03-06 13:43:57 -0800

    feat: add Simple, Compound, and APR interest calculations

--- src/financial/mod.rs
+++ src/financial/mod.rs
@@ -1,19 +1,19 @@
-mod compound_interest;
 mod equated_monthly_installments;
 mod exponential_moving_average;
 mod finance_ratios;
+mod interest;
 mod npv;
 mod npv_sensitivity;
 mod payback;
 mod present_value;
 mod treynor_ratio;
 
-pub use self::compound_interest::compound_interest;
 pub use self::equated_monthly_installments::equated_monthly_installments;
 pub use self::exponential_moving_average::exponential_moving_average;
 pub use self::finance_ratios::{
     debt_to_equity, earnings_per_sale, gross_profit_margin, return_on_investment,
 };
+pub use self::interest::{apr_interest, compound_interest, simple_interest};
 pub use self::npv::npv;
 pub use self::npv_sensitivity::npv_sensitivity;
 pub use self::payback::payback;
